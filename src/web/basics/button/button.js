f48397eb4850d77f8a8beadfd14cfafad4f81d61
Thijs Bernolet <thijs@recyclerobot.com>
2017-04-18 18:17:59 -0700

    refactor button after interface/core refactors

--- src/web/basics/button/button.js
+++ src/web/basics/button/button.js
@@ -12,22 +12,17 @@ var Core = require('../../shared/js/_core.js');
 
 class Button extends Core {
 
-    constructor(target, options){
+    constructor(options){
         super(options);
-        this._render(target, options);
+        this._render(Template, options);
     }
 
-    _render(target, options){
-        // RENDER TO DOM
-        this.el = super._render(target, options, Template);
+    onClick(fn){
+        return this._attachListener("click", this.el, this.el, fn);
     }
 
-    addHigEventListener(event, fn){
-        if(event == 'click'){
-            this._attachListener("click", this.el, this.el, fn);
-        }else if(event == 'hover'){
-            this._attachListener("hover", this.el, this.el, fn);
-        }
+    onHover(fn){
+        return this._attachListener("hover", this.el, this.el, fn);
     }
 
     setTitle(title){
