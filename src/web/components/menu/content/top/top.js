a15721da815d7590d7bcaad198a1525b164087d8
Thijs Bernolet <thijs@recyclerobot.com>
2017-04-27 17:38:04 -0700

    setLogo and setLogoLink for top

--- src/web/components/menu/content/top/top.js
+++ src/web/components/menu/content/top/top.js
@@ -22,6 +22,16 @@ class Top extends Core {
         return this._attachListener("click", '.js-hig__menu__content__top__hamburger', this.el, fn);
     }
 
+    setLogo(logo){
+        var scope = this._findDOMEl('.hig__menu__content__top__logo', this.el);
+        this._findDOMEl("img", scope).setAttribute("src", logo);
+    }
+
+    setLogoLink(link){
+        var scope = this._findDOMEl('.hig__menu__content__top__logo', this.el);
+        this._findDOMEl("a", scope).setAttribute("href", link);
+    }
+
 }
 
 Top._interface = Interface['Components']['Menu']['partials']['Content']['partials']['Top'];
