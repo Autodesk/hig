bab7cdbd4c33dec028fe5cf945c4d567aa2f135b
Thijs Bernolet <thijs@recyclerobot.com>
2017-05-22 16:29:06 -0700

    expose Icons in module exports

--- src/web/index.js
+++ src/web/index.js
@@ -2,5 +2,6 @@ var Hig = {};
 
 Hig.Button = require('./basics/button/button.js');
 Hig.GlobalNav = require('./components/global-nav/global-nav.js');
+Hig.Icons = require('./basics/icons/icons.js')
 
 module.exports = Hig;
