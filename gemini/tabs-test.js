2a492685e32c22923dc4ed898f6e76c6113e9f16
Sean Durham <sean@sdurham.net>
2017-05-22 14:15:29 -0700

    Add geminit test for Tabs

---
+++ gemini/tabs-test.js
@@ -0,0 +1,18 @@
+gemini.suite('tabs', (parent) => {
+  parent.setUrl('src/web/components/global-nav/tests/tests-global-nav-with-tabs.html');
+
+  gemini.suite('tabs', (suite) => {
+    suite.setCaptureElements('.hig__global-nav__sub-nav__tabs')
+         .capture('tabs');
+  });
+
+  gemini.suite('activated tab', (suite) => {
+    suite.setCaptureElements('.hig__global-nav__sub-nav__tabs__tab.hig__global-nav__sub-nav__tabs__tab--active')
+         .capture('activated tab');
+  });
+
+  gemini.suite('deactivated tab', (suite) => {
+    suite.setCaptureElements('.hig__global-nav__sub-nav__tabs__tab:nth-child(2)')
+         .capture('deactivated tab');
+  });
+});
