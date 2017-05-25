0a3660ec9a056522e6bc63230539471b38693347
Shannon Wells-Mongiovi <shannon@carbonfive.com>
2017-05-25 16:34:48 -0700

    testing deploy script

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -1,57 +1,35 @@
-#!/bin/sh
+#!/bin/bash -x
 # ideas used from https://gist.github.com/motemen/8595451
 
 # abort the script if there is a non-zero error
 set -e
 
-# show where we are on the machine
 pwd
 
 remote=$(git config remote.origin.url)
-
-siteSource="$1"
-
-if [ ! -d "$siteSource" ]
-then
-    echo "Usage: $0 <site source dir>"
-    exit 1
-fi
-GH_PAGES_DIR=gh-pages-branch
-
-# make a directory to put the gp-pages branch
-mkdir $GH_PAGES_DIR
-cd $GH_PAGES_DIR
-
-# now lets setup a new repo so we can update the gh-pages branch
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
 git init
-git remote add --fetch origin "$remote"
+git checkout gh-pages
 
-# switch into the the gh-pages branch
-if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
-then
-    git checkout gh-pages
-    # delete any old site as we are going to replace it
-    # Note: this explodes if there aren't any, so moving it here for now
-    git rm -rf .
-else
-    git checkout --orphan gh-pages
-fi
-
-# copy over or recompile the new site
-cp -a "../${siteSource}/." .
+# rebuild index pages
+content=<<EOF
+<!DOCTYPE html>
+<html>
+  <head><title>Hello, world</title>
+  <body>
+    <h1>Example</h1>
+  </body>
+</html>
+EOF
+echo $content > docs/index.html
 
 # stage any changes and new files
-git add -A
-# now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
-git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
-# and push, but send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin gh-pages > /dev/null 2>&1
+git add docs/*
 
-# go back to where we started and remove the gh-pages git repo we made and used
-# for deployment
-cd ..
-rm -rf $GH_PAGES_DIR
+# now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
+git commit --allow-empty -m "Automated deploy to GitHub pages [ci skip]"
+# and push, but send any output to /dev/null to hide anything sensitive
+git push --force origin gh-pages
 
 echo "Finished Deployment!"
