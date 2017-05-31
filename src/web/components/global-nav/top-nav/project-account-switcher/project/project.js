665188649e1fa63e4e43f782de6728bff77c7a84
Sean Durham <sean@sdurham.net>
2017-05-31 16:04:29 -0700

    Enable updating project

--- src/web/components/global-nav/top-nav/project-account-switcher/project/project.js
+++ src/web/components/global-nav/top-nav/project-account-switcher/project/project.js
@@ -20,6 +20,39 @@ class Project extends Core {
         this._render(Template, options);
     }
 
+    setImage(imageUrl) {
+        this.el
+            .querySelector('.hig__global-nav__top-nav__project-account-switcher__project__image')
+            .setAttribute("src", imageUrl);
+    }
+
+    setLabel(label) {
+        // Update the label
+        this.el
+            .querySelector('.hig__global-nav__top-nav__project-account-switcher__project__label')
+            .innerText = label;
+        // Update the image alt attribute
+        this.el
+            .querySelector('.hig__global-nav__top-nav__project-account-switcher__project__image')
+            .setAttribute("alt", label);
+        // Update the placeholder image initials
+        this.el
+            .querySelector('.hig__global-nav__top-nav__project-account-switcher__project__image-placeholder')
+            .innerText = initials(label);
+    }
+
+    activate() {
+        this.el.classList.add("hig__global-nav__top-nav__project-account-switcher__project--active");
+    }
+
+    deactivate() {
+        this.el.classList.remove("hig__global-nav__top-nav__project-account-switcher__project--active");
+    }
+
+    onClick(fn) {
+        return this._attachListener("click", this.el, this.el, fn);
+    }
+
 }
 
 Project._interface = Interface['components']['GlobalNav']['partials']['TopNav']['partials']['ProjectAccountSwitcher']['partials']['Project'];
