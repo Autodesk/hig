9efca2ffe8d120db3ff8c0ad4a7a36b5cd37a6f2
Shannon Wells-Mongiovi <shannon@carbonfive.com>
2017-06-09 14:33:25 -0700

    add back test.js

---
+++ src/web/helpers/test.js
@@ -0,0 +1,32 @@
+/**
+ * Created by shannon on 6/9/17.
+ */
+
+class TestHelpers {
+  createFullNav() {
+    // CREATE GLOBALNAV COMPONENT
+    var GlobalNav = new Hig.GlobalNav();
+    GlobalNav.mount("body");
+
+    // ADD TOPNAV
+    var TopNav = new GlobalNav.partials.TopNav({
+      "logo": "https://www.dexigner.com/images/news/xw/22856.jpg",
+      "logo_link": "#"
+    });
+    GlobalNav.addTopNav(TopNav);
+    var MyProfile1 = new TopNav.partials.Profile({
+      image: `${imagesPath}/egg.jpg`, name: "Jenny Eishingdrelo", email: "jenny.eishingdrelo@autodesk.com"
+    });
+
+    TopNav.addProfile(MyProfile1);
+    // CREATE PROJECT ACCOUNT SWITCHER
+    var ProjectAccountSwitcher = new TopNav.partials.ProjectAccountSwitcher({
+      activeLabel: 'Global Construction / West Oakwood Medical Center',
+      activeImage: `${imagesPath}/project-1.png`,
+      activeType:  'account'
+    });
+
+    TopNav.addProjectAccountSwitcher(ProjectAccountSwitcher);
+    return {TopNav, GlobalNav};
+  }
+}
