608dc0cc99c7f26ed7dd704d25e7fb6ab479ca73
Shannon Wells-Mongiovi <shannon@carbonfive.com>
2017-06-09 13:50:44 -0700

    post-rebase cleanup

--- src/web/components/global-nav/top-nav/shortcut/shortcut.js
+++ src/web/components/global-nav/top-nav/shortcut/shortcut.js
@@ -17,11 +17,30 @@ class Shortcut extends Core {
         this._render(Template, options);
     }
 
+    onClick(fn){
+        return this._attachListener("click", this.el, this.el, fn);
+    }
+
+    setIcon(icon){
+        this._findDOMEl(".hig__global-nav__top-nav__shortcut__link__icon", this.el).innerHTML = this._getIconString(icon);
+    }
+
+    setTitle(title){
+        this.el.textContent = title;
+        this.el.setAttribute("title", title);
+    }
+
+    setLink(link){
+        this.el.setAttribute("href", link);
+    }
 
 }
 
 Shortcut._interface = Interface['components']['GlobalNav']['partials']['TopNav']['partials']['Shortcut'];
-Shortcut._defaults = {};
-Shortcut._partials = {};
+Shortcut._defaults = {
+    icon: "",
+    title: "",
+    link: "#"
+};
 
 module.exports = Shortcut;
