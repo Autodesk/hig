747b3caae77b74c3357da2a8570648f34bc06cf1
Shannon Wells-Mongiovi <shannon@carbonfive.com>
2017-06-12 10:58:26 -0700

    updates to gemini tests and screenshots

--- gemini/shortcut-test.js
+++ gemini/shortcut-test.js
@@ -2,7 +2,7 @@ gemini.suite('shortcut', (parent) => {
   parent.setUrl('src/web/components/global-nav/top-nav/shortcut/tests/gemini-shortcut.html');
 
   gemini.suite('gear', (suite) => {
-    suite.setCaptureElements('body')
+    suite.setCaptureElements('.hig__global-nav__top-nav')
          .capture('gear shortcut');
   });
 });
