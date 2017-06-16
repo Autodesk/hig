a3541c8a4a94ed26046df784b26b04282c474ff8
Thijs Bernolet <thijs@recyclerobot.com>
2017-06-16 14:58:43 -0700

    allow for deploy to s3 with current branch

---
+++ deploy/deploy-dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get current branch (https://stackoverflow.com/questions/1593051/how-to-programmatically-determine-the-current-checked-out-git-branch)
+branch_name=$(git symbolic-ref -q HEAD)
+branch_name=${branch_name##refs/heads/}
+branch_name=${branch_name:-HEAD}
+
+echo "deploying with current branch: $branch_name"
+
+# Deploy server is hidden behind VPN, so no problem to expose path
+curl -H "Content-Type: application/json" -X POST http://10.40.178.10:8080/job/hig/buildWithParameters?token=y43KplNvGh&BRANCH=$branch_name
