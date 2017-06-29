25b93ea4e59e648909203f43d79afb6cef8cba58
Sean Durham <seandurham@Seans-MacBook-Pro-2.local>
2017-06-28 17:48:18 -0700

    Add IconButton

---
+++ src/web/basics/icon-button/README.md
@@ -0,0 +1,8 @@
+# IconButton
+
+Action buttons that include an icon only, with no background. Useful for compact displays or toolbars with a number of related controls.
+
+## Usage
+
+- Use in toolbars or displays with a number of related controls
+- Provide the label text in a flyout, which shows on hover
--- src/web/index.js
+++ src/web/index.js
@@ -1,6 +1,7 @@
 var Hig = {};
 
 Hig.Button = require('./basics/button/button.js');
+Hig.IconButton = require('./basics/icon-button/icon-button.js');
 Hig.Typography = require('./basics/typography/typography.js');
 Hig.Flyout = require('./basics/flyout/flyout.js');
 Hig.GlobalNav = require('./components/global-nav/global-nav.js');
