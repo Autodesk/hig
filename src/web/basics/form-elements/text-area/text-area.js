79d490df9c50784de6c8c93e1353a8c7254af8c5
Sean Durham <seandurham@Seans-MacBook-Pro-2.local>
2017-07-05 13:20:44 -0700

    Enable updating TextArea with setters

--- src/web/basics/form-elements/text-area/text-area.js
+++ src/web/basics/form-elements/text-area/text-area.js
@@ -17,10 +17,61 @@ class TextArea extends Core {
         this._render(Template, options);
     }
 
+    setInstructions(instructions) {
+        if (instructions) {
+            const instructionsEl = this._findOrAddElement('INSTRUCTIONS', 'p', '.hig__text-area__instructions');
+            instructionsEl.textContent = instructions;
+        } else {
+            this._removeElementIfFound('.hig__text-area__instructions');
+        }
+    }
+
+    setLabel(label) {
+        if (label) {
+            const labelEl = this._findOrAddElement('LABEL', 'label', '.hig__text-area__label');
+            const labelPlaceholderEl = this._findOrAddElement('LABEL-PLACEHOLDER', 'div', '.hig__text-area__label-placeholder');
+            labelEl.textContent = label;
+            labelPlaceholderEl.textContent = label;
+        } else {
+            this._removeElementIfFound('.hig__text-area__label');
+            this._removeElementIfFound('.hig__text-area__label-placeholder');
+        }
+    }
+
+    setPlaceholder(placeholder) {
+        this._findDOMEl('.hig__text-area__field', this.el).setAttribute('placeholder', placeholder);
+    }
+
+    setValue(value) {
+        this._findDOMEl('.hig__text-area__field', this.el).textContent = value;
+    }
+
+    _findOrAddElement(searchComment, tagname, selector) {
+        const existingEl = this.el.querySelector(selector, this.el);
+        if (existingEl) {
+            return existingEl;
+        }
+
+        const newEl = document.createElement(tagname);
+        newEl.classList.add(selector.replace('.', ''));
+        return this.mountPartialToComment(searchComment, newEl);
+    }
+
+    _removeElementIfFound(selector) {
+        const existingEl = this.el.querySelector(selector, this.el);
+        if (existingEl) {
+            return existingEl.remove();
+        }
+    }
+
 }
 
 TextArea._interface = Interface['basics']['FormElements']['partials']['TextArea'];
 TextArea._defaults = {
+    label: '',
+    instructions: '',
+    placeholder: '',
+    value: ''
 };
 TextArea._partials = {};
 
