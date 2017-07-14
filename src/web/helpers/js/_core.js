4ae21992f753d573f411f5fbfeeda09f3f8c6347
Sean Durham <seandurham@Seans-MacBook-Pro-2.local>
2017-07-14 09:50:52 -0700

    Remove call to console.log

--- src/web/helpers/js/_core.js
+++ src/web/helpers/js/_core.js
@@ -263,7 +263,6 @@ class Core {
         }
 
         const newEl = document.createElement(tagname);
-        console.log(selector.split('.'))
         newEl.classList.add(...selector.split('.').filter(c => c.length > 0));
         return this.mountPartialToComment(searchComment, newEl);
     }
