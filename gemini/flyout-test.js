766071b6123c58de3b74658457ceeef1ac7f899a
Sean Durham <seandurham@Seans-MacBook-Pro-2.local>
2017-08-08 18:16:24 -0700

    Add anchorPosition to Flyout

--- gemini/flyout-test.js
+++ gemini/flyout-test.js
@@ -1,5 +1,26 @@
-gemini.suite('flyout', (suite) => {
-  suite.setUrl('src/web/basics/flyout/tests/gemini-flyout.html')
-       .setCaptureElements('.testContainer')
-       .capture('open');
+gemini.suite('flyout', parent => {
+  parent.setUrl('src/web/basics/flyout/tests/gemini-flyout.html');
+
+  const examples = [
+    'top-left',
+    'top-center',
+    'top-right',
+    'right-top',
+    'right-center',
+    'right-bottom',
+    'bottom-left',
+    'bottom-center',
+    'bottom-right',
+    'left-top',
+    'left-center',
+    'left-bottom'
+  ];
+
+  examples.forEach(example => {
+    gemini.suite(example, suite => {
+      suite.setCaptureElements(`.test-container`).capture(example, actions => {
+        actions.click(`.test-container`);
+      });
+    });
+  });
 });
