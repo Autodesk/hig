85f4ffe25b7af76fc0313a2ff4473a683ade2d99
Thijs Bernolet <thijs@recyclerobot.com>
2017-08-14 17:16:44 -0700

    refactor typography to single layer

--- src/implementations/vanilla/src/basics/typography/typography.js
+++ src/implementations/vanilla/src/basics/typography/typography.js
@@ -1,12 +1,11 @@
+import "./typography.scss"
+
+var Template = require('./typography.html');
 var Interface = require('interface.json');
 var Core = require('_core.js');
 
-var Heading = require('./heading/heading.js');
-var Body = require('./body/body.js');
-var Caption = require('./caption/caption.js');
-
 /**
- * Holder for Typography
+ * Creates Typography Class
  *
  * @class
  */
@@ -15,16 +14,30 @@ class Typography extends Core {
 
     constructor(options){
         super(options);
+        
+        // Check for compliance
+        if(['h1', 'h2', 'h3', 'sub1', 'sub2', 'body', 'bold', 'disabled', 'caption'].indexOf(options["type"]) === -1){
+            console.error("Not a valid typography type!");
+        }
+        this._render(Template, options);
+    }
+
+    setText(text){
+        this.el.textContent = text;
+    }
+
+    setType(type){
+        this.el.classList = [];
+        // this.el.classList.add("hig__typography");
+        this.el.classList.add(["hig__typography", "hig__typography__"+type]);
     }
 
 }
 
 Typography._interface = Interface['basics']['Typography'];
-Typography._defaults = {};
-Typography._partials = {
-    Heading: Heading,
-    Body: Body,
-    Caption: Caption
+Typography._defaults = {
+    "text": "text",
+    "type": "body"
 };
 
 module.exports = Typography;
