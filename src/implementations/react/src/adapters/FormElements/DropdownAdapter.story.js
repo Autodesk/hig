49adc2fdce43385cbc2c23f34420cb1856006bf3
Anna Neyzberg <anna@carbonfive.com>
2017-08-15 20:54:16 -0700

    storybook for dropdown

--- src/implementations/react/src/adapters/FormElements/DropdownAdapter.story.js
+++ src/implementations/react/src/adapters/FormElements/DropdownAdapter.story.js
@@ -1,11 +1,47 @@
-// import React from "react";
-// import { storiesOf } from "@storybook/react";
-// import { action } from "@storybook/addon-actions";
-// import { text, select, boolean } from "@storybook/addon-knobs";
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { text, select, boolean } from "@storybook/addon-knobs";
 
-// import Dropdown from './DropdownAdapter';
+import Dropdown from "./DropdownAdapter";
 
-// const Option = Dropdown.Option;
+const Option = Dropdown.Option;
 
-// storiesOf('Dropdown', module)
-// 	.addWithInfo('default dropdown with options')
+const OptionData = [
+  { label: "text option label", value: "test option value", selected: true },
+  {
+    label: "text option label 1",
+    value: "test option value 1",
+    selected: false
+  }
+];
+
+storiesOf(
+  "Dropdown",
+  module
+).addWithInfo("default dropdown with options", "", () => {
+  return (
+    <Dropdown
+      instructions="test instructions"
+      label="Dropdown Label"
+      selectedOptionLabel={OptionData[0].label}
+      open={true}
+      onTargetClick={action("Target clicked")}
+      onClickOutside={action("onClick outside")}
+      onBlur={action("on Blur")}
+      onFocus={action("on Focus")}
+      onKeyPress={action("onKeypress")}
+    >
+      <Option
+        label={OptionData[0].label}
+        value={OptionData[0].value}
+        selected={true}
+      />
+      <Option
+        label={OptionData[1].label}
+        value={OptionData[1].value}
+        selected={false}
+      />
+    </Dropdown>
+  );
+});
