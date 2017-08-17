0f6d4ea8da671f5eeae2705b2ec1baa46062a2c8
Shannon Wells-Mongiovi <shannon@carbonfive.com>
2017-08-17 10:18:22 -0700

    convert classes to functions

--- src/implementations/react/src/elements/components/Typography.js
+++ src/implementations/react/src/elements/components/Typography.js
@@ -1,107 +1,42 @@
 // 'h1', 'h2', 'h3', 'sub1', 'sub2', 'body', 'bold', 'disabled', 'caption'
 import React from 'react';
 import Typography from '../../adapters/TypographyAdapter'
-import * as PropTypes from 'prop-types'
 
-class H1Component extends React.Component {
-  render() {
-    return (<Typography type="h1" text={this.props.children} />)
-  }
+function H1Component(props) {
+    return (<Typography type="h1" text={props.children} />)
 }
 
-class H2Component extends React.Component {
-  render() {
-    return (<Typography type="h2" text={this.props.children} />)
-  }
+function H2Component(props) {
+    return (<Typography type="h2" text={props.children} />)
 }
 
-class H3Component extends React.Component {
-  render() {
-    return (<Typography type="h3" text={this.props.children} />)
-  }
+function H3Component(props) {
+    return (<Typography type="h3" text={props.children} />)
 }
 
-class Sub1Component extends React.Component {
-  render() {
-    return (<Typography type="sub1" text={this.props.children} />)
-  }
+function Sub1Component(props) {
+    return (<Typography type="sub1" text={props.children} />)
 }
 
-class Sub2Component extends React.Component {
-  render() {
-    return (<Typography type="sub2" text={this.props.children} />)
-  }
+function Sub2Component(props) {
+    return (<Typography type="sub2" text={props.children} />)
 }
 
-class BodyComponent extends React.Component {
-  render() {
-    return (<Typography type="body" text={this.props.children} />)
-  }
+function BodyComponent(props) {
+    return (<Typography type="body" text={props.children} />)
 }
 
-class BoldComponent extends React.Component {
-  render() {
-    return (<Typography type="bold" text={this.props.children} />)
-  }
+function BoldComponent(props) {
+    return (<Typography type="bold" text={props.children} />)
 }
 
-class DisabledComponent extends React.Component {
-  render() {
-    return (<Typography type="disabled" text={this.props.children} />)
-  }
+function DisabledComponent(props) {
+    return (<Typography type="disabled" text={props.children} />)
 }
 
-class CaptionComponent extends React.Component {
-  render() {
-    return (<Typography type="caption" text={this.props.children} />)
-  }
+function CaptionComponent(props) {
+    return (<Typography type="caption" text={props.children} />)
 }
-
-H1Component.propTypes = {
-  type: PropTypes.string,
-  text: PropTypes.string,
-  children: PropTypes.node
-};
-H2Component.propTypes = {
-  type: PropTypes.string,
-  text: PropTypes.string,
-  children: PropTypes.node
-};
-H3Component.propTypes = {
-  type: PropTypes.string,
-  text: PropTypes.string,
-  children: PropTypes.node
-};
-Sub1Component.propTypes = {
-  type: PropTypes.string,
-  text: PropTypes.string,
-  children: PropTypes.node
-};
-Sub2Component.propTypes = {
-  type: PropTypes.string,
-  text: PropTypes.string,
-  children: PropTypes.node
-};
-BodyComponent.propTypes = {
-  type: PropTypes.string,
-  text: PropTypes.string,
-  children: PropTypes.node
-};
-BoldComponent.propTypes = {
-  type: PropTypes.string,
-  text: PropTypes.string,
-  children: PropTypes.node
-};
-CaptionComponent.propTypes = {
-  type: PropTypes.string,
-  text: PropTypes.string,
-  children: PropTypes.node
-};
-DisabledComponent.propTypes = {
-  type: PropTypes.string,
-  text: PropTypes.string,
-  children: PropTypes.node
-};
 
 export {
   H1Component as H1,
@@ -112,7 +47,7 @@ export {
   BodyComponent as Body,
   BoldComponent as Bold,
   DisabledComponent as Disabled,
-  CaptionComponent as Caption,
+  CaptionComponent as Caption
 }
 
 
