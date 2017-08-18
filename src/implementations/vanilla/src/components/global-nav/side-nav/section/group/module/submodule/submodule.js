1634d48b1a0a7b7d700763a471356164873ca6e0
Sean Durham <seandurham@novc4zqk12.ads.autodesk.com>
2017-08-18 15:40:22 -0700

    Update sidenav styles

--- src/implementations/vanilla/src/components/global-nav/side-nav/section/group/module/submodule/submodule.js
+++ src/implementations/vanilla/src/components/global-nav/side-nav/section/group/module/submodule/submodule.js
@@ -1,8 +1,8 @@
 import './submodule.scss';
 
-var Template = require('./submodule.html');
-var Interface = require('interface.json');
-var Core = require('_core.js');
+const Template = require('./submodule.html');
+const Interface = require('interface.json');
+const Core = require('_core.js');
 
 /**
  * Creates an Submodule
@@ -11,50 +11,48 @@ var Core = require('_core.js');
  */
 
 class Submodule extends Core {
+  constructor(options) {
+    super(options);
+    this._render(Template, options);
+  }
 
-    constructor(options){
-        super(options);
-        this._render(Template, options);
-    }
+  onClick(fn) {
+    return this._attachListener('click', this.el, this.el, fn);
+  }
 
-    onClick(fn){
-        return this._attachListener("click", this.el, this.el, fn);
-    }
+  onHover(fn) {
+    return this._attachListener('hover', this.el, this.el, fn);
+  }
 
-    onHover(fn){
-        return this._attachListener("hover", this.el, this.el, fn);
-    }
+  setTitle(title) {
+    this.el.textContent = title;
+  }
 
-    setTitle(title){
-        this._findDOMEl(".hig__global-nav__side-nav__section__group__module__submodule__link", this.el).textContent = title;
-    }
+  setLink(link) {
+    this.el.setAttribute('href', link);
+  }
 
-    setLink(link){
-        this._findDOMEl(".hig__global-nav__side-nav__section__group__module__submodule__link", this.el).setAttribute("href", link);
-    }
+  show() {
+    this.el.classList.remove('hig__global-nav__side-nav__section__group__module__submodule--hide');
+  }
 
-    show(){
-        this.el.classList.remove("hig__global-nav__side-nav__section__group__module__submodule--hide");
-    }
+  hide() {
+    this.el.classList.add('hig__global-nav__side-nav__section__group__module__submodule--hide');
+  }
 
-    hide(){
-        this.el.classList.add("hig__global-nav__side-nav__section__group__module__submodule--hide");
-    }
+  activate() {
+    this.el.classList.add('hig__global-nav__side-nav__section__group__module__submodule--active');
+  }
 
-    activate() {
-        this._findDOMEl(".hig__global-nav__side-nav__section__group__module__submodule__link", this.el).classList.add("hig__global-nav__side-nav__section__group__module__submodule__link--active");
-    }
-
-    deactivate() {
-        this._findDOMEl(".hig__global-nav__side-nav__section__group__module__submodule__link", this.el).classList.remove("hig__global-nav__side-nav__section__group__module__submodule__link--active");
-    }
-
+  deactivate() {
+    this.el.classList.remove('hig__global-nav__side-nav__section__group__module__submodule--active');
+  }
 }
 
-Submodule._interface = Interface['components']['GlobalNav']['partials']['SideNav']['partials']['Section']['partials']['Group']['partials']['Module']['partials']['Submodule'];
+Submodule._interface = Interface.components.GlobalNav.partials.SideNav.partials.Section.partials.Group.partials.Module.partials.Submodule;
 Submodule._defaults = {
-    "title": "",
-    "link": ""
+  title: '',
+  link: ''
 };
 Submodule._partials = {};
 
