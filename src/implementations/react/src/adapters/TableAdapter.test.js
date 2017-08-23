b2db7f5a0feb7e971dcbb9cab055449f2e80a403
Anna Neyzberg <anna@carbonfive.com>
2017-08-22 18:33:10 -0700

    add test for table adapter

--- src/implementations/react/src/adapters/TableAdapter.test.js
+++ src/implementations/react/src/adapters/TableAdapter.test.js
@@ -1,49 +1,138 @@
-// /**
-//  Copyright 2016 Autodesk,Inc.
+/**
+ Copyright 2016 Autodesk,Inc.
 
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-//  http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 
-//  */
+ */
 
-// import { mount } from "enzyme";
-// import * as HIG from "hig-vanilla";
-// import React from "react";
+import { mount } from "enzyme";
+import * as HIG from "hig-vanilla";
+import React from "react";
 
-// import TableComponent from "./TableAdapter";
+import TableComponent from "./TableAdapter";
 
-// import SlotCell from "../elements/components/SlotCell";
-// import SlotHeadCell from "../elements/components/SlotHeadCell";
+import SlotCell from "../elements/components/SlotCell";
+import SlotHeadCell from "../elements/components/SlotHeadCell";
 
-// import tableImage from "../playground/images/table-image.png";
+import tableImage from "../playground/images/table-image.png";
 
-// const TableHead = TableComponent.TableHead;
-// const TableRow = TableComponent.TableRow;
-// const TextHeadCell = TableComponent.TableHead.TextHeadCell;
-// const TextCell = TableComponent.TableRow.TextCell;
-// const IconCell = TableComponent.TableRow.IconCell;
+const TableHead = TableComponent.TableHead;
+const TableRow = TableComponent.TableRow;
+const TextHeadCell = TableComponent.TableHead.TextHeadCell;
+const TextCell = TableComponent.TableRow.TextCell;
+const IconCell = TableComponent.TableRow.IconCell;
 
-// describe("<TableAdapter>", () => {
-//   function createHigContext(props) {
-//     const higContainer = document.createElement("div");
+describe("<TableAdapter>", () => {
+  function createHigContext(props) {
+    const higContainer = document.createElement("div");
 
-//     const higTable = new HIG.Table(props);
-// 		higTable.mount(higContainer);
+    const higTable = new HIG.Table();
+    higTable.mount(higContainer);
+    higTable.setDensity(props.table.density);
 
-// 		const higTableHead = new higTable.partials.TableHead();
-// 		higTable.addTableHead(higTableHead);
+    const higTableHead = new higTable.partials.TableHead();
+    higTable.addTableHead(higTableHead);
 
-// 		const TextHeadCell = new higTableHead.partials.TextHeadCell({width: props.TextHeadCell})
-		
+    const emptyCell = new higTableHead.partials.TextHeadCell({
+      width: props.emptyCell.width
+    });
+    higTableHead.addCell(emptyCell);
 
-//   }
-// });
+    const textHeadCell = new higTableHead.partials.TextHeadCell({
+      text: props.textHeadCell.text,
+      alignment: props.textHeadCell.alignment,
+      width: props.textHeadCell.width
+    });
+
+    higTableHead.addCell(textHeadCell);
+
+    const slotEl = document.createElement("div");
+    slotEl.innerHTML = "Raw denim flexitarian green juice kinfolk.";
+    const slotHeadCell = new higTableHead.partials.SlotHeadCell({
+      width: props.slotHeadCell.width
+    });
+
+    higTableHead.addCell(slotHeadCell);
+    slotHeadCell.addSlot(slotEl);
+
+    const higTableRow = new higTable.partials.TableRow();
+    higTable.addTableRow(higTableRow);
+
+    const iconCell = new higTableRow.partials.IconCell({
+      icon: props.iconCell.icon
+    });
+    higTableRow.addCell(iconCell);
+
+    const slotEl1 = document.createElement("div");
+    slotEl1.innerHTML = "test slot cell";
+    const slotCell = new higTableRow.partials.SlotCell();
+
+    higTableRow.addCell(slotCell);
+
+    slotCell.addSlot(slotEl1);
+
+    const textCell = new higTableRow.partials.TextCell({
+      width: props.textCell.width,
+      alignment: props.textCell.alignment,
+      text: props.textCell.text
+    });
+
+    higTableRow.addCell(textCell);
+
+    return { higContainer, higTable: higTable };
+  }
+
+  const Context = props => {
+    return (
+      <TableComponent density={props.table.density}>
+        <TableHead>
+          <TextHeadCell width={props.emptyCell.width} />
+          <TextHeadCell text="Title" width="1fr" alignment="left" />
+          <SlotHeadCell>
+            Raw denim flexitarian green juice kinfolk.
+          </SlotHeadCell>
+        </TableHead>
+        <TableRow>
+          <IconCell icon={props.iconCell.icon} />
+          <SlotCell>test slot cell</SlotCell>
+          <TextCell
+            text={props.textCell.text}
+            alignment={props.textCell.alignment}
+          />
+        </TableRow>
+      </TableComponent>
+    );
+  };
+
+  it("renders a dropdown with intial props", () => {
+    const defaults = {
+      table: { density: "standard" },
+      emptyCell: { width: "30px" },
+      textHeadCell: { text: "Title", alignment: "left", width: "1fr" },
+      slotHeadCell: { width: "1fr" },
+      textCell: { text: "Window Punch List", alignment: "left" },
+      iconCell: { icon: "gear" }
+    };
+
+    const { higContainer, higTable } = createHigContext(defaults);
+    const container = document.createElement("div");
+    const wrapper = mount(<Context {...defaults} />, {
+      attachTo: container
+    });
+
+    expect(container.firstElementChild.outerHTML).toMatchSnapshot();
+    expect(container.firstElementChild.outerHTML).toEqual(
+      higContainer.firstElementChild.outerHTML
+    );
+  });
+});
