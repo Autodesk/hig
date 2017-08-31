89551c70aa8a52675b5a376574f1a8680de67d78
Sean Durham <seandurham@Seans-MacBook-Pro-2.local>
2017-08-31 13:14:32 -0700

    Add RichText component

---
+++ src/implementations/vanilla/src/basics/rich-text/rich-text.js
@@ -0,0 +1,36 @@
+// import './rich-text.scss';
+
+const Template = require('./rich-text.html');
+var Interface = require('interface.json');
+var Core = require('../../helpers/js/_core.js');
+
+/**
+ * Creates a RichText
+ *
+ * @class
+ */
+
+class RichText extends Core {
+  constructor(options = {}) {
+    super(options);
+    this._render(Template, options);
+    this.initialOptions = options;
+  }
+
+  _componentDidMount() {
+    if (this.initialOptions.html) {
+      this.setHtml(this.initialOptions.html);
+    }
+  }
+
+  setHtml(html) {
+    this.el.innerHTML = html;
+  }
+}
+
+RichText._interface = Interface['basics']['RichText'];
+RichText._defaults = {
+  html: ''
+};
+
+module.exports = RichText;
