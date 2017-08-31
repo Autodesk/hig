5cbb702095f15a0755f0bcbaf511a84ace87d83f
Anna Neyzberg <anna@carbonfive.com>
2017-08-30 17:42:02 -0700

    storybook for flyout

--- src/implementations/react/src/stories/index.js
+++ src/implementations/react/src/stories/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import './Button.story';
 import './Checkbox.story';
 import "./Dropdown.story";
+import "./Flyout.story";
 import './GlobalNav.story';
 import './IconButton.story';
 import './Modal.story';
