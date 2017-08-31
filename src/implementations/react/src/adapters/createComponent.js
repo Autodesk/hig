44207b14ab55beff6f31360483a31853f1484fdd
Anna Neyzberg <anna@carbonfive.com>
2017-08-31 13:37:04 -0700

    clean up getChildContext()

--- src/implementations/react/src/adapters/createComponent.js
+++ src/implementations/react/src/adapters/createComponent.js
@@ -1,10 +1,6 @@
-
-
-import React from 'react';
-import * as PropTypes from 'prop-types';
-
-import prepareUpdate from './prepareUpdate';
-
+import React from "react";
+import * as PropTypes from "prop-types";
+import prepareUpdate from "./prepareUpdate";
 
 export default function createComponent(ElementConstructor, childContext = {}) {
   const displayName = ElementConstructor.name;
@@ -77,7 +73,7 @@ export default function createComponent(ElementConstructor, childContext = {}) {
 
     render() {
       return React.createElement(
-        'hig-element',
+        "hig-element",
         {
           ref: r => this._el = r
         },
@@ -86,19 +82,7 @@ export default function createComponent(ElementConstructor, childContext = {}) {
     }
 
     getChildContext() {
-      if (Object.keys(childContext).length > 0 ){
-        return {
-          parent: childContext.parent
-        }
-      } else if (this.instance) {
-        return {
-          parent: this.instance
-        };
-      } else {
-        return {
-          parent: null
-        };
-      }
+      return this.instance ? { parent: this.instance, ...childContext } : {};
     }
   };
 
