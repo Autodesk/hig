ae5207fbafdd5116d97dfdaac8b3df021e1d4cb9
Sean Durham <seandurham@mac6mjhvz1.ads.autodesk.com>
2017-09-05 13:44:30 -0700

    Enable setting inner html on rich text

--- src/implementations/react/src/playground/sections/RichTextSection.js
+++ src/implementations/react/src/playground/sections/RichTextSection.js
@@ -6,12 +6,16 @@ import { RichText } from '../../hig-react';
 const Slot = createSlotComponent();
 
 function RichTextSection() {
+  const html = `
+  <h1>H1: example text</h1>
+  <h2>H2: example text</h2>
+  <h3>H3: example text</h3>
+  `;
+
   return ( <PlaygroundSection title="RichText">
     <Slot>
+      <RichText dangerouslySetInnerHTML={{__html: html}} />
       <RichText>
-        <h1>H1: example text</h1>
-        <h2>H2: example text</h2>
-        <h3>H3: example text</h3>
         <h4>H4, Sub1: example text</h4>
         <h5>H5, Sub2: example text</h5>
         <p>P: Body example text</p>
