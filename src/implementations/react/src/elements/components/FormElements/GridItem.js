0cf536d74ee583d140b1c4eaecde339c12048c0e
Anna Neyzberg <anna@carbonfive.com>
2017-09-11 17:42:03 -0700

    add react adapter for grid

---
+++ src/implementations/react/src/elements/components/FormElements/GridItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import GridItemAdapter from "../../../adapters/FormElements/GridItemAdapter";
+
+class GridItem extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.state = {}
+	}
+
+	setSlotEl = (el) => {
+		this.setState({setSlotEl: el})
+	}
+
+	render() {
+		return(
+				<GridItemAdapter fraction={this.props.fraction} content={this.state.setSlotEl}>
+					<div ref={this.setSlotEl}>{this.props.children}</div>
+				</GridItemAdapter>
+		)
+	}
+}
+
+
+export default GridItem;
