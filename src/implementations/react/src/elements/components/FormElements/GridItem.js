b9c08a137623e7eae60423f0cdca1bf471d63097
Anna Neyzberg <anna@carbonfive.com>
2017-09-12 15:19:25 -0700

    fix spacing

--- src/implementations/react/src/elements/components/FormElements/GridItem.js
+++ src/implementations/react/src/elements/components/FormElements/GridItem.js
@@ -1,26 +1,27 @@
 import React from "react";
 import GridItemAdapter from "../../../adapters/FormElements/GridItemAdapter";
 
-
 class GridItem extends React.Component {
-	constructor(props){
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {}
-	}
+    this.state = {};
+  }
 
-	setSlotEl = (el) => {
-		this.setState({setSlotEl: el})
-	}
+  setSlotEl = el => {
+    this.setState({ setSlotEl: el });
+  };
 
-	render() {
-		return(
-				<GridItemAdapter fraction={this.props.fraction} content={this.state.setSlotEl}>
-					<div ref={this.setSlotEl}>{this.props.children}</div>
-				</GridItemAdapter>
-		)
-	}
+  render() {
+    return (
+      <GridItemAdapter
+        fraction={this.props.fraction}
+        content={this.state.setSlotEl}
+      >
+        <div ref={this.setSlotEl}>{this.props.children}</div>
+      </GridItemAdapter>
+    );
+  }
 }
-
 
 export default GridItem;
