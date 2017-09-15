e225fafd5295fa233cc8d941a4cba2d74871f202
Sean Durham <seandurham@eis52lb152.ads.autodesk.com>
2017-09-15 15:59:42 -0700

    Add onChange example to RadioButton in plyaground

--- src/implementations/react/src/playground/sections/RadioButtonSection.js
+++ src/implementations/react/src/playground/sections/RadioButtonSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PlaygroundSection from '../PlaygroundSection';
 import { RadioButton } from '../../hig-react';
 
@@ -18,45 +18,66 @@ function logEvent(event) {
   console.log(messageParts.join(''));
 }
 
-function RadioButtonSection() {
-  return (
-    <PlaygroundSection title="RadioButton">
-      <div style={checkboxStyle}>
-        <div>empty button<RadioButton /></div><hr />
-        <RadioButton
-          label="I AGREE"
-          name="tsandcs"
-          value="asd"
-          required="Required"
-        />
-        <RadioButton
-          label="Not required"
-          name="tsandcs"
-          value="dfdf"
-          required={null}
-        />
-        <RadioButton
-          label="Disabled"
-          name="tsandcs"
-          value="hhh"
-          disabled={true}
-        />
-        <RadioButton
-          label="Checked"
-          name="tsandcs"
-          value="werr"
-          checked={true}
-        />
-        {' '}
-        <hr />
-        <RadioButton
-          label="Click me"
-          onHover={logEvent}
-          onChange={logEvent}
-          onFocus={logEvent}
-        />
-      </div>
-    </PlaygroundSection>
-  );
+class RadioButtonSection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'asd'
+    };
+  }
+
+  setValue = event => {
+    console.log('CHANGE', event.target.value);
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    return (
+      <PlaygroundSection title="RadioButton">
+        <div style={checkboxStyle}>
+          <div>empty button<RadioButton /></div><hr />
+          <RadioButton
+            label="I AGREE"
+            name="tsandcs"
+            value="asd"
+            required="Required"
+            onChange={this.setValue}
+            checked={this.state.value === 'asd'}
+          />
+          <RadioButton
+            label="Not required"
+            name="tsandcs"
+            value="dfdf"
+            required={null}
+            onChange={this.setValue}
+            checked={this.state.value === 'dfdf'}
+          />
+          <RadioButton
+            label="Disabled"
+            name="tsandcs"
+            value="hhh"
+            disabled={true}
+            onChange={this.setValue}
+            checked={this.state.value === 'hhh'}
+          />
+          <RadioButton
+            label="Checked"
+            name="tsandcs"
+            value="werr"
+            onChange={this.setValue}
+            checked={this.state.value === 'werr'}
+          />
+          {' '}
+          <hr />
+          <RadioButton
+            label="Click me"
+            onHover={logEvent}
+            onChange={logEvent}
+            onFocus={logEvent}
+          />
+        </div>
+      </PlaygroundSection>
+    );
+  }
 }
 export default RadioButtonSection;
