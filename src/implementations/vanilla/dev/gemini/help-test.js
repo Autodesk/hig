7e025808a01c8cca8af56a58d8a96ce0e4a4ec4a
Ari Russo <ari.russo@autodesk.com>
2017-09-27 12:01:25 -0400

    add gemini test for open help menu

--- src/implementations/vanilla/dev/gemini/help-test.js
+++ src/implementations/vanilla/dev/gemini/help-test.js
@@ -1,8 +1,8 @@
 gemini.suite('help', (parent) => {
   parent.setUrl('src/components/global-nav/top-nav/help/tests/gemini-help.html');
 
-  gemini.suite('help-button', (suite) => {
-    suite.setCaptureElements('.hig__global-nav__top-nav')
-      .capture('regular help button');
+  gemini.suite('help', (suite) => {
+    suite.setCaptureElements('body')
+      .capture('open');
   });
 });
