2e81e6d8ce40ddb7d52325108c3258e787b09eb7
Sean Durham <seandurham@Seans-MacBook-Pro-2.local>
2017-10-06 16:29:40 -0700

    Check that exported components have propTypes

--- src/implementations/react/src/setupTests.js
+++ src/implementations/react/src/setupTests.js
@@ -1,5 +1,6 @@
 import SharedExamples from './test/sharedExamples/SharedExamples';
 import './test/matchers/toImplementHIGInterfaceOf';
+import './test/matchers/toHavePropTypesAndDocgenInfo';
 import 'jest-enzyme';
 
 global.SharedExamples = SharedExamples;
---
+++ src/implementations/react/src/test/matchers/toHavePropTypesAndDocgenInfo.js
@@ -0,0 +1,44 @@
+function checkDocgenInfo(Component) {
+  return Object.entries(Component.propTypes).reduce((undocumentedProps, [propName, propType]) => (
+    undocumentedProps//(propType && propType.description) ? undocumentedProps : undocumentedProps.concat(propName)
+  ), []);
+}
+
+function checkComponent(Component) {
+  const propTypes = Component.propTypes;
+
+  if (propTypes === undefined || Object.keys(propTypes).length === 0) {
+    return { propTypes: undefined }
+  }
+
+  return {
+    propTypes,
+    undocumentedProps: checkDocgenInfo(Component)
+  };
+}
+
+expect.extend({
+  toHavePropTypesAndDocGenInfo: (Component) => {
+    const { propTypes, undocumentedProps } = checkComponent(Component);
+
+    if (propTypes === undefined) {
+      return {
+        pass: false,
+        message: () => `Expected ${Component.name} to have propTypes, but it did not.`
+      }
+    } else if (undocumentedProps.length > 0) {
+      return {
+        pass: false,
+        message: () => `
+Expected component to have documented all propTypes, but it did not.
+${undocumentedProps.map(propName => `docgenInfo is missing for "${propName}".`).join('\n')}
+      `
+    }
+    } else {
+      return {
+        pass: true,
+        message: () => `Component implements and documents propTypes as expected.`
+      }
+    }
+  }
+});
