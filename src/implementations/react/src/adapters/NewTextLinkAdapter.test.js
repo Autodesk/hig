09e01c4c7741780951ec02ac5ace19caacd4e0fd
Sean Durham <seandurham@Seans-MacBook-Pro-2.local>
2017-10-06 14:26:28 -0700

    Adapt TextLink

---
+++ src/implementations/react/src/adapters/NewTextLinkAdapter.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import * as HIG from 'hig-vanilla';
+import TextLinkAdapter from './NewTextLinkAdapter';
+
+describe('TextLinkAdapter', () => {
+  it('implements the hig interface', () => {
+    expect((mockInstance) => {
+      mount(
+        <TextLinkAdapter
+          higInstance={mockInstance}
+          text="Foo"
+          href="http://autodesk.com"
+          type="primary"
+          onClick={() => {}}
+        />
+      );
+    }).toImplementHIGInterfaceOf(HIG.TextLink);
+  });
+});
