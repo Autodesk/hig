632542b8ce257ae2124145562612ccef8b122f57
Sean Durham <seandurham@Seans-MacBook-Pro-2.local>
2017-10-16 17:28:30 -0700

    WIP

--- src/implementations/react/.eslintrc.js
+++ src/implementations/react/.eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  'extends': './node_modules/eslint-config-airbnb/.eslintrc',
+  'extends': [
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
   'parser': 'babel-eslint',
   'settings': {
     'import/resolver': {
@@ -8,7 +12,8 @@ module.exports = {
     }
   },
   plugins: [
-    "react"
+    "react",
+    "prettier"
   ],
   globals: {
     describe: true,
@@ -20,10 +25,18 @@ module.exports = {
   },
   'rules': {
     'class-methods-use-this': 0,
-    'max-len': [2, { code: 100 }],
+    'max-len': [1, { code: 100 }],
     'no-underscore-dangle': 0,
     'import/no-extraneous-dependencies': 1,
     'react/jsx-filename-extension': 0,
     "jsx-a11y/href-no-hash": "off",
+    "react/style-prop-object": 0,
+    "react/require-default-props": 0,
+    "react/forbid-prop-types": 0,
+    "react/prefer-stateless-function": 0,
+    "prettier/prettier": "error",
+
+    "react/prop-types": 0,
+    "react/no-unused-prop-types": 0,
   }
 };
