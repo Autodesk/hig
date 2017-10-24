62eb9144b34bf7436792338c7b2e5c5fa2b6ffb0
Anna Neyzberg <anna@carbonfive.com>
2017-10-24 15:50:31 -0700

    finished flyout animation

--- src/implementations/vanilla/src/basics/flyout/flyout.js
+++ src/implementations/vanilla/src/basics/flyout/flyout.js
@@ -4,7 +4,16 @@ const Template = require('./flyout.html');
 const Interface = require('interface.json');
 const Core = require('../../helpers/js/_core.js');
 
-const OPEN_CLASS = 'hig__flyout--open';
+const OPENING_CLASS = 'hig__flyout--opening';
+const OPENED_CLASS = 'hig__flyout--opened';
+const CLOSING_CLASS = 'hig__flyout--closing';
+const CLOSED_CLASS = 'hig__flyout--closed';
+const ANIMATION_CLASSES = [
+  OPENING_CLASS,
+  OPENED_CLASS,
+  CLOSING_CLASS,
+  CLOSED_CLASS
+];
 
 const ANCHOR_POINTS = [
   'top-left',
@@ -36,14 +45,61 @@ class Flyout extends Core {
 
   _componentDidMount() {
     this.setAnchorPoint(this.initialOptions.anchorPoint);
+    this.flyoutContainer = this._findDOMEl('.hig__flyout__container', this.el);
   }
 
   open() {
-    this.el.classList.add(OPEN_CLASS);
+    if (
+      this.el.classList.contains(OPENING_CLASS) ||
+      this.el.classList.contains(OPENED_CLASS)
+    ) {
+      return;
+    }
+
+    this._resetAnimationState();
+    this.flyoutContainer.addEventListener(
+      'animationend',
+      this.handleOpenComplete
+    );
+    this.el.classList.add(OPENING_CLASS);
   }
 
+  handleOpenComplete = (event) => {
+    this._resetAnimationState();
+    this.el.classList.add(OPENED_CLASS);
+  };
+
   close() {
-    this.el.classList.remove(OPEN_CLASS);
+    if (
+      this.el.classList.contains(CLOSING_CLASS) ||
+      this.el.classList.contains(CLOSED_CLASS)
+    ) {
+      return;
+    }
+    console.log('Closing flyout');
+    this._resetAnimationState();
+    this.flyoutContainer.addEventListener(
+      'animationend',
+      this.handleCloseComplete
+    );
+    this.el.classList.add(CLOSING_CLASS);
+  }
+
+  handleCloseComplete = (event) => {
+    this._resetAnimationState();
+    this.el.classList.add(CLOSED_CLASS);
+  };
+
+  _resetAnimationState() {
+    this.el.classList.remove(...ANIMATION_CLASSES);
+    this.flyoutContainer.removeEventListener(
+      'animationend',
+      this.handleOpenComplete
+    );
+    this.flyoutContainer.removeEventListener(
+      'animationend',
+      this.handleCloseComplete
+    );
   }
 
   onClickOutside(fn) {
@@ -74,7 +130,9 @@ class Flyout extends Core {
 
     const container = this._findDOMEl('.hig__flyout__container', this.el);
     container.classList.remove(
-      ...Flyout.AvailableAnchorPoints.map(s => `hig__flyout__container--anchor-${s}`)
+      ...Flyout.AvailableAnchorPoints.map(
+        s => `hig__flyout__container--anchor-${s}`
+      )
     );
     container.classList.add(`hig__flyout__container--anchor-${anchorPoint}`);
   }
@@ -83,7 +141,7 @@ class Flyout extends Core {
     if (this.el.contains(event.target) || this.el === event.target) {
       return;
     }
-    if (this.el.classList.contains(OPEN_CLASS)) {
+    if (this.el.classList.contains(OPENED_CLASS)) {
       callback();
     }
   }
