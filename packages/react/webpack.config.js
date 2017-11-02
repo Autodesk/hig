71e71e7f73df7d9ac9f7d324879892910c848d6d
Sean Durham <seandurham@Seans-MacBook-Pro-2.local>
2017-11-02 16:22:45 -0700

    Publish css

--- packages/react/webpack.config.js
+++ packages/react/webpack.config.js
@@ -1,51 +1,50 @@
-const path = require('path');
-const babel = require('./webpack/babel');
-const minify = require('./webpack/minify');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const babel = require("./webpack/babel");
+const minify = require("./webpack/minify");
 
-const externals = [
-  'react',
-  'react-dom',
-  'prop-types'
-];
+const externals = ["react", "react-dom", "prop-types"];
 
 const debug = {
-  devtool: 'source-map',
-  entry: './src/hig-react.js',
+  devtool: "source-map",
+  entry: "./src/hig-react.js",
   output: {
-    path: path.resolve('./lib'),
-    filename: 'hig-react-debug.js',
-    library: 'HigReact',
-    libraryTarget: 'umd'
+    path: path.resolve("./lib"),
+    filename: "hig-react-debug.js",
+    library: "HigReact",
+    libraryTarget: "umd"
   },
   module: {
-    rules: [
-      babel({ plugins: ['react-docgen'] })
-    ]
+    rules: [babel({ plugins: ["react-docgen"] })]
   },
   plugins: [],
   externals
 };
 
 const production = {
-  entry: './src/hig-react.js',
+  entry: "./src/hig-react.js",
   output: {
-    path: path.resolve('./lib'),
-    filename: 'hig-react.js',
-    library: 'HigReact',
-    libraryTarget: 'umd'
+    path: path.resolve("./lib"),
+    filename: "hig-react.js",
+    library: "HigReact",
+    libraryTarget: "umd"
   },
   module: {
-    rules: [
-      babel()
-    ]
+    rules: [babel()]
   },
   plugins: [
-    minify()
+    minify(),
+    new CopyWebpackPlugin([
+      {
+        from: "../vanilla/lib/hig.css",
+        to: "hig-react.css"
+      }
+    ])
   ],
   externals
 };
 
-if (process.env.NODE_ENV != 'production') {
+if (process.env.NODE_ENV !== "production") {
   module.exports = debug;
 } else {
   module.exports = production;
