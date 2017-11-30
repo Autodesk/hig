41d536a27e729cd986816811b38d14f065e898b3
Sean Durham <seandurham@ccs003460174053.ads.autodesk.com>
2017-11-30 15:07:22 -0800

    Clean up unused code

--- packages/react/src/adapters/ActionBar/ActionBarAdapter.js
+++ packages/react/src/adapters/ActionBar/ActionBarAdapter.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { ActionBar as VanillaActionBar } from "hig-vanilla";
-import ActionBarGroupAdapter from "./ActionBarGroupAdapter";
-import ActionBarSpacerAdapter from "./ActionBarSpacerAdapter";
 
 class ActionBarAdapter extends Component {
   render() {
@@ -13,17 +11,14 @@ class ActionBarAdapter extends Component {
 }
 
 ActionBarAdapter.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.instanceOf(ActionBarGroupAdapter),
-    PropTypes.instanceOf(ActionBarSpacerAdapter)
-  ])
+  children: PropTypes.node
 };
 
 ActionBarAdapter.__docgenInfo = {
   props: {
     children: {
       description:
-        "{ ActionBarGroup or ActionBarSpacer } content for the ActionBar"
+        "Content for the ActionBar. Typically ActionBarGroup or ActionBarSpacer"
     }
   }
 };
