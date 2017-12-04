103724e8d4e0da0bffb96209f66e6fcefeec3e33
Anna Neyzberg <anna@carbonfive.com>
2017-12-04 14:34:48 -0800

    fix tests

--- packages/react/src/adapters/ImageAdapter.js
+++ packages/react/src/adapters/ImageAdapter.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { Image as VanillaImage } from "hig-vanilla";
 
 class ImageAdapter extends Component {
   render() {
-    return <img className={VanillaImage.className} {...this.props} />;
+    return (
+      <img
+        className={VanillaImage.className}
+        src={this.props.src}
+        {...this.props}
+      />
+    );
   }
 }
 
+ImageAdapter.propTypes = {
+  src: PropTypes.string
+};
+
+ImageAdapter.__docgenInfo = {
+  props: {
+    src: {
+      description: "path to image"
+    }
+  }
+};
 export default ImageAdapter;
