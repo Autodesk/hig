ed243af12f969bb0637ae0688cf7ba6dbc143322
Sean Durham <seandurham@Seans-MacBook-Pro-2.local>
2017-12-05 16:32:12 -0800

    Style large expanding filter section

--- packages/vanilla/src/components/expanding-filter-section/expanding-filter-section.js
+++ packages/vanilla/src/components/expanding-filter-section/expanding-filter-section.js
@@ -2,12 +2,17 @@ import Interface from 'interface.json';
 import Core from '_core.js';
 import './expanding-filter-section.scss';
 import Template from './expanding-filter-section.html';
+import Icon from '../../basics/icon/icon';
 
 class ExpandingFilterSection extends Core {
   constructor(options = {}) {
     super(options);
     this._render(Template, options);
     this.initialOptions = options;
+  }
+
+  _componentDidMount() {
+    this._addCaretIcon();
   }
 
   close() {
@@ -36,9 +41,18 @@ class ExpandingFilterSection extends Core {
   }
 
   addSlot(content) {
-    this.el.querySelector(
-      '.hig__expanding-filter-section__content'
-    ).innerHTML = content;
+    this.el
+      .querySelector('.hig__expanding-filter-section__content')
+      .appendChild(content);
+  }
+
+  _addCaretIcon() {
+    const mountEl = this._findDOMEl(
+      '.hig__expanding-filter-section__caret',
+      this.el
+    );
+    const icon = new Icon({ nameOrSVG: 'caret' });
+    icon.mount(mountEl);
   }
 }
 
