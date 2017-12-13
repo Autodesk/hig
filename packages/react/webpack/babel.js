2c9ecc7c6a9930785ead102d1ca67e1875a47901
Eric Fung <ericf@carbonfive.com>
2017-12-13 14:01:28 -0800

    [react] Prefer babel-preset-env over es2015

--- packages/react/webpack/babel.js
+++ packages/react/webpack/babel.js
@@ -3,16 +3,26 @@ module.exports = function babel(options = {}) {
     test: /\.js$/,
     exclude: [/node_modules/, /..\/vanilla/],
     use: {
-      loader: 'babel-loader',
-      options: Object.assign({
-        presets: [
-          'es2015',
-          'react',
-          'stage-2'
-        ],
-        babelrc: false,
-        compact: false
-      }, options)
+      loader: "babel-loader",
+      options: Object.assign(
+        {
+          presets: [
+            [
+              "env",
+              {
+                targets: {
+                  browsers: "> 1%"
+                }
+              }
+            ],
+            "react",
+            "stage-2"
+          ],
+          babelrc: false,
+          compact: false
+        },
+        options
+      )
     }
-  }
-}
+  };
+};
