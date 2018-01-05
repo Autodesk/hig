2f0429eb6b35fe44520c375d6aac5860697b7324
Sean Durham <seandurham@rembhf9by1.ads.autodesk.com>
2018-01-04 17:00:52 -0800

    Add Gemini tests for sidenav light and dark blue themes

--- packages/vanilla/dev/gemini/side-nav-test.js
+++ packages/vanilla/dev/gemini/side-nav-test.js
@@ -1,8 +1,14 @@
 gemini.suite('side-nav', (parent) => {
-  parent.setUrl('src/components/global-nav/side-nav/tests/gemini-side-nav.html');
+  parent.setUrl(
+    'src/components/global-nav/side-nav/tests/gemini-side-nav.html'
+  );
 
   gemini.suite('side-nav', (suite) => {
-    suite.setCaptureElements('body')
-      .capture('open');
+    suite
+      .setCaptureElements('body')
+      .capture('light theme')
+      .capture('dark blue theme', (actions, find) => {
+        actions.click(find('body'));
+      });
   });
 });
