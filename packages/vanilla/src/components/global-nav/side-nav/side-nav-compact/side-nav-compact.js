7291c608c027d4a1fe112e9d23e9e387c09d42fc
Eric Fung <eskfung@users.noreply.github.com>
2018-01-16 16:42:44 -0800

    Replicate interface on SideNavCompact

--- packages/vanilla/src/components/global-nav/side-nav/side-nav-compact/side-nav-compact.js
+++ packages/vanilla/src/components/global-nav/side-nav/side-nav-compact/side-nav-compact.js
@@ -1,5 +1,6 @@
 import Interface from 'interface.json';
 import Core from '_core.js';
+import IconButton from 'components/icon-button/icon-button';
 import Group from 'components/global-nav/side-nav/group/group';
 import Template from './side-nav-compact.html';
 import './side-nav-compact.scss';
@@ -17,13 +18,54 @@ export default class SideNavCompact extends Core {
   }
 
   _componentDidMount() {
-    this.themedElements = [this.el];
+    this.themedElements = [
+      this.el,
+      this._findDOMEl('.hig__global-nav__sidenav-compact__collapse', this.el)
+    ];
+
+    this._setExpandButton();
   }
 
   addGroup(groupInstance, referenceInstance) {
     if (groupInstance instanceof Group) {
       this.mountPartialToComment('GROUP', groupInstance, referenceInstance);
     }
+  }
+
+  showVariantToggleButton() {
+    this._findDOMEl(
+      '.hig__global-nav__sidenav-compact__collapse',
+      this.el
+    ).classList.add(
+      'hig__global-nav__sidenav-compact__collapse--active'
+    );
+  }
+
+  hideVariantToggleButton() {
+    this._findDOMEl(
+      '.hig__global-nav__sidenav-compact__collapse',
+      this.el
+    ).classList.remove(
+      'hig__global-nav__sidenav-compact__collapse--active'
+    );
+  }
+
+  onVariantToggleClick(fn) {
+    return this._attachListener(
+      'click',
+      this.iconButton.el,
+      this.el,
+      fn
+    );
+  }
+
+  _setExpandButton() {
+    this.iconButton = new IconButton({
+      title: 'Expand',
+      icon: 'forward',
+      type: 'flat'
+    });
+    this.mountPartialToComment('COLLAPSE', this.iconButton, this.el);
   }
 }
 
