cbd52eb84cd4a9a774ddfbf0652a7b8853dd8e10
Eric Fung <ericf@carbonfive.com>
2018-01-31 15:12:15 -0800

    Split Typography exports into separate files
    This should make it easier for our docgen plugin to find and annotate
    the individual typography components

---
+++ packages/react/src/elements/components/Typography/H2.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Typography as VanillaTypography } from "hig-vanilla";
+import Typography from "../../../adapters/TypographyAdapter";
+
+export default class H2 extends React.PureComponent {
+  render() {
+    const { children, ...remainingProps } = this.props;
+    return <Typography type="h2" text={children} {...remainingProps} />;
+  }
+}
+H2.propTypes = {
+  /**
+   * Whether to render bold text
+   */
+  bold: PropTypes.bool,
+  /**
+   * Text to show inside the typography
+   */
+  children: PropTypes.string.isRequired,
+  /**
+   * Colors the text with one of the supported HIG colors
+   */
+  color: PropTypes.oneOf(VanillaTypography.VALID_COLORS),
+  /**
+   * Whether to show text as disabled
+   */
+  disabled: PropTypes.bool,
+  /**
+   * An opacity value to modify the color, between 0.0 and 1.0
+   */
+  opacity: PropTypes.number,
+  /**
+   * Sizes the text with one of the supported modifiers
+   */
+  size: PropTypes.oneOf(VanillaTypography.VALID_SIZES),
+  /**
+   * Indicates the initial Typography style
+   */
+  type: PropTypes.oneOf(VanillaTypography.VALID_TYPES).isRequired
+};
