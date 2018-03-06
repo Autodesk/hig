1ae4bbcb397ff74df362c3895e50b5c2b9e6bb5d
Eric Fung <eric.fung@autodesk.com>
2018-03-06 13:10:33 -0800

    Add storybook knobs

--- packages/react/stories/Button.js
+++ packages/react/stories/Button.js
@@ -1,16 +1,41 @@
 import React from "react";
+import { Button as VanillaButton } from "hig-vanilla";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import { text, boolean, select } from "@storybook/addon-knobs/react";
 import { Button } from "../src/hig-react";
+import arrayToObject from "../.storybook/arrayToObject";
 
 storiesOf("Button", module)
   .add("default, with event handlers", () => (
     <Button
-      title="Button Text"
+      title={text("Title", "Button Text")}
       onBlur={action("onBlur")}
       onClick={action("onClick")}
       onFocus={action("onFocus")}
       onHover={action("onHover")}
+      disabled={boolean("Disabled", false)}
+      link={text("Link")}
+      target={select(
+        "Target",
+        arrayToObject(["_self", "_blank", "_parent", "_top"])
+      )}
+      size={select(
+        "Size",
+        arrayToObject(VanillaButton.AvailableSizes),
+        "standard"
+      )}
+      type={select(
+        "Type",
+        arrayToObject(VanillaButton.AvailableTypes),
+        "primary"
+      )}
+      width={select(
+        "Width",
+        arrayToObject(VanillaButton.AvailableWidths),
+        "shrink"
+      )}
+      icon={text("Icon")}
     />
   ))
   .add("disabled", () => <Button title="Button Text" disabled />)
@@ -28,16 +53,16 @@ storiesOf("Button/Link", module)
     />
   ));
 
-storiesOf("Button/sizes", module)
+storiesOf("Button/Sizes", module)
   .add("small", () => <Button title="Button Text" size="small" />)
   .add("standard", () => <Button title="Button Text" size="standard" />)
   .add("large", () => <Button title="Button Text" size="large" />);
 
-storiesOf("Button/types", module)
-  .add("primary", () => <Button title="Button Text" types="primary" />)
-  .add("secondary", () => <Button title="Button Text" types="secondary" />)
-  .add("flat", () => <Button title="Button Text" types="flat" />);
+storiesOf("Button/Types", module)
+  .add("primary", () => <Button title="Button Text" type="primary" />)
+  .add("secondary", () => <Button title="Button Text" type="secondary" />)
+  .add("flat", () => <Button title="Button Text" type="flat" />);
 
-storiesOf("Button/widths", module)
+storiesOf("Button/Widths", module)
   .add("shrink", () => <Button title="Button Text" width="shrink" />)
   .add("grow", () => <Button title="Button Text" width="grow" />);
--- packages/react/stories/Typography.js
+++ packages/react/stories/Typography.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { Typography as VanillaTypography } from "hig-vanilla";
+import { number, boolean, select } from "@storybook/addon-knobs/react";
 import {
   H1,
   H2,
@@ -12,13 +14,29 @@ import {
   Sub1,
   Sub2
 } from "../src/hig-react";
+import arrayToObject from "../.storybook/arrayToObject";
 
 [H1, H2, H3, Text, Body, Bold, Caption, Disabled, Sub1, Sub2].forEach(
   Component => {
     const name = Component.name;
 
     storiesOf(name, module)
-      .add("with text", () => <Component>{`${name} example text`}</Component>)
+      .add("with text", () => (
+        <Component
+          bold={boolean("Bold", false)}
+          disabled={boolean("Disabled", false)}
+          color={select("Color", arrayToObject(VanillaTypography.VALID_COLORS))}
+          opacity={number("Opacity", 1.0, {
+            range: true,
+            min: 0.0,
+            max: 1.0,
+            step: 0.1
+          })}
+          size={select("Size", arrayToObject(VanillaTypography.VALID_SIZES))}
+        >
+          {`${name} example text`}
+        </Component>
+      ))
       .add("bold", () => <Component bold>{`${name} example text`}</Component>)
       .add("disabled", () => (
         <Component disabled>{`${name} example text`}</Component>
@@ -30,7 +48,7 @@ import {
         <Component opacity={0.5}>{`${name} example text`}</Component>
       ));
 
-    storiesOf(`${name}/sizes`, module)
+    storiesOf(`${name}/Sizes`, module)
       .add("small", () => (
         <Component size="small">{`${name} example text`}</Component>
       ))
