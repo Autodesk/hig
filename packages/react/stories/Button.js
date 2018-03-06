96ac014c5e78bbfca9d2bc9c04998e107b091191
Eric Fung <eric.fung@autodesk.com>
2018-03-06 14:43:37 -0800

    Reduce to one story per component, with knobs

--- packages/react/stories/Button.js
+++ packages/react/stories/Button.js
@@ -6,63 +6,35 @@ import { text, boolean, select } from "@storybook/addon-knobs/react";
 import { Button } from "../src/hig-react";
 import arrayToObject from "../.storybook/arrayToObject";
 
-storiesOf("Button", module)
-  .add("default, with event handlers", () => (
-    <Button
-      title={text("Title", "Button Text")}
-      onBlur={action("onBlur")}
-      onClick={action("onClick")}
-      onFocus={action("onFocus")}
-      onHover={action("onHover")}
-      disabled={boolean("Disabled", false)}
-      link={text("Link")}
-      target={select(
-        "Target",
-        arrayToObject(["_self", "_blank", "_parent", "_top"])
-      )}
-      size={select(
-        "Size",
-        arrayToObject(VanillaButton.AvailableSizes),
-        "standard"
-      )}
-      type={select(
-        "Type",
-        arrayToObject(VanillaButton.AvailableTypes),
-        "primary"
-      )}
-      width={select(
-        "Width",
-        arrayToObject(VanillaButton.AvailableWidths),
-        "shrink"
-      )}
-      icon={text("Icon")}
-    />
-  ))
-  .add("disabled", () => <Button title="Button Text" disabled />)
-  .add("with icon", () => <Button title="Button Text" icon="settings" />);
-
-storiesOf("Button/Link", module)
-  .add("default", () => (
-    <Button title="Button Text" link="https://www.autodesk.com/" />
-  ))
-  .add("target", () => (
-    <Button
-      title="Button Text"
-      link="https://www.autodesk.com/"
-      target="_blank"
-    />
-  ));
-
-storiesOf("Button/Sizes", module)
-  .add("small", () => <Button title="Button Text" size="small" />)
-  .add("standard", () => <Button title="Button Text" size="standard" />)
-  .add("large", () => <Button title="Button Text" size="large" />);
-
-storiesOf("Button/Types", module)
-  .add("primary", () => <Button title="Button Text" type="primary" />)
-  .add("secondary", () => <Button title="Button Text" type="secondary" />)
-  .add("flat", () => <Button title="Button Text" type="flat" />);
-
-storiesOf("Button/Widths", module)
-  .add("shrink", () => <Button title="Button Text" width="shrink" />)
-  .add("grow", () => <Button title="Button Text" width="grow" />);
+storiesOf("Button", module).add("default, with event handlers", () => (
+  <Button
+    title={text("Title", "Button Text")}
+    onBlur={action("onBlur")}
+    onClick={action("onClick")}
+    onFocus={action("onFocus")}
+    onHover={action("onHover")}
+    disabled={boolean("Disabled", false)}
+    link={text("Link", "https://www.autodesk.com/")}
+    target={select(
+      "Target",
+      arrayToObject(["_self", "_blank", "_parent", "_top"]),
+      "_blank"
+    )}
+    size={select(
+      "Size",
+      arrayToObject(VanillaButton.AvailableSizes),
+      "standard"
+    )}
+    type={select(
+      "Type",
+      arrayToObject(VanillaButton.AvailableTypes),
+      "primary"
+    )}
+    width={select(
+      "Width",
+      arrayToObject(VanillaButton.AvailableWidths),
+      "shrink"
+    )}
+    icon={text("Icon", "settings")}
+  />
+));
--- packages/react/stories/Typography.js
+++ packages/react/stories/Typography.js
@@ -20,43 +20,29 @@ import arrayToObject from "../.storybook/arrayToObject";
   Component => {
     const name = Component.name;
 
-    storiesOf(name, module)
-      .add("with text", () => (
-        <Component
-          bold={boolean("Bold", false)}
-          disabled={boolean("Disabled", false)}
-          color={select("Color", arrayToObject(VanillaTypography.VALID_COLORS))}
-          opacity={number("Opacity", 1.0, {
-            range: true,
-            min: 0.0,
-            max: 1.0,
-            step: 0.1
-          })}
-          size={select("Size", arrayToObject(VanillaTypography.VALID_SIZES))}
-        >
-          {`${name} example text`}
-        </Component>
-      ))
-      .add("bold", () => <Component bold>{`${name} example text`}</Component>)
-      .add("disabled", () => (
-        <Component disabled>{`${name} example text`}</Component>
-      ))
-      .add("with custom colors", () => (
-        <Component color="hig-blue-50">{`${name} example text`}</Component>
-      ))
-      .add("with 0.5 opacity", () => (
-        <Component opacity={0.5}>{`${name} example text`}</Component>
-      ));
-
-    storiesOf(`${name}/Sizes`, module)
-      .add("small", () => (
-        <Component size="small">{`${name} example text`}</Component>
-      ))
-      .add("medium", () => (
-        <Component size="medium">{`${name} example text`}</Component>
-      ))
-      .add("large", () => (
-        <Component size="large">{`${name} example text`}</Component>
-      ));
+    storiesOf(name, module).add("with text", () => (
+      <Component
+        bold={boolean("Bold", false)}
+        disabled={boolean("Disabled", false)}
+        color={select(
+          "Color",
+          arrayToObject(VanillaTypography.VALID_COLORS),
+          "hig-cool-gray-70"
+        )}
+        opacity={number("Opacity", 1.0, {
+          range: true,
+          min: 0.0,
+          max: 1.0,
+          step: 0.1
+        })}
+        size={select(
+          "Size",
+          arrayToObject(VanillaTypography.VALID_SIZES),
+          "medium"
+        )}
+      >
+        {`${name} example text`}
+      </Component>
+    ));
   }
 );
