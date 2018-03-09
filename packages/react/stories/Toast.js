8167c4db05017c816e25e35151c202c91241fe05
Eric Fung <ericf@carbonfive.com>
2018-03-09 15:16:18 -0800

    Initial text positioning

--- packages/react/stories/Toast.js
+++ packages/react/stories/Toast.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import { text } from "@storybook/addon-knobs/react";
 import { Toast, Text } from "../src/hig-react";
 
 storiesOf("Toast", module).add("default", () => (
   <Toast onDismiss={action("Toast dismissed")}>
-    <Text>Who wants toast??</Text>
+    <Text>{text("Children", "Who wants toast?")}</Text>
   </Toast>
 ));
