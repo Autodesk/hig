b818105c7f67563ffa2e356fea50c8ff143df1f9
Eric Fung <ericf@carbonfive.com>
2018-03-09 12:30:27 -0800

    Wire up icon button and click handler

--- packages/react/src/elements/components/Toast/Toast.js
+++ packages/react/src/elements/components/Toast/Toast.js
@@ -3,9 +3,28 @@ import PropTypes from "prop-types";
 import ToastPresenter from "./ToastPresenter";
 
 export default class Toast extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: true
+    };
+  }
+
+  onDismiss = () => {
+    this.setState({ visible: false });
+  };
+
   render() {
     const { children, ...otherProps } = this.props;
-    return <ToastPresenter {...otherProps}>{children}</ToastPresenter>;
+    return (
+      <ToastPresenter
+        {...otherProps}
+        onDismiss={this.onDismiss}
+        visible={this.state.visible}
+      >
+        {children}
+      </ToastPresenter>
+    );
   }
 }
 
--- packages/react/src/elements/components/Toast/ToastPresenter.js
+++ packages/react/src/elements/components/Toast/ToastPresenter.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import IconButton from "../../../adapters/IconButtonAdapter";
 import "./toast.css";
 
 export default class ToastPresenter extends Component {
   render() {
-    return <div className="hig__toast">{this.props.children}</div>;
+    return (
+      this.props.visible && (
+        <div className="hig__toast">
+          <div className="hig__toast__contents">{this.props.children}</div>
+          <IconButton
+            title="Dismiss"
+            icon="close-notification"
+            type="flat"
+            onClick={this.props.onDismiss}
+          />
+        </div>
+      )
+    );
   }
 }
 
@@ -12,5 +25,13 @@ ToastPresenter.propTypes = {
   /**
    * Content for the Toast
    */
-  children: PropTypes.node
+  children: PropTypes.node,
+  /**
+   * Function to call when Toast is dismissed
+   */
+  onDismiss: PropTypes.func,
+  /**
+   * Whether Toast is rendered or not
+   */
+  visible: PropTypes.bool
 };
