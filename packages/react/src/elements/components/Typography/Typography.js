b7b83a92e4e89b5cbfa3e596d7035b432cee8a1b
Eric Fung <ericf@carbonfive.com>
2018-03-14 12:40:27 -0700

    Port typography styles and mixins

--- packages/react/src/elements/components/Typography/Typography.js
+++ packages/react/src/elements/components/Typography/Typography.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
+import "./typography.scss";
 
 export const validColors = [
   "hig-white",
