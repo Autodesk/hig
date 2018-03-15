cb412a3945878928f222d8de55d3c23d8dc1aba9
Eric Fung <ericf@carbonfive.com>
2018-03-15 13:46:13 -0700

    Add and configure Gemini to work with Sauce Labs

---
+++ packages/react/.gemini.js
@@ -0,0 +1,30 @@
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./gemini/.env" });
+
+module.exports = {
+  rootUrl: "http://localhost:8080",
+  gridUrl: "http://ondemand.saucelabs.com/wd/hub",
+  screenshotsDir: "./gemini/screens",
+  windowSize: "1024x768",
+  compositeImage: true,
+  sessionsPerBrowser: 3,
+  system: {
+    plugins: {
+      sauce: {
+        username: process.env.SAUCE_USERNAME,
+        accessKey: process.env.SAUCE_ACCESS_KEY
+      }
+    }
+  },
+  browsers: {
+    chrome: {
+      desiredCapabilities: {
+        browserName: "chrome",
+        version: "54.0",
+        platform: "macOs 10.12",
+        name: "gemini visual regression tests"
+      }
+    }
+  }
+};
