db578827ba9e340a640be873915ff0d25cc2543c
Eric Fung <ericf@carbonfive.com>
2018-03-15 11:24:50 -0700

    Image style updates to account for wrapping text

--- packages/react/stories/Toast.js
+++ packages/react/stories/Toast.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import { text } from "@storybook/addon-knobs/react";
+import { text, select } from "@storybook/addon-knobs/react";
 import { Avatar, Toast } from "../src/hig-react";
 
 storiesOf("Toast", module)
   .add("default", () => (
-    <Toast onDismiss={action("Toast dismissed")} status={text("Status")}>
-      {text("Children", "Who wants toast?")}
+
+    <Toast
+      onDismiss={action("Toast dismissed")}
+      status={select("Status", [
+        "primary",
+        "success",
+        "danger",
+        "warning",
+        null
+      ])}
+    >
+      {text(
+        "Children",
+        "It's probably useful in the long run to default to showing multiple lines of toast. Let's say, three?"
+      )}
     </Toast>
   ))
 
@@ -17,16 +30,26 @@ storiesOf("Toast", module)
       onDismiss={action("Toast dismissed")}
       status={text("Status")}
     >
-      {text("Children", "Who wants toast?")}
+      {text(
+        "Children",
+        "It's probably useful in the long run to default to showing multiple lines of toast. Let's say, three?"
+      )}
     </Toast>
   ))
 
   .add("with a thumbnail", () => (
     <Toast
-      image={<img src="http://placekitten.com/g/48/48" />}
+      image={
+        <div style={{ width: "48px", height: "48px" }}>
+          <img src="http://placekitten.com/g/48/48" />
+        </div>
+      }
       onDismiss={action("Toast dismissed")}
       status={text("Status")}
     >
-      {text("Children", "Who wants toast?")}
+      {text(
+        "Children",
+        "It's probably useful in the long run to default to showing multiple lines of toast. Let's say, three?"
+      )}
     </Toast>
   ));
