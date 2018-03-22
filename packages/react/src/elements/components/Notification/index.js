a4dc02f1af84422913371f97418c09dab3ecebf7
Anna Neyzberg <anna@carbonfive.com>
2018-03-21 18:08:18 -0700

    wip;

--- packages/react/src/elements/components/Notification/index.js
+++ packages/react/src/elements/components/Notification/index.js
@@ -1,31 +1,62 @@
 import React, { Component } from "react";
+import Transition from "react-transition-group/Transition";
 import PropTypes from "prop-types";
 import cx from "classnames";
 import IconButton from "../IconButton/IconButton";
 import RichText from "../RichText";
 
 export default class Notification extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      in: false
+    };
+  }
   componentDidMount() {}
 
   onClickCapture(event) {
     if (event.target.event.target.tagName === "A") {
       this.props.onLinkClick(this.props.id);
     }
-    console.log("event", event);
-    console.log("event target", event.target);
+  }
+
+  dismissFeaturedNotification(){
+    this.props.dismissFeaturedNotification();
+    this.setState({in: false});
   }
 
   render() {
     return (
-      <div className="hig__notification" onClickCapture={this.onClickCapture}>
-        <div className="hig__notification__unread-marker" />
-        <div className="hig__notification__content">
-          <RichText>{this.props.content}</RichText>
-          <IconButton icon="close-notification" type="flat" />
-        </div>
+      <div>
+        { this.props.featuredNotification
+          ?
+          <Transition in={this.setState.inProp} >
+            <div className="hig__notification" onClickCapture={this.onClickCapture}>
+                <div className="hig__notification__unread-marker" />
+                <div className="hig__notification__content">
+                  <RichText>{this.props.content}</RichText>
+                  <IconButton onClick={this.dismissFeaturedNotification} icon="close-notification" type="flat" />
+                </div>
 
-        {/* <Timestamp /> */}
+                {/* <Timestamp /> */}
+              </div>
+          </Transition>
+          : <div className="hig__notification" onClickCapture={this.onClickCapture}>
+              <div className="hig__notification__unread-marker" />
+              <div className="hig__notification__content">
+                <RichText>{this.props.content}</RichText>
+              </div>
+
+              {/* <Timestamp /> */}
+            </div>
+        }
       </div>
+
+
+
+
+      }
+
     );
   }
 }
