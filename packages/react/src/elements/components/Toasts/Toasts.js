1e2b59c3b0683a58032fa9c9c8554950662f3fb7
Eric Fung <ericf@carbonfive.com>
2018-03-22 15:10:04 -0700

    Initial container to manage visibility of individual items

---
+++ packages/react/src/elements/components/Toasts/Toasts.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default class Toasts extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: React.Children.toArray(this.props.children)
+    };
+  }
+
+  handleDismiss = key => {
+    const childIndexToRemove = this.state.items.findIndex(el => el.key === key);
+
+    if (childIndexToRemove > -1) {
+      const newItems = this.state.items.slice();
+      newItems.splice(childIndexToRemove, 1);
+      this.setState({ items: newItems });
+    }
+  };
+
+  render() {
+    return (
+      <div className="hig__toasts">
+        {this.state.items.map(child =>
+          React.cloneElement(child, {
+            onDismiss: this.handleDismiss.bind(this, child.key)
+          })
+        )}
+      </div>
+    );
+  }
+}
