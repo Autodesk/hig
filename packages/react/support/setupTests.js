5a923e7f220bf0487335cf8bfda220020142ea65
Morris Allison III <morris.allison@autodesk.com>
2018-03-22 11:35:48 -0700

    chore(hig-react): Add assertions for constants
    * Add jest-extended
    * Add constants matchers

--- packages/react/support/setupTests.js
+++ packages/react/support/setupTests.js
@@ -1,3 +1,5 @@
 import "jest-enzyme"; // eslint-disable-line import/no-extraneous-dependencies
+import "jest-extended";
+import "./test/matchers/toBeAnObjectOfConstants";
 import "./test/matchers/toImplementHIGInterfaceOf";
 import "./test/matchers/toHavePropTypesAndDocgenInfo";
