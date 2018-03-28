874032e5521da6bf6147e3e9e6f35c6dba7de740
Anna Neyzberg <anna@carbonfive.com>
2018-03-27 17:01:36 -0700

    React(RichText) - update richtext to allow for specifying size

--- packages/react/src/elements/components/RichText/index.js
+++ packages/react/src/elements/components/RichText/index.js
@@ -3,11 +3,12 @@ import PropTypes from "prop-types";
 import cx from "classnames";
 import "./rich-text.scss";
 
-export const _AVAILABLE_SIZES = ["small", "large"];
+export const _AVAILABLE_SIZES = Object.freeze(["small", "large"]);
 
+const COMPONENT_CLASS = "hig__rich-text";
 export default class RichText extends Component {
   render() {
-    const richTextClasses = cx("hig__rich-text", {
+    const richTextClasses = cx(`${COMPONENT_CLASS}`, {
       [`hig__rich-text--${this.props.size}`]: this.props.size
     });
     return this.props.children ? (
@@ -27,7 +28,7 @@ RichText.propTypes = {
    */
   children: PropTypes.node,
   /**
-   * HTML string to be rendered and styled with HIG typography rules
+   * HTML string to be rendered and styled with HIG typography ruless
    */
   dangerouslySetInnerHTML: PropTypes.shape({
     __html: PropTypes.string
