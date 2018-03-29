0cf14b7e56648aac5f75970341d5d8b8ae707a4a
Anna Neyzberg <anna@carbonfive.com>
2018-03-29 15:24:38 -0700

    update snapshots and gemini-tests

--- packages/react/src/elements/components/RichText/RichText.test.js
+++ packages/react/src/elements/components/RichText/RichText.test.js
@@ -1,56 +1,19 @@
-import renderer from "react-test-renderer";
 import React from "react";
-import { RichText } from "../../../hig-react";
+import { shallow } from "enzyme";
+import RichText from "./index";
 
 describe("RichText", () => {
-  it("default", () => {
-    const tree = renderer
-      .create(
-        <RichText>
-          The RichText component lets me include any React children, including:
-          <ul>
-            <li>
-              <strong>Any old HTML tag</strong>
-            </li>
-            <li>React components</li>
-          </ul>
-        </RichText>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+  describe("sizes", () => {
+    it("adds the appropriate size class", () => {
+      const withoutSize = shallow(<RichText>Rich content</RichText>);
+      expect(withoutSize.find(".hig__rich-text")).not.toHaveClassName(
+        "hig__rich-text--small"
+      );
 
-  it("renders correctly with prop size small", () => {
-    const tree = renderer
-      .create(
-        <RichText size="small">
-          The RichText component lets me include any React children, including:
-          <ul>
-            <li>
-              <strong>Any old HTML tag</strong>
-            </li>
-            <li>React components</li>
-          </ul>
-        </RichText>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it("renders correctly with prop size large", () => {
-    const tree = renderer
-      .create(
-        <RichText size="large">
-          The RichText component lets me include any React children, including:
-          <ul>
-            <li>
-              <strong>Any old HTML tag</strong>
-            </li>
-            <li>React components</li>
-          </ul>
-        </RichText>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+      const withSize = shallow(<RichText size="small">Rich content</RichText>);
+      expect(withSize.find(".hig__rich-text")).toHaveClassName(
+        "hig__rich-text--small"
+      );
+    });
   });
 });
