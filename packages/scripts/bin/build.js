6510235c5fe951d91a47db427f6c88d506f8a0a8
Sean Durham <sean@sdurham.net>
2018-03-30 16:21:37 -0700

    Remove dead code from build script

--- packages/scripts/bin/build.js
+++ packages/scripts/bin/build.js
@@ -2,25 +2,13 @@
 
 const rollup = require('rollup');
 const babel = require('rollup-plugin-babel');
-// const string = require('rollup-plugin-string');
 const postcss = require('rollup-plugin-postcss');
-// const svg = require('rollup-plugin-svg');
-// const rootImport = require('rollup-plugin-root-import');
-// const resolve = require('rollup-plugin-node-resolve');
-// const commonjs = require('rollup-plugin-commonjs');
 const json = require('rollup-plugin-json');
 const postcssFuncitons = require('postcss-functions');
 
 const inputOptions = {
   input: 'src/index.js',
   plugins: [
-    // resolve(),
-    // commonjs(),
-    // rootImport({
-    //   root: `${__dirname}/src`,
-    //   useEntry: 'prepend',
-    //   extensions: '.js'
-    // }),
     babel({
       exclude: ['node_modules/**', '**/*.scss', '**/*.html', '**/*.svg', '**/*.json'],
       "presets": [
@@ -43,7 +31,6 @@ const inputOptions = {
       output: "build/index.css",
       plugins: [postcssFuncitons]
     }),
-    // svg()
   ]
 };
 
@@ -54,18 +41,11 @@ const esModulesOutputOptions = {
   external: ['hig-interface', 'mustache', 'i18next']
 };
 
-// const browserFriendlyOutputOptions = {
-//   name: 'HIG',
-//   file: 'build/index.js',
-//   format: 'cjs'
-// };
-
 async function build() {
   let bundle;
   try {
     bundle = await rollup.rollup(inputOptions);
     bundle.write(esModulesOutputOptions);
-    // bundle.write(browserFriendlyOutputOptions);
   } catch (e) {
     console.log(e);
   }
