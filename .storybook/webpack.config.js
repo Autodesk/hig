992fd167b8a48a76225465a95ab053e74a408b00
Eric Fung <ericf@carbonfive.com>
2018-04-03 15:16:03 -0700

    Copy storybook config to root

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
+        loader: require.resolve("file-loader"),
+        query: {
+          name: "static/media/[name].[hash:8].[ext]"
+        }
+      }
+    ]
+  }
+};
