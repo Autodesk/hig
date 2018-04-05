f007d4b7f1da9bc125f1dc109708836f70b9be49
Eric Fung <ericf@carbonfive.com>
2018-04-05 13:10:09 -0700

    Copy initial Gemini config

---
+++ packages/storybook/.gemini.js
@@ -0,0 +1,34 @@
+const dotenv = require("dotenv");
+
+dotenv.config({ path: ".env" });
+
+module.exports = {
+  rootUrl: "http://localhost:9876",
+  gridUrl: "http://ondemand.saucelabs.com/wd/hub",
+  screenshotsDir: "./gemini/screens",
+  compositeImage: true,
+  sessionsPerBrowser: 3,
+  system: {
+    plugins: {
+      sauce: {
+        username: process.env.SAUCE_USERNAME,
+        accessKey: process.env.SAUCE_ACCESS_KEY
+      }
+    }
+  },
+  browsers: {
+    chrome: {
+      desiredCapabilities: {
+        browserName: "chrome",
+        version: "65.0",
+        platform: "macOs 10.12",
+        name: "gemini visual regression tests"
+      }
+    }
+  },
+  sets: {
+    chrome: {
+      files: []
+    }
+  }
+};
