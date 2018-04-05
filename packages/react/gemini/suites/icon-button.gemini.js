77729684991de57040fd894c6ff703ff16b7e7be
Eric Fung <ericf@carbonfive.com>
2018-04-05 13:48:31 -0700

    Update CircleCI to use relocated storybook-test

--- packages/react/gemini/suites/icon-button.gemini.js
+++
@@ -1,6 +0,0 @@
-gemini.suite("IconButton", suite => {
-  suite
-    .setUrl("iframe.html?selectedKind=IconButton&selectedStory=default")
-    .setCaptureElements(".hig__icon-button")
-    .capture("Default");
-});
--- packages/react/gemini/suites/notifications.gemini.js
+++
@@ -1,6 +0,0 @@
-gemini.suite("Notifications", suite => {
-  suite
-    .setUrl("iframe.html?selectedKind=Notifications&selectedStory=V1")
-    .setCaptureElements(".hig__flyout__panel")
-    .capture("Default");
-});
