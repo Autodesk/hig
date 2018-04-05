e3e8a6445063fa88665286fc05e1b505e0c09ffd
Eric Fung <ericf@carbonfive.com>
2018-04-05 10:51:29 -0700

    Clean up first pass

--- .storybook/development/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from "@storybook/react";
-import "../shared/config";
-
-const req = require.context("../../packages", true, /stories.js$/);
-function loadStories() {
-  req.keys().forEach(req);
-}
-
-configure(loadStories, module);
--- .storybook/shared/config.js
+++
@@ -1,6 +0,0 @@
-import { addDecorator } from "@storybook/react";
-import { withInfo } from "@storybook/addon-info";
-import { withKnobs } from "@storybook/addon-knobs/react";
-
-addDecorator((story, context) => withInfo({ header: true })(story)(context));
-addDecorator(withKnobs);
