eea0e09382dbde919e9aca8332a009832a805ba8
Morris Allison III <morris.allison@autodesk.com>
2018-04-11 15:50:46 -0700

    test(themes): Add test for default value

--- packages/themes/src/ThemeContext/ThemeContext.test.js
+++ packages/themes/src/ThemeContext/ThemeContext.test.js
@@ -1,30 +1,38 @@
 import { mount } from "enzyme";
 import React from "react";
 
+import HIGLightTheme from "../themes/HIGLightTheme";
+
 import ThemeContext from "./index";
 
 const TestTheme = {
-  themeId: 'test-theme',
-  themeClass: 'hig--test-theme'
-}
+  themeId: "test-theme",
+  themeClass: "hig--test-theme"
+};
 
 describe("ThemeContext", () => {
   describe("with a provided theme", () => {
-    let renderFunction = jest.fn();
-    renderFunction.mockReturnValue(null);
-
-    let wrapper;
+    const renderFunction = jest.fn();
 
     beforeEach(() => {
-      wrapper = mount(
+      renderFunction.mockReset();
+      renderFunction.mockReturnValue(null);
+    });
+
+    it("provides the theme to the consumer", () => {
+      mount(
         <ThemeContext.Provider value={TestTheme}>
           <ThemeContext.Consumer>{renderFunction}</ThemeContext.Consumer>
         </ThemeContext.Provider>
       );
+
+      expect(renderFunction).toHaveBeenCalledWith(TestTheme);
     });
 
-    it(`provides the theme to the consumer`, () => {
-      expect(renderFunction).toHaveBeenCalledWith(TestTheme);
+    it("provides the default theme without a provider", () => {
+      mount(<ThemeContext.Consumer>{renderFunction}</ThemeContext.Consumer>);
+
+      expect(renderFunction).toHaveBeenCalledWith(HIGLightTheme);
     });
   });
 });
