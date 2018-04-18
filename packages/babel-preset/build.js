1ef7a9d2fbb9be46afb94cf629ae7418980ca108
Morris Allison III <morris.allison@autodesk.com>
2018-04-18 12:21:01 -0700

    refactor(babel-preset): Consolidate Babel configs

---
+++ packages/babel-preset/build.js
@@ -0,0 +1,26 @@
+module.exports = function createBuildPreset() {
+  return {
+    exclude: [
+      "**/node_modules/**",
+      "**/*.css",
+      "**/*.scss",
+      "**/*.html",
+      "**/*.svg",
+      "**/*.json"
+    ],
+    presets: [
+      [
+        "env",
+        {
+          modules: false,
+          targets: {
+            browser: ">1%"
+          }
+        }
+      ],
+      "stage-2",
+      "react"
+    ],
+    plugins: ["react-docgen"]
+  };
+};
