e05ebfaba1a0cd73774ec4174589a6264471483d
Eric Fung <ericf@carbonfive.com>
2018-05-16 14:54:01 -0700

    docs(checkbox): Add knobs to Checkbox story

--- packages/checkbox/src/__stories__/Checkbox.stories.js
+++ packages/checkbox/src/__stories__/Checkbox.stories.js
@@ -1,30 +1,18 @@
-import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
-import Checkbox from "../index";
-import infoOptions from "./infoOptions";
 
-storiesOf("Checkbox", module)
-  .add("default", withInfo(infoOptions)(() => <Checkbox label="Default" />))
-  .add(
-    "required",
-    withInfo(infoOptions)(() => (
-      <Checkbox label="Required" required="You must check this box" />
-    ))
-  )
-  .add(
-    "disabled",
-    withInfo(infoOptions)(() => <Checkbox label="Disabled" disabled />)
-  )
-  .add(
-    "disabled and checked",
-    withInfo(infoOptions)(() => (
-      <Checkbox label="Disabled and Checked" disabled checked />
-    ))
-  )
-  .add(
-    "indeterminate",
-    withInfo(infoOptions)(() => (
-      <Checkbox label="Indeterminate" indeterminate />
-    ))
+import infoOptions from "./infoOptions";
+import renderStory from "./renderStory";
+import stories from "./stories";
+
+const storybook = storiesOf("Checkbox", module);
+
+stories.forEach(({ description, getProps }) => {
+  storybook.add(
+    description,
+    withInfo(infoOptions)(() => {
+      const props = getProps();
+      return renderStory(props);
+    })
   );
+});
