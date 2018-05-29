79702e7f7c49099d630e8bb6af9d4c96617dd8be
Eric Fung <ericf@carbonfive.com>
2018-05-29 14:31:34 -0700

    test(text-field): Add tests around event handlers

--- packages/text-field/src/TextField.test.js
+++ packages/text-field/src/TextField.test.js
@@ -4,6 +4,24 @@ import TextField from "./index";
 import Input from "./presenters/Input";
 
 describe("TextField", () => {
+  it("allows value to be changed by onChange", () => {
+    const wrapper = mount(<TextField value="foo" />);
+    const input = wrapper.find("input");
+
+    expect(input.prop("value")).toEqual("foo");
+    input.simulate("change", { target: { value: "bar" } });
+    expect(input.prop("value")).toEqual("bar");
+  });
+
+  it("ignores changes to the value prop", () => {
+    const wrapper = mount(<TextField value="foo" />);
+    const input = wrapper.find("input");
+
+    expect(input.prop("value")).toEqual("foo");
+    wrapper.setProps({ value: "baz" });
+    expect(input.prop("value")).toEqual("foo");
+  });
+
   describe("id", () => {
     it("passes the ID prop to the underlying input", () => {
       const wrapper = mount(<TextField />);
@@ -15,6 +33,29 @@ describe("TextField", () => {
     it("generates an ID when one is not provided", () => {
       const wrapper = mount(<TextField id="important-field" />);
       expect(wrapper.find(Input).prop("id")).toEqual("important-field");
+    });
+  });
+
+  describe("event handlers", () => {
+    it("calls onChange when provided", () => {
+      const eventHandler = jest.fn();
+      const wrapper = mount(<TextField onChange={eventHandler} />);
+      wrapper.find("input").simulate("change", { target: { value: "foo" } });
+
+      expect(eventHandler).toHaveBeenCalled();
+    });
+
+    it("calls onClearButtonClick when provided", () => {
+      const eventHandler = jest.fn();
+      const wrapper = mount(
+        <TextField
+          value="Foo"
+          onClearButtonClick={eventHandler}
+          showClearButton
+        />
+      );
+      wrapper.find(".hig__text-field__clear button").simulate("click");
+      expect(eventHandler).toHaveBeenCalled();
     });
   });
 
