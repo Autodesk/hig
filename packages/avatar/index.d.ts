30526b93005d63967fc882a3a888edd1b02d72b8
Curtis Schlak <34490321+hr-curtissimo@users.noreply.github.com>
2018-06-15 07:37:28 -0400

    Comment the declarations file.

--- packages/avatar/index.d.ts
+++ packages/avatar/index.d.ts
@@ -1,19 +1,28 @@
-import React from 'react';
+import * as React from 'react';
 
-declare module '@hig/avatar' {
-  export interface IAvatarProps {
-    name: string;
-    size: string;
-    image?: string;
-  }
-  export enum sizes {
-    SMALL_16 = "small",
-    MEDIUM_24 = "medium",
-    MEDIUM_32 = "medium-32",
-    LARGE_36 = "large",
-    LARGE_48 = "large-48",
-    XLARGE_64 = "extralarge"
-  }
-  class Avatar extends React.Component<IAvatarProps> {}
-  export default Avatar;
+/**
+ * The availabe sizes for the Avatar component.
+ */
+export enum sizes {
+  SMALL_16 = "small",
+  MEDIUM_24 = "medium",
+  MEDIUM_32 = "medium-32",
+  LARGE_36 = "large",
+  LARGE_48 = "large-48",
+  XLARGE_64 = "extralarge"
 }
+
+/**
+ * The properties for the Avatar component.
+ */
+export interface IAvatarProps {
+  name: string;
+  size: string;
+  image?: string;
+}
+
+/**
+ * The Avatar component.
+ */
+class Avatar extends React.Component<IAvatarProps> {}
+export default Avatar;
