8d76b91de8d80efffbd4a243f10526c8d6aceb78
Morris Allison III <morris.allison@autodesk.com>
2018-06-14 23:56:03 -0700

    chore(ci): Ensure branch is up-to-date

--- scripts/update-development.sh
+++ scripts/update-development.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git fetch origin
+git pull --ff-only origin "master:remotes/origin/master"
 git checkout development
 git merge master --no-ff -m "chore(release): Merge master into development [skip ci]"
 git push origin development
