f03640816d45dcb9721bbac4f744707dc669821f
Morris Allison III <morris.allison@autodesk.com>
2018-06-19 14:21:02 -0700

    feat(flyout): Rewrite Flyout component
    * Removed `hig-react` dependency
    * Improved positioning functionality
    * More consistent transition behavior

--- packages/flyout/src/__stories__/renderStory.js
+++ packages/flyout/src/__stories__/renderStory.js
@@ -1,9 +1,29 @@
+/* eslint-disable react/prop-types */
 import React from "react";
-import DefaultExport from "../index";
+import Flyout from "../index";
 import getKnobs from "./getKnobs";
+
+function Wrapper({ children }) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "300px",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      {children}
+    </div>
+  );
+}
 
 export default function renderStory(props) {
   const { children, ...otherProps } = getKnobs(props);
 
-  return <DefaultExport {...otherProps}>{children}</DefaultExport>;
+  return (
+    <Wrapper>
+      <Flyout {...otherProps}>{children}</Flyout>
+    </Wrapper>
+  );
 }
