d8c5097f8fe69a035595aca35cae6ba59b189cbb
Sean Durham <sean@sdurham.net>
2018-07-05 11:21:25 -0700

    feat(themability): ThemeContext provides theme data

--- packages/themes/src/themes/HIGDarkBlueTheme.js
+++ packages/themes/src/themes/HIGDarkBlueTheme.js
@@ -1,6 +1,10 @@
+import darkBlueThemeData from "@hig/theme-data-poc/build/darkBlueTheme.json";
+
 const HIGDarkBlueTheme = {
+  name: "Dark Blue",
   themeId: "hig-dark-blue",
-  themeClass: "hig--dark-blue-theme"
+  themeClass: "hig--dark-blue-theme",
+  data: darkBlueThemeData
 };
 
 export default HIGDarkBlueTheme;
---
+++ packages/themes/src/themes/HIGLightGrayTheme.js
@@ -0,0 +1,10 @@
+import lightGrayThemeData from "@hig/theme-data-poc/build/lightGrayTheme.json";
+
+const HIGLightGrayTheme = {
+  name: "Light Gray",
+  themeId: "hig-light-gray",
+  themeClass: "hig--light-gray-theme",
+  data: lightGrayThemeData
+};
+
+export default HIGLightGrayTheme;
