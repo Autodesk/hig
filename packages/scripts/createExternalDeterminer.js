7942d6d2437fcb064ea27fe87cc913534b590844
Morris Allison III <morris.allison@autodesk.com>
2018-07-05 10:18:04 -0700

    fix(build): include CSS modules in bundle

--- packages/scripts/createExternalDeterminer.js
+++ packages/scripts/createExternalDeterminer.js
@@ -1,18 +1,15 @@
 /**
  * @param {string[]} externalDependencies dependencies and peerDependencies
- * @returns {Function} A function to use for `rollupConfig.inputOptions.external`
+ * @returns {function(string): boolean} A function to use for `rollupConfig.inputOptions.external`
  */
 module.exports = function createExternalDeterminer(externalDependencies) {
-  /**
-   * Determines whether the given module is external
-   * @param {string} moduleName
-   * @returns {boolean}
-   */
   return function external(moduleName) {
-    return Boolean(
-      externalDependencies.find(dependencyName =>
+    const isCssModule = moduleName.endsWith(".css");
+    const isPackageDependency =
+      externalDependencies.findIndex(dependencyName =>
         moduleName.startsWith(dependencyName)
-      )
-    );
+      ) >= 0;
+
+    return !isCssModule && isPackageDependency;
   };
 };
