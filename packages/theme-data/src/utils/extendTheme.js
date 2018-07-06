86971f9b8dea820488b661cf8cb5d0d0419ae5f9
Sean Durham <sean@sdurham.net>
2018-07-05 18:54:21 -0700

    Remove unneeded test

--- packages/theme-data/src/utils/extendTheme.js
+++ packages/theme-data/src/utils/extendTheme.js
@@ -1,12 +1,3 @@
-function validateNoUnknownKeys(theme, extension) {
-  const themeKeys = Object.keys(theme);
-  const extensionKeys = Object.keys(extension);
-  const unknownKeys = extensionKeys.filter(key => !themeKeys.includes(key));
-  if (unknownKeys.length > 0) {
-    throw new Error(`Keys ${unknownKeys.join(", ")} are not in the theme`);
-  }
-}
-
 /**
  * extendTheme
  * @param {*} theme
@@ -14,9 +5,6 @@ function validateNoUnknownKeys(theme, extension) {
  *
  * Add additional roles to a theme
  */
-export default function extendTheme(theme, extension, { strict = false } = {}) {
-  if (strict) {
-    validateNoUnknownKeys(theme, extendTheme);
-  }
+export default function extendTheme(theme, extension) {
   return { ...theme, ...extension };
 }
