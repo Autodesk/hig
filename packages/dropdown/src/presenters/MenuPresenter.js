9bd971ed3e4b262dabcf17eb65dad4688a91cb38
Morris Allison III <morris.allison@autodesk.com>
2018-07-12 16:06:51 -0700

    feat(a11y): upgrade Downshift to apply Aria attributes to the option menu

--- packages/dropdown/src/presenters/MenuPresenter.js
+++ packages/dropdown/src/presenters/MenuPresenter.js
@@ -1,17 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 import "./MenuPresenter.scss";
 
-export default class MenuPresenter extends Component {
-  static propTypes = {
-    /**
-     * Options
-     */
-    children: PropTypes.node
-  };
-
-  render() {
-    return <div className="hig__dropdown-v1__menu">{this.props.children}</div>;
-  }
+export default function MenuPresenter({ children, ...otherProps }) {
+  return (
+    <div className="hig__dropdown-v1__menu" {...otherProps}>
+      {children}
+    </div>
+  );
 }
+
+MenuPresenter.propTypes = {
+  children: PropTypes.node
+};
