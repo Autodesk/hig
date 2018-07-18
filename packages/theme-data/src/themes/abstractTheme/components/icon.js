ec2ed7595a8efa79157c89bda0765eac7b235816
Sean Durham <sean@sdurham.net>
2018-07-17 19:54:00 -0700

    feat: add roles for input, text area, form field, checkbox, and icon

---
+++ packages/theme-data/src/themes/abstractTheme/components/icon.js
@@ -0,0 +1,3 @@
+export default {
+  "ICON.WIDTH": "24px"
+};
--- packages/theme-data/src/themes/darkBlueTheme/config.js
+++ packages/theme-data/src/themes/darkBlueTheme/config.js
@@ -3,6 +3,7 @@ import mapKeys from "../../utils/mapKeys";
 import system from "./system";
 import { config as abstractThemeConfig } from "../abstractTheme";
 import { config as mediumDensityThemeConfig } from "../mediumDensityTheme";
+import formField from "./components/formField";
 import input from "./components/input";
 import label from "./components/label";
 
@@ -12,6 +13,7 @@ const darkBlueThemeConfig = extendTheme(
     {},
     mediumDensityThemeConfig,
     mapKeys(system.colorScheme, key => `COLOR_SCHEME.${key}`),
+    formField,
     input,
     label
   )
