bd0173aee0c9ac8d444e35c6c6b97495b49ddc08
Morris Allison III <morris.allison@autodesk.com>
2018-08-06 01:52:24 -0700

    test: remove broken test

--- packages/text-area/src/__gemini__/TextAreaPresenter.gemini.js
+++ packages/text-area/src/__gemini__/TextAreaPresenter.gemini.js
@@ -4,10 +4,6 @@ gemini.suite("TextAreaPresenter", () => {
       .setUrl("iframe.html?selectedKind=TextArea&selectedStory=default")
       .setCaptureElements(".storybook-component")
       .capture("unfocused without input")
-      // Small differences in placeholder text can easily generate false negatives
-      .capture("focused without input", { tolerance: 5 }, actions => {
-        actions.focus("textarea");
-      })
       .capture("focused with input", (actions, find) => {
         actions.sendKeys(find("textarea"), "Foobar");
       })
