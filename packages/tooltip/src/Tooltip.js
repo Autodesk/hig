3ce54e63c5670daa1cd9f24a161b6b744848c2cf
ziweiwu <ziwei.wu@autodesk.com>
2018-08-13 16:54:12 -0400

    fixes(tooltip): updated hover flicking issues

--- packages/tooltip/src/Tooltip.js
+++ packages/tooltip/src/Tooltip.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import ReactToolTip from 'rc-tooltip';
-import {anchorPoints} from "@hig/flyout";
-import {css} from 'react-emotion'
-import './tooltip.css';
+import React from "react";
+import PropTypes from "prop-types";
+import ReactToolTip from "rc-tooltip";
+import { polyfill } from "react-lifecycles-compat";
+import { anchorPoints } from "@hig/flyout";
+import { css } from "react-emotion";
+import "./tooltip.scss";
 
 const placements = {
   [anchorPoints.TOP_CENTER]: "top",
@@ -18,72 +18,91 @@ const placements = {
   [anchorPoints.LEFT_BOTTOM]: "leftBottom",
   [anchorPoints.RIGHT_CENTER]: "right",
   [anchorPoints.RIGHT_TOP]: "rightTop",
-  [anchorPoints.RIGHT_BOTTOM]: "rightBottom",
+  [anchorPoints.RIGHT_BOTTOM]: "rightBottom"
 };
 
-export default class Tooltip extends React.Component {
+class Tooltip extends React.Component {
   static propTypes = {
-    trigger: PropTypes.string,
-    children: PropTypes.oneOfType([PropTypes.node, PropTypes.func,]).isRequired,
-    content: PropTypes.oneOfType([PropTypes.node, PropTypes.func,]),
-    mouseEnterDelay: PropTypes.number,
-    mouseLeaveDelay: PropTypes.number,
-    id: PropTypes.string,
-    title: PropTypes.string,
+    anchorPoint: PropTypes.string,
+    children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]).isRequired,
+    content: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
     description: PropTypes.string,
+    id: PropTypes.string,
+    isVisible: PropTypes.bool,
     linkTitle: PropTypes.string,
     linkURL: PropTypes.string,
-    anchorPoint: PropTypes.string,
-    width: PropTypes.number,
-    lightTheme: PropTypes.bool,
+    mouseEnterDelay: PropTypes.number,
+    mouseLeaveDelay: PropTypes.number,
+    title: PropTypes.string,
+    trigger: PropTypes.string,
+    width: PropTypes.number
   };
 
   static defaultProps = {
-    prefixCls: 'hig-tooltip',
-    mouseEnterDelay: 1,
-    destroyTooltipOnHide: true,
-    mouseLeaveDelay: 0.2,
-    anchorPoint: placements[anchorPoints.RIGHT_CENTER],
-    trigger: 'click',
-    arrowContent: null,
-    width: 300,
-    lightTheme: false,
+    anchorPoint: placements[anchorPoints.BOTTOM_CENTER],
+    mouseEnterDelay: 0.5,
+    mouseLeaveDelay: 0.1,
+    trigger: "hover",
+    width: 300
   };
 
-  render() {
-    const props = this.props;
-    // use react emotion to create a styled wrapper with props.width
-    const tooltipWidth = css`
-      max-width: ${props.width}px;
-    `;
+  renderTitle = () => (
+    <div className="hig__tooltip-title">{this.props.title}</div>
+  );
 
-    const tooltipTheme = classNames({
-      'hig-tooltip-light': this.props.lightTheme,
-      'hig-tooltip-dark': !this.props.lightTheme
-    });
+  renderDescription = () => (
+    <div className="hig__tooltip-description">{this.props.description}</div>
+  );
+
+  renderContent = () => (
+    <div className="hig__tooltip-inner-content">{this.props.content}</div>
+  );
+
+  renderURL = () => (
+    <div className="hig__tooltip-link">
+      {!this.props.content && <div className="line" />}
+      <a href={this.props.linkURL}>
+        {this.props.linkTitle || this.props.linkURL}
+      </a>
+    </div>
+  );
+
+  renderContainer = () => (
+    <div className="hig__tooltip-container">
+      {this.props.title ? this.renderTitle() : null}
+      {this.props.description ? this.renderDescription() : null}
+      {this.props.content ? this.renderContent() : null}
+      {this.props.linkURL ? this.renderURL() : null}
+    </div>
+  );
+
+  renderChildren = () => (
+    <div className="hig__tooltip-children-wrapper">{this.props.children}</div>
+  );
+
+  render() {
+    const { anchorPoint, trigger, isVisible, ...otherProps } = this.props;
+
+    const tooltipWidth = css`
+      max-width: ${this.props.width}px;
+    `;
 
     return (
       <ReactToolTip
-        {...props}
+        destroyTooltipOnHide
+        isVisible={isVisible}
+        overlay={this.renderContainer}
+        overlayClassName={tooltipWidth}
+        placement={placements[anchorPoint]}
+        prefixCls="hig__tooltip"
         transitionName="fade"
-        overlayClassName={tooltipWidth + ' ' + tooltipTheme}
-        placement={placements[props.anchorPoint]}
-        children={<div className="hig-tooltip-children-wrapper">{props.children}</div>}
-        overlay={
-          <div className="hig-tooltip-container">
-            {props.title && <div className="hig-tooltip-title">{props.title}</div>}
-            {props.description && <div className="hig-tooltip-description">{props.description}</div>}
-            {props.content && <div className="hig-tooltip-inner-content">{props.content}</div>}
-            {props.linkURL && <div className="hig-tooltip-link">
-              {!props.content && <hr/>}
-              <a
-                href={props.linkURL}>
-                {props.linkTitle || props.linkURL}
-              </a>
-            </div>}
-          </div>
-        }
-      />
+        trigger={trigger}
+        {...otherProps}
+      >
+        {this.renderChildren()}
+      </ReactToolTip>
     );
   }
 }
+
+export default polyfill(Tooltip);
