896d886d4580b98e794d7ad3ad5dcfee5100c8f1
Ari Russo <ari.russo@autodesk.com>
2018-08-13 15:59:41 -0400

    test: Add markup snapshots for Dropdown presenters

---
+++ packages/dropdown/src/presenters/MenuPresenter.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { takeSnapshotsOf } from "@hig/jest-preset/helpers";
+
+import MenuPresenter from "./MenuPresenter";
+
+describe("Dropdown/presenters/MenuPresenter", () => {
+  const rendererOptions = {
+    createNodeMock(element) {
+      return document.createElement(element.type);
+    }
+  };
+  const cases = [
+    {
+      desc: "renders with only required props",
+      props: {
+        innerRef: () => {}
+      }
+    },
+    {
+      desc: "renders with all props",
+      props: {
+        innerRef: () => {},
+        isOpen: true,
+        children: <p key="p">hi</p>
+      }
+    }
+  ];
+
+  takeSnapshotsOf(MenuPresenter, cases, rendererOptions);
+});
