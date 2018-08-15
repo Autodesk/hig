0b91be937d286d26970281ecf2effad79002d30b
Sean Durham <sean@sdurham.net>
2018-08-15 12:36:26 -0700

    test: add gemini tests for each theme
    revert unintentional package change

---
+++ packages/input/src/__gemini__/Input.gemini.js
@@ -0,0 +1,36 @@
+const themes = [
+  {
+    name: "Light gray medium density",
+    url:
+      "iframe.html?selectedKind=Input&selectedStory=Light%20gray%2C%20medium%20density"
+  },
+  {
+    name: "Light gray high density",
+    url:
+      "iframe.html?selectedKind=Input&selectedStory=Light%20gray%2C%20high%20density"
+  },
+  {
+    name: "Dark blue medium density",
+    url:
+      "iframe.html?selectedKind=Input&selectedStory=Dark%20blue%2C%20medium%20density"
+  },
+  {
+    name: "Dark blue high density",
+    url:
+      "iframe.html?selectedKind=Input&selectedStory=Dark%20blue%2C%20high%20density"
+  },
+  {
+    name: "Web light",
+    url: "iframe.html?selectedKind=Input&selectedStory=Web%20light"
+  }
+];
+
+gemini.suite("Input", parent => {
+  parent.setCaptureElements(".storybook-component");
+
+  themes.forEach(({ name, url }) => {
+    gemini.suite(name, child => {
+      child.setUrl(url).capture(name);
+    });
+  });
+});
