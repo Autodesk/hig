5db5cd909ff87b3ab620497c365bab0d1f58ebb0
Sean Durham <sean@sdurham.net>
2018-08-27 10:28:02 -0700

    Move source files into src dir

--- packages/style-dictionary/scripts/build.js
+++ packages/style-dictionary/scripts/build.js
@@ -1,4 +1,9 @@
-const StyleDictionary = require('style-dictionary');
+const StyleDictionary = require("style-dictionary");
+const colorTransformAlpha = require("../src/transforms/color-transform-alpha");
+const componentAttributeTransformer = require("../src/transforms/attributes/components-cti");
+
+StyleDictionary.registerTransform(componentAttributeTransformer);
+StyleDictionary.registerTransform(colorTransformAlpha);
 
 const colorSchemes = ["light-gray", "dark-blue"];
 const densities = ["medium-density", "high-density"];
@@ -7,37 +12,46 @@ colorSchemes.forEach(colorScheme => {
   densities.forEach(density => {
     StyleDictionary.extend({
       source: [
-        "properties/basics/*.json",
-        `themes/${colorScheme}.json`,
-        `themes/${density}.json`,
-        "properties/components/*.json"
+        "src/properties/basics/*.json",
+        `src/themes/${colorScheme}.json`,
+        `src/themes/${density}.json`,
+        "src/properties/components/*.json"
       ],
       platforms: {
-        scss: {
-          transformGroup: "scss",
-          buildPath: "build/scss/",
-          files: [{
-            destination: `${colorScheme}-${density}-variables.scss`,
-            format: "scss/variables"
-          }]
-        },
-        js: {
-          transformGroup: "js",
-          buildPath: "build/js/",
-          files: [{
-            name: "__style_properties",
-            destination: `${colorScheme}-${density}-properties.js`,
-            format: "javascript/es6"
-          }]
-        },
+        // scss: {
+        //   transforms: ["attribute/component-cti"],
+        //   // transformGroup: "scss",
+        //   buildPath: "build/scss/",
+        //   files: [
+        //     {
+        //       destination: `${colorScheme}-${density}-variables.scss`,
+        //       format: "scss/variables"
+        //     }
+        //   ]
+        // },
+        // js: {
+        //   transforms: ["attribute/component-cti"],
+        //   transformGroup: "js",
+        //   buildPath: "build/js/",
+        //   files: [
+        //     {
+        //       name: "__style_properties",
+        //       destination: `${colorScheme}-${density}-properties.js`,
+        //       format: "javascript/es6"
+        //     }
+        //   ]
+        // },
         json: {
+          transforms: ["attribute/component-cti"],
           transformGroup: "web",
           buildPath: "build/json/",
-          files: [{
-            name: "__style_properties",
-            destination: `${colorScheme}-${density}-properties.json`,
-            format: "json"
-          }]
+          files: [
+            {
+              name: "__style_properties",
+              destination: `${colorScheme}-${density}-properties.json`,
+              format: "json"
+            }
+          ]
         }
       }
     }).buildAllPlatforms();
