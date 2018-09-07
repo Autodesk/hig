0ee1ff75af950d92daaf1ba7df2cee592f4bf8da
Morris Allison III <morris.allison@autodesk.com>
2018-09-07 13:07:58 -0700

    test: add Tooltip visual regression test

---
+++ packages/tooltip/src/__gemini__/Tooltip.gemini.js
@@ -0,0 +1,8 @@
+gemini.suite("Tooltip", () => {
+  gemini.suite("default", suite => {
+    suite
+      .setUrl("iframe.html?selectedKind=Tooltip&selectedStory=default")
+      .setCaptureElements('[data-capture="Tooltip"]')
+      .capture("Default");
+  });
+});
