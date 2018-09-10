3dafeffad8364d07e0fa0f28764b77a9a1b63b09
Alex Millatmal <alexandrajmh@gmail.com>
2018-09-10 17:52:17 -0400

    wip

--- packages/profile-flyout/src/__gemini__/ProfileFlyoutPresenter.stories-test.js
+++ packages/profile-flyout/src/__gemini__/ProfileFlyoutPresenter.stories-test.js
@@ -1,32 +1,12 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import Button from "@hig/button";
 import avatarImagePath from "@hig/storybook/storybook-support/fixtures/avatar/chris-reynolds.png";
 import ProfileFlyoutPresenter from "../presenters/ProfileFlyoutPresenter";
 import ProfileContent from "../presenters/ProfileContent";
 import "./ProfileFlyoutPresenter.stories-test.scss";
 
 function renderChildren() {
-  return (
-    <div className="hig__profile-flyout__content__links">
-      <div className={"hig__profile-flyout__content__link"}>
-        <Button
-          title={"Sign Out"}
-          type={"secondary"}
-          link={"https://www.autodesk.com/"}
-          size={"small"}
-        />
-      </div>
-      <div className={"hig__profile-flyout__content__link"}>
-        <Button
-          title={"Profile Settings"}
-          type={"secondary"}
-          link={"https://www.autodesk.com/"}
-          size={"small"}
-        />
-      </div>
-    </div>
-  );
+  return <p>Child content goes here.</p>;
 }
 
 function renderProfileContent() {
--- packages/profile-flyout/src/__stories__/stories.js
+++ packages/profile-flyout/src/__stories__/stories.js
@@ -1,29 +1,9 @@
 import React from "react";
-import Button from "@hig/button";
 import avatarImagePath from "@hig/storybook/storybook-support/fixtures/avatar/chris-reynolds.png";
 import ProfileContent from "../presenters/ProfileContent";
 
 function renderChildren() {
-  return (
-    <div className="hig__profile-flyout__content__links">
-      <div className={"hig__profile-flyout__content__link"}>
-        <Button
-          title={"Sign Out"}
-          type={"secondary"}
-          link={"https://www.autodesk.com/"}
-          size={"small"}
-        />
-      </div>
-      <div className={"hig__profile-flyout__content__link"}>
-        <Button
-          title={"Profile Settings"}
-          type={"secondary"}
-          link={"https://www.autodesk.com/"}
-          size={"small"}
-        />
-      </div>
-    </div>
-  );
+  return <p>Child content can go here.</p>;
 }
 
 function renderProfileContent() {
