55eba2369e10d03af4f20bd03abf1016fe7b2c4f
Morris Allison III <morris.allison@autodesk.com>
2018-09-17 15:43:47 -0700

    test(behavior): add tests for proper controlled behavior

--- packages/slider/src/Slider.test.js
+++ packages/slider/src/Slider.test.js
@@ -4,39 +4,6 @@ import Slider from "./index";
 import Input from "./presenters/Input";
 
 describe("slider/Slider", () => {
-  it("allows value to be changed by onChange", () => {
-    const wrapper = mount(<Slider defaultValue="10" />);
-    const input = wrapper.find("input");
-
-    expect(input.prop("value")).toEqual("10");
-    input.simulate("change", { target: { value: "20" } });
-    expect(input.prop("value")).toEqual("20");
-  });
-
-  describe("id", () => {
-    it("generates an ID when one is not provided", () => {
-      const wrapper = mount(<Slider />);
-      expect(wrapper.find(Input).prop("id")).toEqual(
-        expect.stringMatching("slider-")
-      );
-    });
-
-    it("passes the ID prop to the underlying input", () => {
-      const wrapper = mount(<Slider id="important-field" />);
-      expect(wrapper.find(Input).prop("id")).toEqual("important-field");
-    });
-  });
-
-  describe("event handlers", () => {
-    it("calls onChange when provided", () => {
-      const eventHandler = jest.fn();
-      const wrapper = mount(<Slider onChange={eventHandler} />);
-      wrapper.find("input").simulate("change", { target: { value: "10" } });
-
-      expect(eventHandler).toHaveBeenCalled();
-    });
-  });
-
   it("renders a label when provided", () => {
     const label = "Age";
     const wrapper = mount(<Slider label={label} />);
@@ -54,24 +21,68 @@ describe("slider/Slider", () => {
     const wrapper = mount(<Slider required={requiredMsg} />);
     expect(wrapper.text()).toEqual(expect.stringMatching(requiredMsg));
   });
+  describe("props", () => {
+    describe("id", () => {
+      it("generates an ID when one is not provided", () => {
+        const wrapper = mount(<Slider />);
+        expect(wrapper.find(Input).prop("id")).toEqual(
+          expect.stringMatching("slider-")
+        );
+      });
 
-  describe("when controlled", () => {
-    it("doesn't allow the value to be changed by onChange", () => {
-      const wrapper = mount(<Slider value="10" />);
-      const input = wrapper.find("input");
-
-      expect(input.prop("value")).toEqual("10");
-      input.simulate("change", { target: { value: "20" } });
-      expect(input.prop("value")).toEqual("10");
+      it("passes the ID prop to the underlying input", () => {
+        const wrapper = mount(<Slider id="important-field" />);
+        expect(wrapper.find(Input).prop("id")).toEqual("important-field");
+      });
     });
 
-    it("recognizes changes to the value prop", () => {
-      const wrapper = mount(<Slider value="10" />);
-      const input = wrapper.find("input");
+    describe("onChange", () => {
+      describe("when uncontrolled", () => {
+        it("calls the `onChange`the changed value", () => {
+          const eventHandler = jest.fn();
+          const wrapper = mount(<Slider onChange={eventHandler} />);
+          wrapper.find("input").simulate("change", { target: { value: "10" } });
 
-      expect(input.prop("value")).toEqual("10");
-      wrapper.setProps({ value: "20" });
-      expect(input.prop("value")).toEqual("20");
+          expect(eventHandler).toHaveBeenCalledWith(10);
+        });
+
+        it("allows value to be changed by change events", () => {
+          const wrapper = mount(<Slider defaultValue="10" />);
+          const input = wrapper.find("input");
+
+          expect(input.prop("value")).toEqual("10");
+          input.simulate("change", { target: { value: "20" } });
+          expect(input.prop("value")).toEqual("20");
+        });
+      });
+
+      describe("when controlled", () => {
+        it("calls the `onChange` handler", () => {
+          const eventHandler = jest.fn();
+          const wrapper = mount(<Slider value="8" onChange={eventHandler} />);
+          wrapper.find("input").simulate("change", { target: { value: "10" } });
+
+          expect(eventHandler).toHaveBeenCalledWith(10);
+        });
+
+        it("doesn't allow the value to be changed by change events", () => {
+          const wrapper = mount(<Slider value="10" />);
+          const input = wrapper.find("input");
+
+          expect(input.prop("value")).toEqual("10");
+          input.simulate("change", { target: { value: "20" } });
+          expect(input.prop("value")).toEqual("10");
+        });
+
+        it("recognizes changes to the value prop", () => {
+          const wrapper = mount(<Slider value="10" />);
+          const input = wrapper.find("input");
+
+          expect(input.prop("value")).toEqual("10");
+          wrapper.setProps({ value: "20" });
+          expect(input.prop("value")).toEqual("20");
+        });
+      });
     });
   });
 });
