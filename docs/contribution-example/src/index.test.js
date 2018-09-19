d5493d5322e76c48766eeb858ac1d8c605e1cf6f
Morris Allison III <morris.allison@autodesk.com>
2018-09-19 13:24:10 -0700

    docs: change `contribution-example` to `package-example`

--- docs/contribution-example/src/index.test.js
+++
@@ -1,7 +0,0 @@
-import * as index from "./index";
-
-describe("contribution-example/index", () => {
-  it(`exports default`, () => {
-    expect(index).toHaveProperty("default", expect.any(Function));
-  });
-});
