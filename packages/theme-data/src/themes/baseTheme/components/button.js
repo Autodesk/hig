602806bd222c180012bdb480e5d98def9940a868
Sean Durham <sean@sdurham.net>
2018-10-12 16:10:57 -0700

    wip: Add button roles to base theme

---
+++ packages/theme-data/src/themes/baseTheme/components/button.js
@@ -0,0 +1,182 @@
+export default {
+  // General
+  "button.borderRadius": { type: "borderRadius" },
+  "button.borderWidth": { type: "borderWidth" },
+  "button.fontWeight": { type: "fontWeight" },
+  "button.fontFamily": { type: "fontFamily" },
+  "button.fontSize": { type: "fontSize" },
+  "button.gutter": {
+    type: "length",
+    comment: `
+      The space between elements with the component.
+      E.g. The space between an icon and label within a button.`
+  },
+  "button.horizontalPadding": { type: "length" },
+  "button.minimumWidth": { type: "length" },
+  "button.veticalPadding": { type: "length" },
+
+  /**
+   * ## States
+   *
+   * ### Default
+   */
+  "button.halo.width": { type: "length" },
+
+  /**
+   * ### Hover
+   */
+  "button.hover.halo.width": { type: "length" },
+
+  /**
+   * ### Focus
+   */
+  "button.focus.halo.width": { type: "length" },
+
+  /**
+   * ### Pressed
+   */
+  "button.pressed.halo.width": { type: "length" },
+
+  /**
+   * ## Variants
+   *
+   * ### Outline
+   *
+   * #### Default
+   */
+  "button.outline.backgroundColor": { type: "color" },
+  "button.outline.borderColor": { type: "color" },
+  "button.outline.halo.color": { type: "color" },
+  "button.outline.textColor": { type: "color" },
+  "button.outline.icon.color": { type: "color" },
+
+  /**
+   * #### Hover
+   */
+  "button.outline.hover.backgroundColor": { type: "color" },
+  "button.outline.hover.borderColor": { type: "color" },
+  "button.outline.hover.halo.color": { type: "color" },
+  "button.outline.hover.textColor": { type: "color" },
+  "button.outline.hover.icon.color": { type: "color" },
+
+  /**
+   * #### Focus
+   */
+  "button.outline.focus.backgroundColor": { type: "color" },
+  "button.outline.focus.borderColor": { type: "color" },
+  "button.outline.focus.halo.color": { type: "color" },
+  "button.outline.focus.textColor": { type: "color" },
+  "button.outline.focus.icon.color": { type: "color" },
+
+  /**
+   * #### Pressed
+   */
+  "button.outline.pressed.backgroundColor": { type: "color" },
+  "button.outline.pressed.borderColor": { type: "color" },
+  "button.outline.pressed.halo.color": { type: "color" },
+  "button.outline.pressed.textColor": { type: "color" },
+  "button.outline.pressed.icon.color": { type: "color" },
+
+  /**
+   * #### Disbled
+   */
+  "button.outline.disabled.backgroundColor": { type: "color" },
+  "button.outline.disabled.borderColor": { type: "color" },
+  "button.outline.disabled.halo.color": { type: "color" },
+  "button.outline.disabled.textColor": { type: "color" },
+  "button.outline.disabled.icon.color": { type: "color" },
+
+  /**
+   * ### Solid
+   *
+   * #### Default
+   */
+  "button.solid.backgroundColor": { type: "color" },
+  "button.solid.borderColor": { type: "color" },
+  "button.solid.halo.color": { type: "color" },
+  "button.solid.textColor": { type: "color" },
+  "button.solid.icon.color": { type: "color" },
+
+  /**
+   * #### Hover
+   */
+  "button.solid.hover.backgroundColor": { type: "color" },
+  "button.solid.hover.borderColor": { type: "color" },
+  "button.solid.hover.halo.color": { type: "color" },
+  "button.solid.hover.textColor": { type: "color" },
+  "button.solid.hover.icon.color": { type: "color" },
+
+  /**
+   * #### Focus
+   */
+  "button.solid.focus.backgroundColor": { type: "color" },
+  "button.solid.focus.borderColor": { type: "color" },
+  "button.solid.focus.halo.color": { type: "color" },
+  "button.solid.focus.textColor": { type: "color" },
+  "button.solid.focus.icon.color": { type: "color" },
+
+  /**
+   * #### Pressed
+   */
+  "button.solid.pressed.backgroundColor": { type: "color" },
+  "button.solid.pressed.borderColor": { type: "color" },
+  "button.solid.pressed.halo.color": { type: "color" },
+  "button.solid.pressed.textColor": { type: "color" },
+  "button.solid.pressed.icon.color": { type: "color" },
+
+  /**
+   * #### Disabled
+   */
+  "button.solid.disabled.backgroundColor": { type: "color" },
+  "button.solid.disabled.borderColor": { type: "color" },
+  "button.solid.disabled.halo.color": { type: "color" },
+  "button.solid.disabled.textColor": { type: "color" },
+  "button.solid.disabled.icon.color": { type: "color" },
+
+  /**
+   * ### Flat
+   *
+   * #### Default
+   */
+  "button.flat.backgroundColor": { type: "color" },
+  "button.flat.borderColor": { type: "color" },
+  "button.flat.halo.color": { type: "color" },
+  "button.flat.textColor": { type: "color" },
+  "button.flat.icon.color": { type: "color" },
+
+  /**
+   * #### Hover
+   */
+  "button.flat.hover.backgroundColor": { type: "color" },
+  "button.flat.hover.borderColor": { type: "color" },
+  "button.flat.hover.halo.color": { type: "color" },
+  "button.flat.hover.textColor": { type: "color" },
+  "button.flat.hover.icon.color": { type: "color" },
+
+  /**
+   * #### Focus
+   */
+  "button.flat.focus.backgroundColor": { type: "color" },
+  "button.flat.focus.borderColor": { type: "color" },
+  "button.flat.focus.halo.color": { type: "color" },
+  "button.flat.focus.textColor": { type: "color" },
+  "button.flat.focus.icon.color": { type: "color" },
+
+  /**
+   * #### Pressed
+   */
+  "button.flat.pressed.backgroundColor": { type: "color" },
+  "button.flat.pressed.borderColor": { type: "color" },
+  "button.flat.pressed.halo.color": { type: "color" },
+  "button.flat.pressed.textColor": { type: "color" },
+  "button.flat.pressed.icon.color": { type: "color" },
+
+  /**
+   * #### Disabled
+   */
+  "button.flat.disabled.backgroundColor": { type: "color" },
+  "button.flat.disabled.borderColor": { type: "color" },
+  "button.flat.disabled.halo.color": { type: "color" },
+  "button.flat.disabled.textColor": { type: "color" },
+  "button.flat.disabled.icon.color": { type: "color" }
+};
