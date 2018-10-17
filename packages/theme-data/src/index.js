fbbb4fe765e7a2770943fdb98b340d8d3703f446
Sean Durham <sean@sdurham.net>
2018-10-17 14:59:42 -0700

    BREAKING: Remove deprecated exports

--- packages/theme-data/src/index.js
+++ packages/theme-data/src/index.js
@@ -1,7 +1,2 @@
-export { default as basics } from "./basics";
 export { default as extendTheme } from "./utils/extendTheme";
 export { default as resolveTheme } from "./utils/resolveTheme";
-
-export { default as webLightTheme } from "./themes/webLightTheme";
-export { default as lightGrayTheme } from "./themes/lightGrayTheme";
-export { default as darkBlueTheme } from "./themes/darkBlueTheme";
