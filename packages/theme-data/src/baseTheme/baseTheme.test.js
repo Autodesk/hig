76cad2c98a48669b43124ed82d809a82061950d0
Sean Durham <sean@sdurham.net>
2018-10-24 16:56:02 -0700

    test: ensure theme data types are a valid type

--- packages/theme-data/src/baseTheme/baseTheme.test.js
+++ packages/theme-data/src/baseTheme/baseTheme.test.js
@@ -1,11 +1,15 @@
 import baseTheme from "./index";
+import * as typeConsts from "../consts/types";
+
+const types = Object.values(typeConsts);
 
 describe("baseTheme", () => {
   describe("unresolvedRoles", () => {
     Object.keys(baseTheme.unresolvedRoles).forEach(role => {
       describe(role, () => {
-        it("has a type", () => {
+        it("has a valid type", () => {
           expect(baseTheme.unresolvedRoles[role]).toHaveProperty("type");
+          expect(types).toInclude(baseTheme.unresolvedRoles[role].type);
         });
       });
     });
