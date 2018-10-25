1e8ef5621f548c0b8e7c2b95ce1ef83ee63a2ad0
Sean Durham <sean@sdurham.net>
2018-10-24 17:55:12 -0700

    feat: Add theme data for typography

---
+++ packages/theme-data/src/baseTheme/components/typography.js
@@ -0,0 +1,168 @@
+import {
+  FONT_FAMILY,
+  FONT_WEIGHT,
+  FONT_SIZE,
+  LINE_HEIGHT
+} from "../../consts/types";
+
+export default {
+  // H1
+  "typography.h1.fontFamily": {
+    type: FONT_FAMILY,
+    value: {
+      ref: "basics.fontFamilies.main"
+    }
+  },
+  "typography.h1.fontWeight": {
+    type: FONT_WEIGHT,
+    value: {
+      ref: "basics.fontWeights.regular"
+    }
+  },
+  "typography.h1.fontSize": {
+    type: FONT_SIZE,
+    value: {
+      ref: "density.fontSizes.extraExtraLarge"
+    }
+  },
+  "typography.h1.lineHeight": {
+    type: LINE_HEIGHT,
+    value: {
+      ref: "bascis.lineHeights.large"
+    }
+  },
+  "typography.h1.color": {
+    type: LINE_HEIGHT,
+    value: {
+      ref: "colorScheme.textColor"
+    }
+  },
+
+  // H2
+  "typography.h2.fontFamily": {
+    type: FONT_FAMILY,
+    value: {
+      ref: "basics.fontFamilies.main"
+    }
+  },
+  "typography.h2.fontWeight": {
+    type: FONT_WEIGHT,
+    value: {
+      ref: "basics.fontWeights.regular"
+    }
+  },
+  "typography.h2.fontSize": {
+    type: FONT_SIZE,
+    value: {
+      ref: "density.fontSizes.extraLarge"
+    }
+  },
+  "typography.h2.lineHeight": {
+    type: LINE_HEIGHT,
+    value: {
+      ref: "bascis.lineHeights.small"
+    }
+  },
+  "typography.h2.color": {
+    type: LINE_HEIGHT,
+    value: {
+      ref: "colorScheme.textColor"
+    }
+  },
+
+  // H3
+  "typography.h3.fontFamily": {
+    type: FONT_FAMILY,
+    value: {
+      ref: "basics.fontFamilies.main"
+    }
+  },
+  "typography.h3.fontWeight": {
+    type: FONT_WEIGHT,
+    value: {
+      ref: "basics.fontWeights.medium"
+    }
+  },
+  "typography.h3.fontSize": {
+    type: FONT_SIZE,
+    value: {
+      ref: "density.fontSizes.large"
+    }
+  },
+  "typography.h3.lineHeight": {
+    type: LINE_HEIGHT,
+    value: {
+      ref: "bascis.lineHeights.medium"
+    }
+  },
+  "typography.h3.color": {
+    type: LINE_HEIGHT,
+    value: {
+      ref: "colorScheme.textColor"
+    }
+  },
+
+  // Body
+  "typography.body.fontFamily": {
+    type: FONT_FAMILY,
+    value: {
+      ref: "basics.fontFamilies.main"
+    }
+  },
+  "typography.body.fontWeight": {
+    type: FONT_WEIGHT,
+    value: {
+      ref: "basics.fontWeights.regular"
+    }
+  },
+  "typography.body.fontSize": {
+    type: FONT_SIZE,
+    value: {
+      ref: "density.fontSizes.medium"
+    }
+  },
+  "typography.body.lineHeight": {
+    type: LINE_HEIGHT,
+    value: {
+      ref: "bascis.lineHeights.small"
+    }
+  },
+  "typography.body.color": {
+    type: LINE_HEIGHT,
+    value: {
+      ref: "colorScheme.textColor"
+    }
+  },
+
+  // Caption
+  "typography.caption.fontFamily": {
+    type: FONT_FAMILY,
+    value: {
+      ref: "basics.fontFamilies.main"
+    }
+  },
+  "typography.caption.fontWeight": {
+    type: FONT_WEIGHT,
+    value: {
+      ref: "basics.fontWeights.regular"
+    }
+  },
+  "typography.caption.fontSize": {
+    type: FONT_SIZE,
+    value: {
+      ref: "density.fontSizes.small"
+    }
+  },
+  "typography.caption.lineHeight": {
+    type: LINE_HEIGHT,
+    value: {
+      ref: "bascis.lineHeights.small"
+    }
+  },
+  "typography.caption.color": {
+    type: LINE_HEIGHT,
+    value: {
+      ref: "colorScheme.textColor"
+    }
+  }
+};
