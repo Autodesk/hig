559aefee3ae01dad90dd3d8c8b582055b4fe3177
Morris Allison III <morris.allison@autodesk.com>
2018-10-25 16:23:22 -0700

    style: fix lint error

--- packages/theme-context/src/ThemeContext/createThemeProxy.js
+++ packages/theme-context/src/ThemeContext/createThemeProxy.js
@@ -9,7 +9,7 @@ export default function createThemeProxy(theme) {
 
       if (!isValidProperty) {
         /* eslint-disable-next-line no-console */
-        console.error(`Role ${propertyName} does not exist`);
+        console.error(`Role ${propertyName.toString()} does not exist`);
         return null;
       }
 
