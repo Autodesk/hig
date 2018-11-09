742c0decd2e26ee4647677423ee4071ea34c19e4
Wilbert Mui <wmui51@yahoo.com>
2018-11-08 21:06:06 -0800

    docs: add theme provider to flyout storybook

--- packages/flyout/src/__stories__/Flyout.stories.js
+++ packages/flyout/src/__stories__/Flyout.stories.js
@@ -1,5 +1,7 @@
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
+import KnobbedThemeProvider from "@hig/storybook/storybook-support/decorators/KnobbedThemeProvider";
+
 import Button from "@hig/button";
 
 import infoOptions from "./infoOptions";
@@ -13,7 +15,7 @@ stories.forEach(({ description, getProps }) => {
     description,
     withInfo({
       ...infoOptions,
-      propTablesExclude: [Button]
+      propTablesExclude: [Button, KnobbedThemeProvider]
     })(() => {
       const props = getProps();
       return renderStory(props);
