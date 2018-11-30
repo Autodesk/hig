451bd1fd7149a0390dea9910a63e0df9289e82c0
Wilbert Mui <wmui51@yahoo.com>
2018-11-29 23:59:13 -0800

    feat(theme-data): add button legacy web light theme

--- packages/theme-data/src/colorSchemes/webLight/unresolvedRoles.js
+++ packages/theme-data/src/colorSchemes/webLight/unresolvedRoles.js
@@ -3,12 +3,114 @@ import lightGrayTheme from "../lightGray";
 
 const oldBasics = {
   colors: {
+    "hig-turquoise-40": "#5CE1D3",
     "hig-turquoise-50": "#0ED3BE",
-    "hig-cool-gray-20": "#D4DBE1"
+    "hig-cool-gray-20": "#D4DBE1",
+    "hig-cool-gray-30": "#BEC8D2",
+    "hig-blue-40": "#66BFE9",
+    "hig-blue-60": "#0671A1"
   }
 };
 
 const webLightThemeResolvedRoles = extendTheme(lightGrayTheme.unresolvedRoles, {
+  "button.fontWeight": {
+    value: {
+      ref: "basics.fontWeights.bold"
+    }
+  },
+  "button.halo.width": {
+    value: 0
+  },
+  "button.flat.textColor": {
+    value: {
+      ref: "basics.colors.black"
+    }
+  },
+  "button.flat.focus.backgroundColor": {
+    value: oldBasics.colors["hig-turquoise-40"]
+  },
+  "button.flat.focus.borderColor": {
+    value: oldBasics.colors["hig-turquoise-40"]
+  },
+  "button.flat.focus.textColor": {
+    value: {
+      ref: "basics.colors.white"
+    }
+  },
+  "button.flat.hover.borderColor": {
+    value: oldBasics.colors["hig-turquoise-50"]
+  },
+  "button.flat.pressed.halo.color": {
+    value: {
+      ref: "basics.colors.white"
+    },
+    transform: {
+      alpha: 0
+    }
+  },
+  "button.outline.textColor": {
+    value: {
+      ref: "basics.colors.black"
+    }
+  },
+  "button.outline.borderColor": {
+    value: oldBasics.colors["hig-cool-gray-30"]
+  },
+  "button.outline.focus.backgroundColor": {
+    value: oldBasics.colors["hig-turquoise-40"]
+  },
+  "button.outline.focus.borderColor": {
+    value: oldBasics.colors["hig-turquoise-40"]
+  },
+  "button.outline.focus.textColor": {
+    value: {
+      ref: "basics.colors.white"
+    }
+  },
+  "button.outline.pressed.borderColor": {
+    value: oldBasics.colors["hig-turquoise-40"]
+  },
+  "button.outline.pressed.halo.color": {
+    value: {
+      ref: "basics.colors.white"
+    },
+    transform: {
+      alpha: 0
+    }
+  },
+  "button.outline.hover.borderColor": {
+    value: oldBasics.colors["hig-turquoise-50"]
+  },
+  "button.solid.focus.backgroundColor": {
+    value: oldBasics.colors["hig-blue-60"]
+  },
+  "button.solid.focus.halo.color": {
+    value: {
+      ref: "basics.colors.white"
+    },
+    transform: {
+      alpha: 0
+    }
+  },
+  "button.solid.hover.backgroundColor": {
+    value: oldBasics.colors["hig-blue-40"]
+  },
+  "button.solid.hover.halo.color": {
+    value: {
+      ref: "basics.colors.white"
+    },
+    transform: {
+      alpha: 0
+    }
+  },
+  "button.solid.pressed.halo.color": {
+    value: {
+      ref: "basics.colors.white"
+    },
+    transform: {
+      alpha: 0
+    }
+  },
   "input.hover.borderBottomColor": {
     value: oldBasics.colors["hig-turquoise-50"]
   },
