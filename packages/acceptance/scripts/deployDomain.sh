eaa09e13cd775f96428148170f8e2d72f4864aa9
Sean Durham <sean@sdurham.net>
2018-12-10 14:38:31 -0800

    chore: adds deploy scripts for acceptance app

---
+++ packages/acceptance/scripts/deployDomain.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Using the branch name
+# 1. Remove all characters that are not alphanumeric, hyphens, periods, or forward slashes
+# 2. Replace forward slashes and periods with hyphens
+# 3. Translate characters to lowercase
+storybookSubDomainSuffix() {
+  echo $CIRCLE_BRANCH | awk '{ gsub(/[^a-zA-Z0-9-.\/]/, ""); gsub(/[\/.]/, "-"); $0=tolower($0); print }'
+}
+
+echo "hig-accept-$(storybookSubDomainSuffix).surge.sh"
