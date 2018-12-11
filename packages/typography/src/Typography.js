df6418acbfddf7280ee390c37b739ee57298e21b
Rae Bonfanti <rae@carbonfive.com>
2018-12-10 17:15:59 -0800

    feat (theming): adds stylesheet func, Typography
    This is the first step to getting Typography themable.
    * versions typography at 1.0.0-alpha
    * converts old Typography enzyme tests to snapshot style
    * uses a stylesheet function to add styles
    * uses ThemeContext.Consumer
    * adds AVAILABLE_ALIGNMENTS, AVAILABLE_FONT_WEIGHTS, and
    AVAILABLE_VARIANTS constants to the package
    * adds emotion 10 package

--- packages/typography/src/Typography.js
+++ packages/typography/src/Typography.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
+import { css } from "emotion";
+import { ThemeContext } from "@hig/theme-context";
 
-import { _VALID_COLORS, _VALID_SIZES, _VALID_TYPES } from "./_constants";
+import {
+  _VALID_COLORS,
+  _VALID_SIZES,
+  _VALID_TYPES,
+  AVAILABLE_ALIGNMENTS,
+  AVAILABLE_FONT_WEIGHTS,
+  AVAILABLE_VARIANTS
+} from "./_constants";
 import "./typography.scss";
+import stylesheet from "./Typography.stylesheet";
 
 export default class Typography extends Component {
   static propTypes = {
     /**
+     * Sets the horizontal alignment of an inline box.
+     */
+    align: PropTypes.oneOf(AVAILABLE_ALIGNMENTS),
+    /**
      * Whether to render bold text
      */
     bold: PropTypes.bool,
+    /**
+     * Text or content to render.
+     */
+    children: PropTypes.node,
     /**
      * Colors the text with one of the supported HIG colors
      */
@@ -19,6 +37,10 @@ export default class Typography extends Component {
      * Whether to show text as disabled
      */
     disabled: PropTypes.bool,
+    /**
+     * Specifies the weight (or boldness) of the font.
+     */
+    fontWeight: PropTypes.oneOf(AVAILABLE_FONT_WEIGHTS),
     /**
      * An opacity value to modify the color, between 0.0 and 1.0
      */
@@ -30,36 +52,50 @@ export default class Typography extends Component {
     /**
      * Indicates the initial Typography style
      */
-    type: PropTypes.oneOf(_VALID_TYPES).isRequired,
+    type: PropTypes.oneOf(_VALID_TYPES),
     /**
      * Text to render styled based on provided type
      */
-    text: PropTypes.string.isRequired
+    text: PropTypes.string,
+    /**
+     * Indicates the initial Typography style
+     */
+    variant: PropTypes.oneOf(AVAILABLE_VARIANTS)
   };
 
+  classes = resolvedRoles =>
+    cx(
+      "hig__typography",
+      {
+        [`hig__typography__${this.props.type}`]: this.props.type,
+        [`hig__typography--${this.props.color}`]: this.props.color,
+        [`hig__typography--${this.props.size}`]: this.props.size,
+        "hig__typography--bold": this.props.bold,
+        "hig__typography--disabled": this.props.disabled
+      },
+      css(stylesheet(this.props, resolvedRoles).typography)
+    );
+
+  /**
+   * N.B.: The intent is to apply opacity to the color. Typography components currently only render text, so
+   * applying opacity should have no side effects. If this component ever wraps styled content, we should reconsider
+   * this mechanism, because opacity in children nodes can be magnified by this parent node.
+   */
+  opacity = () =>
+    typeof this.props.opacity === "number"
+      ? { opacity: this.props.opacity }
+      : {};
+
   render() {
-    const classes = cx("hig__typography", {
-      [`hig__typography__${this.props.type}`]: this.props.type,
-      [`hig__typography--${this.props.color}`]: this.props.color,
-      [`hig__typography--${this.props.size}`]: this.props.size,
-      "hig__typography--bold": this.props.bold,
-      "hig__typography--disabled": this.props.disabled
-    });
-
-    /**
-     * N.B.: The intent is to apply opacity to the color. Typography components currently only render text, so
-     * applying opacity should have no side effects. If this component ever wraps styled content, we should reconsider
-     * this mechanism, because opacity in children nodes can be magnified by this parent node.
-     */
-    const customStyles =
-      typeof this.props.opacity === "number"
-        ? { opacity: this.props.opacity }
-        : {};
-
     return (
-      <span className={classes} style={customStyles}>
-        {this.props.text}
-      </span>
+      <ThemeContext.Consumer>
+        {({ resolvedRoles }) => (
+          <span className={this.classes(resolvedRoles)} style={this.opacity()}>
+            {this.props.text}
+            {this.props.children}
+          </span>
+        )}
+      </ThemeContext.Consumer>
     );
   }
 }
--- packages/typography/src/_constants.js
+++ packages/typography/src/_constants.js
@@ -22,3 +22,25 @@ export const _VALID_TYPES = Object.freeze([
   "disabled",
   "caption"
 ]);
+
+export const AVAILABLE_ALIGNMENTS = Object.freeze([
+  "center",
+  "justify",
+  "left",
+  "right"
+]);
+export const AVAILABLE_FONT_WEIGHTS = Object.freeze([
+  300,
+  400,
+  500,
+  700,
+  "bold", // same as 700
+  "normal" // same as 400
+]);
+export const AVAILABLE_VARIANTS = Object.freeze([
+  "body",
+  "caption",
+  "h1",
+  "h2",
+  "h3"
+]);
