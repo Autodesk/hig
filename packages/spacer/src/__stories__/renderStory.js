23d89f5dbb8dbf94677d28909e3de4e49e857926
Wilbert Mui <wmui51@yahoo.com>
2018-12-17 13:27:12 -0800

    chore: storybook knobbed theme provider default is all 4 themes

--- packages/spacer/src/__stories__/renderStory.js
+++ packages/spacer/src/__stories__/renderStory.js
@@ -1,7 +1,5 @@
 import React from "react";
-import KnobbedThemeProvider, {
-  THEMES
-} from "@hig/storybook/storybook-support/decorators/KnobbedThemeProvider";
+import KnobbedThemeProvider from "@hig/storybook/storybook-support/decorators/KnobbedThemeProvider";
 import DefaultExport from "../index";
 import getKnobs from "./getKnobs";
 
@@ -9,9 +7,7 @@ export default function renderStory(props) {
   const { children, theme, ...otherProps } = getKnobs(props);
 
   return (
-    <KnobbedThemeProvider
-      supportedThemes={[THEMES.WEB_LIGHT, THEMES.DARK_BLUE]}
-    >
+    <KnobbedThemeProvider>
       <div
         style={{ width: "100px", height: "100px", backgroundColor: "blue" }}
       />
