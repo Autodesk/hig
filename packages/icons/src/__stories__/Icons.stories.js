c2cecad43bee562a62faa0d6f2851eb7c2739852
Albert Yi <albert@carbonfive.com>
2019-01-02 13:44:43 -0800

    chore: update storybook for icons to include color

--- packages/icons/src/__stories__/Icons.stories.js
+++ packages/icons/src/__stories__/Icons.stories.js
@@ -1,8 +1,22 @@
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
+import { text } from "@storybook/addon-knobs";
 import infoOptions from "./infoOptions";
 import renderStory from "./renderStory";
 import stories from "./stories";
+
+const knobLabels = {
+  color: "Color"
+};
+
+function getKnobs(props) {
+  const { color, ...otherProps } = props;
+
+  return {
+    ...otherProps,
+    color: text(knobLabels.color, color)
+  };
+}
 
 const storybook = storiesOf("Basics|Icons", module);
 
@@ -10,7 +24,7 @@ stories.forEach(({ description, getProps }) => {
   storybook.add(
     description,
     withInfo(infoOptions)(() => {
-      const props = getProps();
+      const props = getKnobs(getProps());
       return renderStory(props);
     })
   );
