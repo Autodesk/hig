329eb5e28deb45c292b4aab0be80f7017ae4189a
Rae Bonfanti <rae@carbonfive.com>
2019-01-04 17:31:39 -0800

    style: prettier in acceptance app

--- acceptance/src/pages/InputPage.js
+++ acceptance/src/pages/InputPage.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import Input from '@hig/Input';
-import Label from '@hig/label';
-import Spacer from '@hig/spacer';
+import React from "react";
+import Input from "@hig/Input";
+import Label from "@hig/label";
+import Spacer from "@hig/spacer";
 
-import ThemeRepeater from '../components/ThemeRepeater';
-import Text from '../components/Text';
+import ThemeRepeater from "../components/ThemeRepeater";
+import Text from "../components/Text";
 
 const states = [
   { label: "interactive", props: {} },
@@ -12,38 +12,40 @@ const states = [
   { label: "disabled", props: { disabled: true } }
 ];
 
-const variants = ['line', 'box']
+const variants = ["line", "box"];
 
 function InputPage() {
   return (
-    <ThemeRepeater>{({ id }) => {
-      return (
-        <div>
-          {variants.map(variant => {
-            return (
-              <div key={variant}>
-                <Text>{variant}</Text>
-                {states.map(({ label, props }) => {
-                  const key = id + label + variant;
-                  return (
-                    <div key={key}>
-                      <div style={{}}>
-                        <Label htmlFor={key} disabled={props.disabled} >
-                          {label}
-                        </Label>
-                        <Input id={key} {...props} variant={variant}/>
-                        <Spacer spacing="xs" />
+    <ThemeRepeater>
+      {({ id }) => {
+        return (
+          <div>
+            {variants.map(variant => {
+              return (
+                <div key={variant}>
+                  <Text>{variant}</Text>
+                  {states.map(({ label, props }) => {
+                    const key = id + label + variant;
+                    return (
+                      <div key={key}>
+                        <div style={{}}>
+                          <Label htmlFor={key} disabled={props.disabled}>
+                            {label}
+                          </Label>
+                          <Input id={key} {...props} variant={variant} />
+                          <Spacer spacing="xs" />
+                        </div>
+                        <Spacer />
                       </div>
-                      <Spacer />
-                    </div>
-                  )
-                })}
-              </div>
-            )
-          })}
-        </div>
-      )
-    }}</ThemeRepeater>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        );
+      }}
+    </ThemeRepeater>
   );
 }
 
--- acceptance/src/routes.js
+++ acceptance/src/routes.js
@@ -1,11 +1,11 @@
-import ButtonsPage from './pages/ButtonsPage';
-import FlyoutPage from './pages/FlyoutPage';
-import CheckboxPage from './pages/CheckboxPage';
-import InputPage from './pages/InputPage';
+import ButtonsPage from "./pages/ButtonsPage";
+import FlyoutPage from "./pages/FlyoutPage";
+import CheckboxPage from "./pages/CheckboxPage";
+import InputPage from "./pages/InputPage";
 
 export default [
-  { path: '/button/', component: ButtonsPage, label: "Button" },
-  { path: '/flyout/', component: FlyoutPage, label: "Flyout" },
-  { path: '/checkbox/', component: CheckboxPage, label: "Checkbox" },
-  { path: '/input/', component: InputPage, label: "Input" }
+  { path: "/button/", component: ButtonsPage, label: "Button" },
+  { path: "/flyout/", component: FlyoutPage, label: "Flyout" },
+  { path: "/checkbox/", component: CheckboxPage, label: "Checkbox" },
+  { path: "/input/", component: InputPage, label: "Input" }
 ];
