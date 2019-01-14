a514520afb3f0baf8616a996bdcbf5fe75ff363d
Sean Durham <sean@sdurham.net>
2019-01-14 12:29:36 -0800

    feat: add theem data for text link

---
+++ packages/theme-data/src/baseTheme/components/textLink.js
@@ -0,0 +1,16 @@
+import { COLOR, LENGTH } from "../../consts/types";
+
+export default {
+  "textLink.textColor": {
+    type: COLOR
+  },
+  "textLink.hover.underline.color": {
+    type: COLOR
+  },
+  "textLink.focus.halo.color": {
+    type: COLOR
+  },
+  "textLink.focus.halo.width": {
+    type: LENGTH
+  }
+};
