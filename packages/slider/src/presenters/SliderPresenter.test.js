282abbf5e6fe44ce551f7c9378ca99b8a5a3ffe5
Rae Bonfanti <rae@carbonfive.com>
2019-01-24 17:05:20 -0800

    chore: tidy slider/presenters/SliderPresenter test

--- packages/slider/src/presenters/SliderPresenter.test.js
+++ packages/slider/src/presenters/SliderPresenter.test.js
@@ -1,66 +1,6 @@
-import React from "react";
-import { mount } from "enzyme";
-import renderer from "react-test-renderer";
+import { takeSnapshotsOf } from "@hig/jest-preset/helpers";
 import SliderPresenter from "./SliderPresenter";
 
-const subject = (props = {}) => mount(<SliderPresenter {...props} />);
-
 describe("Slider/presenters/SliderPresenter", () => {
-  let wrapper;
-  it("renders correctly without a value", () => {
-    const tree = renderer.create(<SliderPresenter />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  describe("event handlers", () => {
-    let eventHandler;
-
-    describe("onBlur", () => {
-      beforeEach(() => {
-        eventHandler = jest.fn();
-        wrapper = subject({ onBlur: eventHandler });
-      });
-
-      it("is triggered on blur", () => {
-        wrapper.simulate("blur");
-        expect(eventHandler).toBeCalled();
-      });
-    });
-
-    describe("onChange", () => {
-      beforeEach(() => {
-        eventHandler = jest.fn();
-        wrapper = subject({ onChange: eventHandler });
-      });
-
-      it("is triggered on change", () => {
-        wrapper.simulate("change");
-        expect(eventHandler).toBeCalled();
-      });
-    });
-
-    describe("onFocus", () => {
-      beforeEach(() => {
-        eventHandler = jest.fn();
-        wrapper = subject({ onFocus: eventHandler });
-      });
-
-      it("is triggered on focus", () => {
-        wrapper.simulate("focus");
-        expect(eventHandler).toBeCalled();
-      });
-    });
-
-    describe("onInput", () => {
-      beforeEach(() => {
-        eventHandler = jest.fn();
-        wrapper = subject({ onInput: eventHandler });
-      });
-
-      it("is triggered on input", () => {
-        wrapper.simulate("input");
-        expect(eventHandler).toBeCalled();
-      });
-    });
-  });
+  takeSnapshotsOf(SliderPresenter, [{ desc: "renders", props: {} }]);
 });
