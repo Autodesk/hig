b196546feffd4b0e755780475db4617b566cf6da
Albert Yi <albert@carbonfive.com>
2019-02-06 17:12:51 -0800

    chore(dropdown): add theme knob to storybook

--- packages/dropdown/src/__stories__/renderStory.js
+++ packages/dropdown/src/__stories__/renderStory.js
@@ -1,9 +1,14 @@
 import React from "react";
 import DefaultExport from "../index";
 import getKnobs from "./getKnobs";
+import KnobbedThemeProvider from "@hig/storybook/storybook-support/decorators/KnobbedThemeProvider";
 
 export default function renderStory(props) {
   const { children, ...otherProps } = getKnobs(props);
 
-  return <DefaultExport {...otherProps}>{children}</DefaultExport>;
+  return (
+    <KnobbedThemeProvider>
+      <DefaultExport {...otherProps}>{children}</DefaultExport>
+    </KnobbedThemeProvider>
+  );
 }
