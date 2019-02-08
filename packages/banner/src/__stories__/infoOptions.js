f9974752df5eaf3c3c21abde730d811964c93173
Wilbert Mui <wmui51@yahoo.com>
2019-02-08 09:25:05 -0800

    chore(banner): add rich-text devdependency remove legacy css

--- packages/banner/src/__stories__/infoOptions.js
+++ packages/banner/src/__stories__/infoOptions.js
@@ -1,6 +1,5 @@
 import React from "react";
 import RichText from "@hig/rich-text";
-import "@hig/rich-text/build/index.css";
 
 import Banner from "../Banner";
 import readme from "../../README.md";
