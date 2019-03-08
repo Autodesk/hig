2ee7ae6b707cb7a13d98a7b70db0c81235d8080c
Wilbert Mui <wmui51@yahoo.com>
2019-03-08 13:38:04 -0800

    docs: update tabs in acceptance app

--- acceptance/src/pages/TabsPage.js
+++ acceptance/src/pages/TabsPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Spacer from "@hig/spacer";
 import Tabs, { Tab } from "@hig/tabs";
 import Typography from "@hig/typography";
 import "@hig/fonts/build/ArtifaktElement.css";
@@ -11,12 +12,15 @@ function TabsPage() {
         return (
           <Tabs align="center">
             <Tab label="Details" key="details">
+              <Spacer spacing="m" />
               <Typography>Details content</Typography>
             </Tab>
             <Tab label="Activities" key="activities">
+              <Spacer spacing="m" />
               <Typography>Activities content</Typography>
             </Tab>
             <Tab label="Inspector" key="inspector">
+              <Spacer spacing="m" />
               <Typography>Inspector content</Typography>
             </Tab>
           </Tabs>
