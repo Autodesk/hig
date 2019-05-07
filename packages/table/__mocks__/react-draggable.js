57fbec829a35e543f1fb1d35c68ee21be8c87e61
Wilbert Mui <wmui51@yahoo.com>
2019-05-07 14:03:28 -0700

    chore: remove unsupported table

--- packages/table/__mocks__/react-draggable.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function DraggableCoreMock({ children }) {
-  return <div data-mock="DraggableCore">{children}</div>;
-}
-
-module.exports = {
-  DraggableCore: jest.fn(DraggableCoreMock)
-};
