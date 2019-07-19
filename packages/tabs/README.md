5d12def64d0f5d1c12b299334f2f8302f2cc2fe7
Gary Sun <gary.sun@autodesk.com>
2019-07-19 16:55:08 +0800

    docs: update stories and descriptions for Tabs

--- packages/tabs/README.md
+++ packages/tabs/README.md
@@ -21,9 +21,31 @@ import Tabs, { Tab } from "@hig/tabs";
 ## Basic usage
 
 ```jsx
-<Tabs align="center">
-  <Tab label="Details">Details content</Tab>
-  <Tab label="Activities">Activities content</Tab>
-  <Tab label="Inspector">Inspector content</Tab>
+<Tabs>
+  <Tab key="details" label="Details">Details content</Tab>
+  <Tab key="activities" label="Activities">Activities content</Tab>
+  <Tab key="inspector" label="Inspector">Inspector content</Tab>
 </Tabs>
 ```
+
+## Customization
+
+The visual styles of `Tabs` can be customized by its props, available values can be found via:
+```js
+import {
+  AVAILABLE_ALIGNMENTS,
+  AVAILABLE_VARIANTS,
+  AVAILABLE_ORIENTATIONS,
+  alignments,
+  variants,
+  orientations
+} from "@hig/tabs";
+```
+
+### Complex Tabs
+
+Complex tabs are composed of an interactive container, text label, divider and optional close icon. Complex tabs only works when `varient` is set to `box` or `canvas`.
+
+### Custom CSS
+
+Use the `className` prop to pass in a css class name to the outermost container of the component. The class name will also pass down to most of the other styled elements within the component.
--- packages/tabs/src/__stories__/getKnobs.js
+++ packages/tabs/src/__stories__/getKnobs.js
@@ -1,10 +1,12 @@
 import { action } from "@storybook/addon-actions";
-import { select } from "@storybook/addon-knobs/react";
+import { boolean, select } from "@storybook/addon-knobs/react";
 import { makeSelectOptions } from "@hig/storybook/utils";
 
-import { alignments } from "../alignments";
+import { alignments, variants, orientations } from "../constants";
 
 const alignmentOptions = makeSelectOptions(alignments);
+const variantOptions = makeSelectOptions(variants);
+const orientationOptions = makeSelectOptions(orientations);
 
 const knobGroupIds = {
   basic: "Basic"
@@ -12,11 +14,22 @@ const knobGroupIds = {
 
 const knobLabels = {
   align: "Alignment",
-  onTabChange: "onTabChange"
+  variant: "Variant",
+  orientation: "Orientation",
+  showTabDivider: "Show tab divider",
+  onTabChange: "onTabChange",
+  onTabClose: "onTabClose"
 };
 
 export default function getKnobs(props) {
-  const { align, onTabChange, ...otherProps } = props;
+  const {
+    align,
+    variant,
+    orientation,
+    showDivider,
+    onTabChange,
+    ...otherProps
+  } = props;
 
   return {
     ...otherProps,
@@ -26,6 +39,24 @@ export default function getKnobs(props) {
       align,
       knobGroupIds.basic
     ),
-    onTabChange: action(knobLabels.onTabChange)
+    variant: select(
+      knobLabels.variant,
+      variantOptions,
+      variant,
+      knobGroupIds.basic
+    ),
+    orientation: select(
+      knobLabels.orientation,
+      orientationOptions,
+      orientation,
+      knobGroupIds.basic
+    ),
+    showTabDivider: boolean(
+      knobLabels.showTabDivider,
+      showDivider,
+      knobGroupIds.basic
+    ),
+    onTabChange: action(knobLabels.onTabChange),
+    onTabClose: action(knobLabels.onTabClose)
   };
 }
