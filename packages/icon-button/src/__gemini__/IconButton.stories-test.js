4fd31cb18750fa000f025045533699c924a8b133
Wilbert Mui <wmui51@yahoo.com>
2019-08-01 11:20:56 -0700

    refactor: linter errors

--- packages/icon-button/src/__gemini__/IconButton.stories-test.js
+++ packages/icon-button/src/__gemini__/IconButton.stories-test.js
@@ -5,23 +5,35 @@ import { Settings24 } from "@hig/icons";
 import IconButton from "../index";
 
 storiesOf("IconButton", module)
-  .add("default", () => {
-    return (
-      <div>
-        <IconButton title="Icon button" icon={<Settings24 />} />
-        <IconButton title="Icon button" on icon={<Settings24 />} />
-        <IconButton title="Icon button" disabled icon={<Settings24 />} />
-        <IconButton title="Icon button" on disabled icon={<Settings24 />} />
-      </div>
-    );
-  })
-  .add("static", () => {
-    return (
-      <div>
-        <IconButton variant="static" title="Icon button" icon={<Settings24 />} />
-        <IconButton variant="static" title="Icon button" on icon={<Settings24 />} />
-        <IconButton variant="static" title="Icon button" disabled icon={<Settings24 />} />
-        <IconButton variant="static" title="Icon button" on disabled icon={<Settings24 />} />
-      </div>
-    );
-  });
+  .add("default", () => (
+    <div>
+      <IconButton title="Icon button" icon={<Settings24 />} />
+      <IconButton title="Icon button" on icon={<Settings24 />} />
+      <IconButton title="Icon button" disabled icon={<Settings24 />} />
+      <IconButton title="Icon button" on disabled icon={<Settings24 />} />
+    </div>
+  ))
+  .add("static", () => (
+    <div>
+      <IconButton variant="static" title="Icon button" icon={<Settings24 />} />
+      <IconButton
+        variant="static"
+        title="Icon button"
+        on
+        icon={<Settings24 />}
+      />
+      <IconButton
+        variant="static"
+        title="Icon button"
+        disabled
+        icon={<Settings24 />}
+      />
+      <IconButton
+        variant="static"
+        title="Icon button"
+        on
+        disabled
+        icon={<Settings24 />}
+      />
+    </div>
+  ));
