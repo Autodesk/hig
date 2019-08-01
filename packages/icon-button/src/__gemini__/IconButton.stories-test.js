47fb7f4fc7c44e829a6fa944de7baf66dbb52bdd
Wilbert Mui <wmui51@yahoo.com>
2019-08-01 11:00:33 -0700

    test: update gemini tests

--- packages/icon-button/src/__gemini__/IconButton.stories-test.js
+++ packages/icon-button/src/__gemini__/IconButton.stories-test.js
@@ -5,14 +5,23 @@ import { Settings24 } from "@hig/icons";
 import IconButton from "../index";
 
 storiesOf("IconButton", module)
-  .add("default", () => (
-    <IconButton type="primary" title="Icon button" icon={<Settings24 />} />
-  ))
-  .add("with link", () => (
-    <IconButton
-      type="primary"
-      title="Icon button"
-      link="http://www.autodesk.com"
-      icon={<Settings24 />}
-    />
-  ));
+  .add("default", () => {
+    return (
+      <div>
+        <IconButton title="Icon button" icon={<Settings24 />} />
+        <IconButton title="Icon button" on icon={<Settings24 />} />
+        <IconButton title="Icon button" disabled icon={<Settings24 />} />
+        <IconButton title="Icon button" on disabled icon={<Settings24 />} />
+      </div>
+    );
+  })
+  .add("static", () => {
+    return (
+      <div>
+        <IconButton variant="static" title="Icon button" icon={<Settings24 />} />
+        <IconButton variant="static" title="Icon button" on icon={<Settings24 />} />
+        <IconButton variant="static" title="Icon button" disabled icon={<Settings24 />} />
+        <IconButton variant="static" title="Icon button" on disabled icon={<Settings24 />} />
+      </div>
+    );
+  });
