ce0d93275b29571b8f8a61c0eb69e1c121a6bd88
Wilbert Mui <wmui51@yahoo.com>
2020-02-10 15:43:31 -0800

    feat: align colorScheme roles with design toolkit
    Added new roles, renamed roles, and begin deprecations process on older roles
    Both new and old roles wil be exported and old roles will be removed at a later release
    
    Renamed:
    
    colorScheme.accentColor => colorScheme.reference.accent
    colorScheme.baseColor => colorScheme.reference.base
    colorScheme.highShadowColor => colorScheme.shadow.high
    colorScheme.iconColor => colorScheme.icon.default
    colorScheme.lowShadowColor => colorScheme.shadow.low
    colorScheme.surfaceLevel100Color => colorScheme.surface.level100
    colorScheme.surfaceLevel200Color => colorScheme.surface.level200
    colorScheme.surfaceLevel250Color => colorScheme.surface.level250
    colorScheme.surfaceLevel300Color => colorScheme.surface.level300
    colorScheme.surfaceLevel350Color => colorScheme.surface.level350
    colorScheme.textColor => colorScheme.text.default
    colorScheme.textColorDim => colorScheme.text.dim
    colorScheme.infoColor => colorScheme.status.info
    colorScheme.errorColor => colorScheme.status.error
    colorScheme.successColor => colorScheme..status.success
    colorScheme..warningColor => colorScheme..status.warning
    
    Added:
    
    colorScheme.background.active
    colorScheme.background.hover
    colorScheme.background.on.default
    colorScheme.background.on.focus
    colorScheme.background.on.hover
    colorScheme.background.pressed
    colorScheme.background.selected
    colorScheme.border.default
    colorScheme.border.focus
    colorScheme.border.hover
    colorScheme.border.pressed
    colorScheme.disabledOpacity
    colorScheme.halo.hover
    colorScheme.halo.pressed
    colorScheme.icon.active
    colorScheme.icon.focus
    colorScheme.icon.hover
    colorScheme.icon.pressed
    colorScheme.shadow.high
    colorScheme.shadow.low
    colorScheme.mask
    colorScheme.transparent
    colorScheme.divider.lightweight
    colorScheme.divider.heavyweight
    colorScheme.indicator.default
    colorScheme.indicator.focus
    colorScheme.indicator.hover
    colorScheme.indicator.pressed
    colorScheme.status.info
    colorScheme.status.read

--- packages/theme-data/src/colorSchemes/darkGray/system/colorScheme.js
+++ packages/theme-data/src/colorSchemes/darkGray/system/colorScheme.js
@@ -1,77 +1,117 @@
 export default {
-  accentColor: {
-    value: {
-      ref: "basics.colors.autodeskBlue400"
-    }
-  },
   baseColor: {
     value: {
-      ref: "basics.colors.white"
+      ref: "colorScheme.reference.base"
     },
     transform: {
       alpha: 0.5
     }
   },
-  "component.backgroundColor": {
+  "reference.base": {
     value: {
-      ref: "basics.colors.surfaceDarkGrayLevel100"
+      ref: "basics.colors.primary.white"
     }
   },
   highShadowColor: {
     value: {
-      ref: "basics.colors.surfaceDarkGrayShadow"
+      ref: "colorScheme.shadow.high"
     },
     transform: {
       alpha: 0.5
     }
   },
+  "shadow.high": {
+    value: {
+      ref: "basics.colors.shadow.darkGray"
+    }
+  },
   iconColor: {
-    value: { ref: "basics.colors.iconDarkGrayColor" }
+    value: { ref: "colorScheme.icon.default" }
+  },
+  "icon.default": {
+    value: {
+      ref: "basics.colors.icon.darkGray"
+    }
   },
   lowShadowColor: {
     value: {
-      ref: "basics.colors.surfaceDarkGrayShadow"
-    },
-    transform: {
-      alpha: 0.8
+      ref: "colorScheme.shadow.low"
+    }
+  },
+  "shadow.low": {
+    value: {
+      ref: "basics.colors.shadow.darkGray"
     }
   },
   surfaceLevel100Color: {
     value: {
-      ref: "basics.colors.surfaceDarkGrayLevel100"
+      ref: "colorScheme.surface.level100"
+    }
+  },
+  "surface.level100": {
+    value: {
+      ref: "basics.colors.surface.darkGray.100"
     }
   },
   surfaceLevel200Color: {
     value: {
-      ref: "basics.colors.surfaceDarkGrayLevel200"
+      ref: "colorScheme.surface.level200"
+    }
+  },
+  "surface.level200": {
+    value: {
+      ref: "basics.colors.surface.darkGray.200"
     }
   },
   surfaceLevel250Color: {
     value: {
-      ref: "basics.colors.surfaceDarkGrayLevel250"
+      ref: "colorScheme.surface.level250"
+    }
+  },
+  "surface.level250": {
+    value: {
+      ref: "basics.colors.surface.darkGray.250"
     }
   },
   surfaceLevel300Color: {
     value: {
-      ref: "basics.colors.surfaceDarkGrayLevel300"
+      ref: "colorScheme.surface.level300"
+    }
+  },
+  "surface.level300": {
+    value: {
+      ref: "basics.colors.surface.darkGray.300"
     }
   },
   surfaceLevel350Color: {
     value: {
-      ref: "basics.colors.surfaceDarkGrayLevel350"
+      ref: "colorScheme.surface.level350"
     }
   },
-  textColor: {
+  "surface.level350": {
     value: {
-      ref: "basics.colors.textAgainstDark"
+      ref: "basics.colors.surface.darkGray.350"
     }
   },
-  textColorDim: {
-    value: {
-      ref: "basics.colors.textAgainstDark"
-    },
+  "background.hover": {
+    value: "#1f1f1f",
     transform: {
-      alpha: 0.7
+      alpha: 0.3
+    }
+  },
+  "icon.focus": {
+    value: {
+      ref: "basics.colors.icon.darkGray"
+    }
+  },
+  "icon.hover": {
+    value: {
+      ref: "basics.colors.primary.charcoal.200"
+    }
+  },
+  "icon.pressed": {
+    value: {
+      ref: "basics.colors.primary.charcoal.200"
     }
   }
 };
