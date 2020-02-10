0d45d34d8f881afe83ae29590f211556cd04e401
Wilbert Mui <wmui51@yahoo.com>
2020-02-10 15:43:31 -0800

    feat: align colorScheme roles with design toolkit
    Added new roles, renamed roles, and begin deprecations process on older roles
    Both new and old roles wil be exported and old roles will be removed at a later release
    
    Renamed:
    
    colorScheme.accentColor => colorScheme.reference.accent
    colorScheme.baseColor => colorScheme.reference.base
    colorScheme.highShadowColor => colorScheme.shadow.high
    colorScheme.iconColor => colorScheme.icon.default
    colorScheme.lowShadowColor => colorScheme.shadow.low
    colorScheme.surfaceLevel100Color => colorScheme.surface.level100
    colorScheme.surfaceLevel200Color => colorScheme.surface.level200
    colorScheme.surfaceLevel250Color => colorScheme.surface.level250
    colorScheme.surfaceLevel300Color => colorScheme.surface.level300
    colorScheme.surfaceLevel350Color => colorScheme.surface.level350
    colorScheme.textColor => colorScheme.text.default
    colorScheme.textColorDim => colorScheme.text.dim
    colorScheme.infoColor => colorScheme.status.info
    colorScheme.errorColor => colorScheme.status.error
    colorScheme.successColor => colorScheme.status.success
    colorScheme.warningColor => colorScheme.status.warning
    
    Added:
    
    colorScheme.background.on.default
    colorScheme.background.on.focus
    colorScheme.background.on.hover
    colorScheme.background.on.pressed
    colorScheme.background.selected
    colorSchmee.background.transparent
    colorScheme.halo.hover
    colorScheme.halo.pressed
    colorScheme.icon.on.default
    colorScheme.icon.on.hover
    colorScheme.icon.on.focus
    colorScheme.icon.on.pressed
    colorScheme.icon.focus
    colorScheme.icon.hover
    colorScheme.icon.pressed
    colorScheme.shadow.high
    colorScheme.shadow.low
    colorScheme.opacity.mask
    colorScheme.opacity.disabled
    colorScheme.divider.lightweight
    colorScheme.divider.heavyweight
    colorScheme.indicator.default
    colorScheme.indicator.focus
    colorScheme.indicator.hover
    colorScheme.indicator.pressed
    colorScheme.status.info
    colorScheme.status.read
    colorScheme.background.empty.level100To250.hover
    colorScheme.background.empty.level100To250.pressed
    colorScheme.background.empty.level300To350.hover
    colorScheme.background.empty.level300To350.pressed
    colorScheme.background.filled.level100To250.default
    colorScheme.background.filled.level100To250.focus
    colorScheme.background.filled.level100To250.hover
    colorScheme.background.filled.level100To250.pressed
    colorScheme.background.filled.level300To350.default
    colorScheme.background.filled.level300To350.focus
    colorScheme.background.filled.level300To350.hover
    colorScheme.background.filled.level300To350.pressed
    colorScheme.border.on
    colorScheme.border.base
    colorScheme.border.accent
    colorScheme.border.transparent
    colorScheme.text.placeholder
    colorScheme.text.active

--- packages/theme-data/src/baseTheme/system/colorScheme.js
+++ packages/theme-data/src/baseTheme/system/colorScheme.js
@@ -1,41 +1,371 @@
-import { COLOR } from "../../consts/types";
+import { COLOR, OPACITY } from "../../consts/types";
 
 export default {
-  accentColor: { type: COLOR },
-  baseColor: { type: COLOR },
-  highShadowColor: { type: COLOR },
-  iconColor: { type: COLOR },
-  lowShadowColor: { type: COLOR },
-  surfaceLevel100Color: { type: COLOR },
-  surfaceLevel200Color: { type: COLOR },
-  surfaceLevel250Color: { type: COLOR },
-  surfaceLevel300Color: { type: COLOR },
-  surfaceLevel350Color: { type: COLOR },
-  "component.backgroundColor": { type: COLOR },
-  textColor: { type: COLOR },
-  textColorDim: { type: COLOR },
-  infoColor: {
+  accentColor: {
+    type: COLOR,
+    metadata: {
+      deprecated: { equivalent: "colorScheme.reference.accent" }
+    }
+  },
+  "reference.accent": { type: COLOR },
+  baseColor: {
+    type: COLOR,
+    metadata: {
+      deprecated: { equivalent: "colorScheme.reference.base" }
+    }
+  },
+  "reference.base": { type: COLOR },
+  surfaceLevel100Color: {
+    type: COLOR,
+    metadata: {
+      deprecated: { equivalent: "colorScheme.surface.level100" }
+    }
+  },
+  "surface.level100": { type: COLOR },
+  surfaceLevel200Color: {
+    type: COLOR,
+    metadata: {
+      deprecated: { equivalent: "colorScheme.surface.level200" }
+    }
+  },
+  "surface.level200": { type: COLOR },
+  surfaceLevel250Color: {
+    type: COLOR,
+    metadata: {
+      deprecated: { equivalent: "colorScheme.surface.level250" }
+    }
+  },
+  "surface.level250": { type: COLOR },
+  surfaceLevel300Color: {
+    type: COLOR,
+    metadata: {
+      deprecated: { equivalent: "colorScheme.surface.level300" }
+    }
+  },
+  "surface.level300": { type: COLOR },
+  surfaceLevel350Color: {
+    type: COLOR,
+    metadata: {
+      deprecated: { equivalent: "colorScheme.surface.level350" }
+    }
+  },
+  "surface.level350": { type: COLOR },
+  "background.selected": { type: COLOR },
+  "background.on.default": { type: COLOR },
+  "background.on.focus": { type: COLOR },
+  "background.on.hover": { type: COLOR },
+  "background.on.pressed": { type: COLOR },
+  "background.empty.level100To250.hover": { type: COLOR },
+  "background.empty.level100To250.pressed": { type: COLOR },
+  "background.filled.level100To250.default": {
+    type: COLOR,
     value: {
-      ref: "basics.colors.autodeskBlue400"
+      ref: "colorScheme.reference.base"
+    },
+    transform: {
+      alpha: 0.15
+    }
+  },
+  "background.filled.level100To250.hover": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.reference.base"
+    },
+    transform: {
+      alpha: 0.25
+    }
+  },
+  "background.filled.level100To250.focus": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.reference.base"
+    },
+    transform: {
+      alpha: 0.15
+    }
+  },
+  "background.filled.level100To250.pressed": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.reference.base"
+    },
+    transform: {
+      alpha: 0.35
+    }
+  },
+  "background.empty.level300To350.hover": {
+    type: COLOR,
+    value: {
+      ref: "basics.colors.primary.white"
+    },
+    transform: {
+      alpha: 0.3
+    }
+  },
+  "background.empty.level300To350.pressed": {
+    type: COLOR,
+    value: {
+      ref: "basics.colors.primary.white"
+    },
+    transform: {
+      alpha: 0.5
+    }
+  },
+  "background.filled.level300To350.default": {
+    type: COLOR,
+    value: {
+      ref: "basics.colors.primary.white"
+    },
+    transform: {
+      alpha: 0.5
+    }
+  },
+  "background.filled.level300To350.hover": {
+    type: COLOR,
+    value: {
+      ref: "basics.colors.primary.white"
+    },
+    transform: {
+      alpha: 0.7
+    }
+  },
+  "background.filled.level300To350.focus": {
+    type: COLOR,
+    value: {
+      ref: "basics.colors.primary.white"
+    },
+    transform: {
+      alpha: 0.5
+    }
+  },
+  "background.filled.level300To350.pressed": {
+    type: COLOR,
+    value: {
+      ref: "basics.colors.primary.white"
+    },
+    transfrom: {
+      alpha: 0.9
+    }
+  },
+  "border.base": {
+    value: {
+      ref: "colorScheme.reference.base"
+    },
+    transform: {
+      alpha: 0.5
+    },
+    type: COLOR
+  },
+  "border.accent": { type: COLOR },
+  "border.on": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.reference.accent"
+    },
+    transform: {
+      alpha: 0.5
+    }
+  },
+  "halo.focus": {
+    value: {
+      ref: "colorScheme.reference.accent"
+    },
+    transform: {
+      alpha: 0.35
+    },
+    type: COLOR
+  },
+  "halo.hover": {
+    value: {
+      ref: "colorScheme.reference.base"
+    },
+    transform: {
+      alpha: 0.15
+    },
+    type: COLOR
+  },
+  "halo.pressed": {
+    value: {
+      ref: "colorScheme.reference.base"
+    },
+    transform: {
+      alpha: 0.25
+    },
+    type: COLOR
+  },
+  highShadowColor: {
+    type: COLOR,
+    metadata: {
+      deprecated: { equivalent: "colorScheme.shadow.high" }
+    }
+  },
+  "shadow.high": { type: COLOR },
+  lowShadowColor: {
+    type: COLOR,
+    metadata: {
+      deprecated: { equivalent: "colorScheme.shadow.low" }
+    }
+  },
+  "shadow.low": { type: COLOR },
+  textColor: {
+    type: COLOR,
+    metadata: {
+      deprecated: { equivalent: "colorScheme.text.default" }
+    }
+  },
+  "text.default": { type: COLOR },
+  textColorDim: {
+    type: COLOR,
+    metadata: {
+      deprecated: { equivalent: "colorScheme.text.dim" }
+    }
+  },
+  "text.dim": { type: COLOR },
+  "text.placeholder": {
+    type: COLOR,
+    value: {
+      ref: "basics.colors.text.againstLight"
+    },
+    transform: {
+      alpha: 0.4
+    }
+  },
+  "text.active": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.reference.accent"
+    }
+  },
+  iconColor: {
+    type: COLOR,
+    metadata: {
+      deprecated: { equivalent: "colorScheme.icon.default" }
+    }
+  },
+  "icon.default": { type: COLOR },
+  "icon.active": {
+    value: {
+      ref: "colorScheme.reference.accent"
+    },
+    type: COLOR
+  },
+  "icon.focus": { type: COLOR },
+  "icon.hover": { type: COLOR },
+  "icon.pressed": { type: COLOR },
+  "indicator.default": { type: COLOR },
+  "indicator.focus": { type: COLOR },
+  "indicator.hover": { type: COLOR },
+  "indicator.pressed": {
+    value: {
+      ref: "colorScheme.reference.accent"
     },
     type: COLOR
   },
   errorColor: {
     value: {
-      ref: "basics.colors.red500"
+      ref: "colorScheme.status.error"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "colorScheme.status.error"
+      }
+    },
+    type: COLOR
+  },
+  "status.error": {
+    value: {
+      ref: "basics.colors.secondary.red.500"
     },
     type: COLOR
   },
   successColor: {
     value: {
-      ref: "basics.colors.green500"
+      ref: "colorScheme.status.success"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "colorScheme.status.success"
+      }
+    },
+    type: COLOR
+  },
+  "status.success": {
+    value: {
+      ref: "basics.colors.secondary.green.500"
     },
     type: COLOR
   },
   warningColor: {
     value: {
-      ref: "basics.colors.yellowOrange500"
+      ref: "colorScheme.status.warning"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "colorScheme.status.warning"
+      }
     },
     type: COLOR
+  },
+  "status.warning": {
+    value: {
+      ref: "basics.colors.secondary.yellowOrange.500"
+    },
+    type: COLOR
+  },
+  infoColor: {
+    value: {
+      ref: "colorScheme.status.info"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "colorScheme.status.info"
+      }
+    },
+    type: COLOR
+  },
+  "status.info": {
+    value: {
+      ref: "colorScheme.reference.accent"
+    },
+    type: COLOR
+  },
+  "status.read": {
+    value: {
+      ref: "colorScheme.reference.base"
+    },
+    transform: {
+      alpha: 0.4
+    },
+    type: COLOR
+  },
+  "divider.lightweight": { type: COLOR },
+  "divider.heavyweight": { type: COLOR },
+  "opacity.mask": {
+    type: COLOR,
+    value: {
+      ref: "basics.colors.tertiary.pink.500"
+    },
+    transform: {
+      alpha: 0
+    }
+  },
+  "opacity.transparent": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.reference.base"
+    },
+    transform: {
+      alpha: 0
+    }
+  },
+  "opacity.disabled": {
+    value: 0.4,
+    type: OPACITY
+  },
+  "component.backgroundColor": {
+    type: COLOR,
+    metadata: {
+      deprecated: { equivalent: "colorScheme.surface.level" }
+    }
   }
 };
--- packages/theme-data/src/colorSchemes/lightGray/system/colorScheme.js
+++ packages/theme-data/src/colorSchemes/lightGray/system/colorScheme.js
@@ -1,74 +1,239 @@
 export default {
   accentColor: {
     value: {
-      ref: "basics.colors.autodeskBlue500"
+      ref: "colorScheme.reference.accent"
+    }
+  },
+  "reference.accent": {
+    value: {
+      ref: "basics.colors.primary.autodeskBlue.500"
     }
   },
   baseColor: {
     value: {
-      ref: "basics.colors.charcoal600"
+      ref: "colorScheme.reference.base"
+    }
+  },
+  "reference.base": {
+    value: {
+      ref: "basics.colors.primary.charcoal.600"
     }
   },
   "component.backgroundColor": {
     value: {
-      ref: "basics.colors.white"
+      ref: "basics.colors.primary.white"
     }
   },
   highShadowColor: {
     value: {
-      ref: "basics.colors.surfaceLightGrayShadow"
-    },
-    transform: {
-      alpha: 0.25
+      ref: "colorScheme.shadow.high"
+    }
+  },
+  "shadow.high": {
+    value: {
+      ref: "basics.colors.shadow.lightGray"
     }
   },
   iconColor: {
-    value: { ref: "basics.colors.iconLightGrayColor" }
+    value: { ref: "colorScheme.icon.default" }
+  },
+  "icon.default": {
+    value: { ref: "basics.colors.icon.lightGray" }
   },
   lowShadowColor: {
     value: {
-      ref: "basics.colors.surfaceLightGrayShadow"
-    },
-    transform: {
-      alpha: 0.25
+      ref: "colorScheme.shadow.low"
+    }
+  },
+  "shadow.low": {
+    value: {
+      ref: "basics.colors.shadow.lightGray"
     }
   },
   surfaceLevel100Color: {
     value: {
-      ref: "basics.colors.surfaceLightGrayLevel100"
+      ref: "colorScheme.surface.level100"
+    }
+  },
+  "surface.level100": {
+    value: {
+      ref: "basics.colors.surface.lightGray.100"
     }
   },
   surfaceLevel200Color: {
     value: {
-      ref: "basics.colors.surfaceLightGrayLevel200"
+      ref: "colorScheme.surface.level200"
+    }
+  },
+  "surface.level200": {
+    value: {
+      ref: "basics.colors.surface.lightGray.200"
     }
   },
   surfaceLevel250Color: {
     value: {
-      ref: "basics.colors.surfaceLightGrayLevel250"
+      ref: "colorScheme.surface.level250"
+    }
+  },
+  "surface.level250": {
+    value: {
+      ref: "basics.colors.surface.lightGray.250"
     }
   },
   surfaceLevel300Color: {
     value: {
-      ref: "basics.colors.surfaceLightGrayLevel300"
+      ref: "colorScheme.surface.level300"
+    }
+  },
+  "surface.level300": {
+    value: {
+      ref: "basics.colors.surface.lightGray.300"
     }
   },
   surfaceLevel350Color: {
     value: {
-      ref: "basics.colors.surfaceLightGrayLevel350"
+      ref: "colorScheme.surface.level350"
+    }
+  },
+  "surface.level350": {
+    value: {
+      ref: "basics.colors.surface.lightGray.350"
     }
   },
   textColor: {
     value: {
-      ref: "basics.colors.textAgainstLight"
+      ref: "colorScheme.text.default"
+    }
+  },
+  "text.default": {
+    value: {
+      ref: "basics.colors.text.againstLight"
     }
   },
   textColorDim: {
     value: {
-      ref: "basics.colors.textAgainstLight"
+      ref: "colorScheme.text.dim"
+    }
+  },
+  "text.dim": {
+    value: {
+      ref: "basics.colors.text.againstLight"
     },
     transform: {
       alpha: 0.7
+    }
+  },
+  "background.empty.level100To250.hover": {
+    value: {
+      ref: "colorScheme.reference.base"
+    },
+    transform: {
+      alpha: 0.08
+    }
+  },
+  "background.empty.level100To250.pressed": {
+    value: {
+      ref: "colorScheme.reference.base"
+    },
+    transform: {
+      alpha: 0.2
+    }
+  },
+  "background.on.default": {
+    value: {
+      ref: "basics.colors.primary.autodeskBlue.100"
+    },
+    transform: {
+      alpha: 0.6
+    }
+  },
+  "background.on.focus": {
+    value: {
+      ref: "basics.colors.primary.autodeskBlue.100"
+    },
+    transform: {
+      alpha: 0.6
+    }
+  },
+  "background.on.hover": {
+    value: {
+      ref: "basics.colors.primary.autodeskBlue.100"
+    },
+    transform: {
+      alpha: 0.7
+    }
+  },
+  "background.on.pressed": {
+    value: {
+      ref: "basics.colors.primary.autodeskBlue.100"
+    },
+    transform: {
+      alpha: 0.7
+    }
+  },
+  "background.selected": {
+    value: {
+      ref: "colorScheme.reference.accent"
+    },
+    transform: {
+      alpha: 0.15
+    }
+  },
+  "border.accent": {
+    value: {
+      ref: "colorScheme.reference.accent"
+    }
+  },
+  "icon.focus": {
+    value: {
+      ref: "basics.colors.icon.lightGray"
+    }
+  },
+  "icon.hover": {
+    value: {
+      ref: "basics.colors.primary.charcoal.900"
+    }
+  },
+  "icon.pressed": {
+    value: {
+      ref: "basics.colors.primary.charcoal.900"
+    }
+  },
+  "divider.lightweight": {
+    value: {
+      ref: "basics.colors.primary.charcoal.900"
+    },
+    transform: {
+      alpha: 0.1
+    }
+  },
+  "divider.heavyweight": {
+    value: {
+      ref: "basics.colors.primary.charcoal.900"
+    },
+    transform: {
+      alpha: 0.25
+    }
+  },
+  "indicator.default": {
+    value: {
+      ref: "basics.colors.primary.charcoal.700"
+    },
+    transform: {
+      alpha: 0.7
+    }
+  },
+  "indicator.focus": {
+    value: {
+      ref: "basics.colors.primary.charcoal.700"
+    },
+    transform: {
+      alpha: 0.7
+    }
+  },
+  "indicator.hover": {
+    value: {
+      ref: "basics.colors.primary.charcoal.700"
     }
   }
 };
