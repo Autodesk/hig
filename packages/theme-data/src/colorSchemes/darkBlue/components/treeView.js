7462d7242af79a25ec6e732b67caee90ccf52563
Wilbert Mui <wmui51@yahoo.com>
2020-02-18 11:37:40 -0800

    feat: add treeView theme-data

---
+++ packages/theme-data/src/colorSchemes/darkBlue/components/treeView.js
@@ -0,0 +1,7 @@
+export default {
+  "treeView.guideLine.backgroundColor": {
+    transform: {
+      alpha: 0.4
+    }
+  }
+};
--- packages/theme-data/src/densities/mediumDensity/unresolvedRoles.js
+++ packages/theme-data/src/densities/mediumDensity/unresolvedRoles.js
@@ -2,11 +2,13 @@ import mapKeys from "../../utils/mapKeys";
 import system from "./system";
 import button from "./components/button";
 import modal from "./components/modal";
+import treeView from "./components/treeView";
 import typography from "./components/typography";
 
 const mediumDensityThemeConfig = Object.assign(
   { ...button },
   modal,
+  treeView,
   typography,
   mapKeys(system.density, key => `density.${key}`)
 );
