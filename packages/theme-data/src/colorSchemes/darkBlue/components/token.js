e4fb5ffb8507f8322edce24f3bf20e51584a7f0b
Wilbert Mui <wmui51@yahoo.com>
2020-02-18 14:57:28 -0800

    feat: add token theme-data

---
+++ packages/theme-data/src/colorSchemes/darkBlue/components/token.js
@@ -0,0 +1,22 @@
+export default {
+  "token.default.backgroundColor": {
+    transform: {
+      alpha: 0.2
+    }
+  },
+  "token.hover.backgroundColor": {
+    transform: {
+      alpha: 0.3
+    }
+  },
+  "token.pressed.backgroundColor": {
+    transform: {
+      alpha: 0.4
+    }
+  },
+  "token.selected.backgroundColor": {
+    transform: {
+      alpha: 0.4
+    }
+  }
+};
