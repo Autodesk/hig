16e19b581f8d68644139b5792ab03d0bb3179644
Wilbert Mui <wmui51@yahoo.com>
2020-02-18 14:57:28 -0800

    feat: add token theme-data

--- packages/theme-data/src/baseTheme/unresolvedRoles.js
+++ packages/theme-data/src/baseTheme/unresolvedRoles.js
@@ -25,6 +25,7 @@ import tabs from "./components/tabs";
 import textarea from "./components/textarea";
 import textLink from "./components/textLink";
 import thumbnail from "./components/thumbnail";
+import token from "./components/token";
 import tooltip from "./components/tooltip";
 import topNav from "./components/topNav";
 import treeView from "./components/treeView";
@@ -67,6 +68,7 @@ const baseThemeConfig = extendTheme(
     textarea,
     textLink,
     thumbnail,
+    token,
     tooltip,
     topNav,
     treeView,
