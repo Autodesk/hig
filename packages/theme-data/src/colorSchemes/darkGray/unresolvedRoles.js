be3100de8ff3a9f302d35a2a9ab989d69ef44da6
Wilbert Mui <wmui51@yahoo.com>
2020-02-25 16:18:53 -0800

    feat: add tile theme-data

--- packages/theme-data/src/colorSchemes/darkGray/unresolvedRoles.js
+++ packages/theme-data/src/colorSchemes/darkGray/unresolvedRoles.js
@@ -8,6 +8,7 @@ import iconButton from "./components/iconButton";
 import input from "./components/input";
 import slider from "./components/slider";
 import thumbnail from "./components/thumbnail";
+import tile from "./components/tile";
 import tooltip from "./components/tooltip";
 
 const darkGrayThemeConfig = extendTheme(
@@ -21,6 +22,7 @@ const darkGrayThemeConfig = extendTheme(
     input,
     slider,
     thumbnail,
+    tile,
     tooltip
   )
 );
