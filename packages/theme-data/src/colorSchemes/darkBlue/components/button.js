be9c4c07d81743e9c83d6662af659805982c3f04
Wilbert Mui <wmui51@yahoo.com>
2020-03-19 16:10:58 -0700

    feat: add/rename/deprecate roles for button
    Renamed:
    button.fontWeight => button.label.fontWeight
    button.fontFamily => button.label.fontFamily
    button.fontSize => button.label.fontSize
    button.gutter => button.gutterWidth
    button.horizontalPadding => button.paddingHorizontal
    button.minimumWidth => button.minWidth
    button.verticalPadding => button.paddingVertical
    button.hover.halo.width => button.hover.haloWidth
    button.hover.halo.color => button.hover.haloColor
    button.focus.halo.width => button.haloWidth
    button.focus.halo.color => button.focus.haloColor
    button.pressed.halo.width => button.pressed.haloWidth
    button.pressed.halo.color => button.pressed.haloColor
    button.outline.backgroundColor => button.outline.default.backgroundColor
    button.outline.borderColor => button.outline.default.borderColor
    button.outline.hover.backgroundColor
    button.outline.hover.borderColor
    button.outline.icon.color => button.outline.iconColor
    button.solid.backgroundColor => button.solid.default.backgroundColor
    button.solid.borderColor => button.solid.default.borderColor
    button.flat.backgroundColor => button.flat.default.backgroundColor
    button.flat.borderColor => button.flat.default.borderColor
    
    Deprecate:
    button.lineHeight
    button.halo.width
    button.halo.color
    button.outline.focus.halo.color
    button.outline.focus.icon.color
    button.outline.focus.textColor
    button.outline.halo.color
    button.outline.hover.halo.color
    button.outline.hover.icon.color
    button.outline.hover.textColor
    button.outline.pressed.halo.color
    button.outline.pressed.haloWidth
    button.outline.pressed.icon.color
    button.outline.pressed.textColor
    button.outline.textColor
    button.solid.halo.color
    button.solid.textColor
    button.solid.icon.color
    button.solid.hover.halo.color
    button.solid.hover.textColor
    button.solid.hover.icon.color
    button.solid.focus.halo.color
    button.solid.focus.textColor
    button.solid.focus.icon.color
    button.solid.pressed.haloWidth
    button.solid.pressed.halo.color
    button.solid.pressed.textColor
    button.solid.pressed.icon.color
    button.flat.halo.color
    button.flat.textColor
    button.flat.icon.color
    button.flat.hover.halo.color
    button.flat.hover.textColor
    button.flat.hover.icon.color
    button.flat.focus.halo.color
    button.flat.focus.textColor
    button.flat.focus.icon.color
    button.flat.pressed.haloWidth
    button.flat.pressed.halo.color
    button.flat.pressed.textColor
    button.flat.pressed.icon.color
    
    Added:
    
    button.outline.label.fontColor
    button.outline.indicatorColor
    button.outline.iconColor
    button.solid.label.fontColor
    button.solid.iconColor
    button.solid.indicatorColor
    button.flat.label.fontColor
    button.flat.iconColor
    button.flat.indicatorColor

--- packages/theme-data/src/colorSchemes/darkBlue/components/button.js
+++ packages/theme-data/src/colorSchemes/darkBlue/components/button.js
@@ -1,91 +1,11 @@
 export default {
-  /* solid */
-  "button.solid.backgroundColor": {
-    value: {
-      ref: "basics.colors.autodeskBlue500"
-    }
-  },
-  "button.solid.hover.backgroundColor": {
-    value: {
-      ref: "button.solid.backgroundColor"
-    }
-  },
-  "button.solid.hover.halo.color": {
-    value: {
-      ref: "basics.colors.white"
-    },
-    transform: {
-      alpha: 0.15
-    }
-  },
-  "button.solid.focus.backgroundColor": {
-    value: {
-      ref: "button.solid.backgroundColor"
-    }
-  },
-  "button.solid.focus.halo.color": {
-    value: {
-      ref: "colorScheme.accentColor"
-    },
-    transform: {
-      alpha: 0.35
-    }
-  },
-  /* outline */
-  "button.outline.textColor": {
-    value: {
-      ref: "colorScheme.textColor"
-    }
-  },
-  "button.outline.icon.color": {
-    value: {
-      ref: "colorScheme.textColor"
-    }
-  },
-  "button.outline.borderColor": {
-    value: {
-      ref: "basics.colors.white"
-    },
-    transform: {
-      alpha: 0.35
-    }
-  },
-  "button.outline.hover.halo.color": {
-    value: {
-      ref: "basics.colors.white"
-    },
-    transform: {
-      alpha: 0.15
-    }
-  },
-  "button.outline.focus.borderColor": {
-    value: {
-      ref: "colorScheme.accentColor"
-    }
-  },
-  "button.outline.focus.halo.color": {
-    value: {
-      ref: "button.solid.focus.halo.color"
-    }
-  },
-  /* flat */
-  "button.flat.textColor": {
+  "button.flat.label.fontColor": {
     value: "#6DD2FF"
   },
-  "button.flat.icon.color": {
+  "button.flat.iconColor": {
     value: "#6DD2FF"
   },
-  "button.flat.hover.halo.color": {
-    value: {
-      ref: "basics.colors.white"
-    },
-    transform: {
-      alpha: 0.15
-    }
-  },
-  "button.flat.focus.halo.color": {
-    value: {
-      ref: "button.solid.focus.halo.color"
-    }
+  "button.flat.indicatorColor": {
+    value: "#6DD2FF"
   }
 };
