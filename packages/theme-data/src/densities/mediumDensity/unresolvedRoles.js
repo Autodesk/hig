7177c6d304a04e853f5219977db62425cc0f7469
Wilbert Mui <wmui51@yahoo.com>
2020-03-19 16:10:58 -0700

    feat: add/rename/deprecate roles for button
    Renamed:
    button.fontWeight => button.label.fontWeight
    button.fontFamily => button.label.fontFamily
    button.fontSize => button.label.fontSize
    button.gutter => button.gutterWidth
    button.horizontalPadding => button.paddingHorizontal
    button.minimumWidth => button.minWidth
    button.verticalPadding => button.paddingVertical
    button.hover.halo.width => button.hover.haloWidth
    button.hover.halo.color => button.hover.haloColor
    button.focus.halo.width => button.haloWidth
    button.focus.halo.color => button.focus.haloColor
    button.pressed.halo.width => button.pressed.haloWidth
    button.pressed.halo.color => button.pressed.haloColor
    button.outline.backgroundColor => button.outline.default.backgroundColor
    button.outline.borderColor => button.outline.default.borderColor
    button.outline.hover.backgroundColor
    button.outline.hover.borderColor
    button.outline.icon.color => button.outline.iconColor
    button.solid.backgroundColor => button.solid.default.backgroundColor
    button.solid.borderColor => button.solid.default.borderColor
    button.flat.backgroundColor => button.flat.default.backgroundColor
    button.flat.borderColor => button.flat.default.borderColor
    
    Deprecate:
    button.lineHeight
    button.halo.width
    button.halo.color
    button.outline.focus.halo.color
    button.outline.focus.icon.color
    button.outline.focus.textColor
    button.outline.halo.color
    button.outline.hover.halo.color
    button.outline.hover.icon.color
    button.outline.hover.textColor
    button.outline.pressed.halo.color
    button.outline.pressed.haloWidth
    button.outline.pressed.icon.color
    button.outline.pressed.textColor
    button.outline.textColor
    button.solid.halo.color
    button.solid.textColor
    button.solid.icon.color
    button.solid.hover.halo.color
    button.solid.hover.textColor
    button.solid.hover.icon.color
    button.solid.focus.halo.color
    button.solid.focus.textColor
    button.solid.focus.icon.color
    button.solid.pressed.haloWidth
    button.solid.pressed.halo.color
    button.solid.pressed.textColor
    button.solid.pressed.icon.color
    button.flat.halo.color
    button.flat.textColor
    button.flat.icon.color
    button.flat.hover.halo.color
    button.flat.hover.textColor
    button.flat.hover.icon.color
    button.flat.focus.halo.color
    button.flat.focus.textColor
    button.flat.focus.icon.color
    button.flat.pressed.haloWidth
    button.flat.pressed.halo.color
    button.flat.pressed.textColor
    button.flat.pressed.icon.color
    
    Added:
    
    button.outline.label.fontColor
    button.outline.indicatorColor
    button.outline.iconColor
    button.solid.label.fontColor
    button.solid.iconColor
    button.solid.indicatorColor
    button.flat.label.fontColor
    button.flat.iconColor
    button.flat.indicatorColor

--- packages/theme-data/src/densities/mediumDensity/unresolvedRoles.js
+++ packages/theme-data/src/densities/mediumDensity/unresolvedRoles.js
@@ -1,12 +1,10 @@
 import mapKeys from "../../utils/mapKeys";
 import system from "./system";
-import button from "./components/button";
 import modal from "./components/modal";
 import treeView from "./components/treeView";
 import typography from "./components/typography";
 
 const mediumDensityThemeConfig = Object.assign(
-  { ...button },
   modal,
   treeView,
   typography,
