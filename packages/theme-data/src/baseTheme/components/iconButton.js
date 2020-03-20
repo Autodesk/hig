558587ea42c55dba74a3375f6f4ca194f361b260
Wilbert Mui <wmui51@yahoo.com>
2020-03-20 11:55:27 -0700

    feat: add/rename/deprecate icon-button roles
    Renamed:
    iconButton.focus.halo.color => iconButton.focus.haloColor
    iconButton.focus.halo.width => iconButton.haloWidth
    iconButton.focus.iconColor => iconButton.dynamic.focus.iconColor
    iconButton.focus.on.iconColor => iconButton.dynamic.on.focus.iconColor
    iconButton.hover.iconColor => iconButton.dynamic.hover.iconColor
    iconButton.hover.on.iconColor => iconButton.dynamic.on.hover.iconColor
    iconButton.iconColor => iconButton.dynamic.default.iconColor
    iconButton.on.iconColor => iconButton.dynamic.on.default.iconColor
    iconButton.pressed.iconColor => iconButton.dynamic.pressed.iconColor
    iconButton.pressed.level100To250.backgroundColor => iconButton.dynamic.pressed.100To250BackgroundColor
    iconButton.pressed.level300To350.backgroundColor => iconButton.dynamic.pressed.300To350BackgroundColor
    iconButton.pressed.on.iconColor => iconButton.dynamic.on.pressed.iconColor
    iconButton.static.hover.level100To250.backgroundColor => iconButton.static.hover.100To250BackgroundColor
    iconButton.static.hover.level300To350.backgroundColor => iconButton.static.hover.300To350BackgroundColor
    iconButton.static.on.backgroundColor => iconButton.static.on.default.backgroundColor
    iconButton.static.on.borderColor => iconButton.static.on.default.borderColor
    iconButton.static.on.focus.borderColor => iconButton.static.on.focus.borderColor
    iconButton.static.on.hover.borderColor => iconButton.static.on.hover.borderColor
    iconButton.static.on.hover.level100To250.backgroundColor => iconButton.static.on.hover.100To250BackgroundColor
    iconButton.static.on.hover.level300To350.backgroundColor => iconButton.static.on.hover.300To350BackgroundColor
    
    Deprecate:
    iconButton.pressed.backgroundColor
    
    Added:
    iconButton.borderWidth
    iconButton.static.on.focus.backgroundColor
    iconButton.static.on.focus.borderColor
    iconButton.static.default.backgroundColor
    iconButton.static.default.borderColor
    iconButton.static.pressed.100To250BackgroundColor
    iconButton.static.pressed.300To350BackgroundColor
    iconButton.dynamic.default.backgroundColor
    iconButton.dynamic.focus.backgroundColor
    iconButton.dynamic.hover.backgroundColor
    iconButton.static.focus.backgroundColor
    iconButton.static.focus.borderColor
    iconButton.static.hover.borderColor*
    iconButton.static.on.hover.backgroundColor
    iconButton.static.on.pressed.backgroundColor
    iconButton.static.on.pressed.borderColor
    iconButton.static.pressed.borderColor
    iconButton.dynamic.default.iconColor
    iconButton.dynamic.focus.iconColor
    iconButton.dynamic.hover.iconColor
    iconButton.dynamic.on.default.iconColor
    iconButton.dynamic.on.focus.iconColor
    iconButton.dynamic.on.hover.iconColor
    iconButton.dynamic.on.pressed.iconColor
    iconButton.dynamic.pressed.iconColor
    iconButton.dynamic.pressed.100To250BackgroundColor
    iconButton.dynamic.pressed.300To350BackgroundColor

--- packages/theme-data/src/baseTheme/components/iconButton.js
+++ packages/theme-data/src/baseTheme/components/iconButton.js
@@ -1,108 +1,248 @@
-import { BORDER_RADIUS, COLOR, LENGTH } from "../../consts/types";
+import { BORDER_RADIUS, COLOR, LENGTH, BORDER_WIDTH } from "../../consts/types";
 
 export default {
-  "iconButton.iconColor": {
-    type: COLOR,
-    value: {
-      ref: "colorScheme.baseColor"
-    }
-  },
-  "iconButton.on.iconColor": {
-    type: COLOR,
-    value: {
-      ref: "colorScheme.accentColor"
-    }
-  },
   "iconButton.borderRadius": {
     type: BORDER_RADIUS,
     value: {
       ref: "button.borderRadius"
     }
   },
-  "iconButton.hover.iconColor": {
+  "iconButton.focus.haloColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.textColor"
-    }
-  },
-  "iconButton.hover.on.iconColor": {
-    type: COLOR,
-    value: {
-      ref: "basics.colors.autodeskBlue700"
-    }
-  },
-  "iconButton.focus.iconColor": {
-    type: COLOR,
-    value: {
-      ref: "iconButton.iconColor"
-    }
-  },
-  "iconButton.focus.on.iconColor": {
-    type: COLOR,
-    value: {
-      ref: "iconButton.on.iconColor"
+      ref: "button.focus.haloColor"
     }
   },
   "iconButton.focus.halo.color": {
     type: COLOR,
     value: {
-      ref: "button.focus.halo.color"
+      ref: "iconButton.focus.haloColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.focus.haloColor"
+      }
+    }
+  },
+  "iconButton.focus.haloWidth": {
+    type: LENGTH,
+    value: {
+      ref: "button.haloWidth"
     }
   },
   "iconButton.focus.halo.width": {
     type: LENGTH,
     value: {
-      ref: "button.focus.halo.width"
+      ref: "iconButton.focus.haloWidth"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.focus.haloWidth"
+      }
     }
   },
-  "iconButton.pressed.iconColor": {
+  "iconButton.dyanmic.focus.iconColor": {
     type: COLOR,
     value: {
-      ref: "iconButton.hover.iconColor"
+      ref: "iconButton.dynamic.default.iconColor"
     }
   },
-  "iconButton.pressed.on.iconColor": {
+  "iconButton.focus.iconColor": {
     type: COLOR,
     value: {
-      ref: "iconButton.hover.on.iconColor"
+      ref: "iconButton.dyanmic.focus.iconColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.dyanmic.focus.iconColor"
+      }
+    }
+  },
+  "iconButton.dynamic.on.focus.iconColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.icon.active"
+    }
+  },
+  "iconButton.focus.on.iconColor": {
+    type: COLOR,
+    value: {
+      ref: "iconButton.dynamic.on.focus.iconColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.dynamic.on.focus.iconColor"
+      }
+    }
+  },
+  "iconButton.dynamic.hover.iconColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.icon.hover"
+    }
+  },
+  "iconButton.hover.iconColor": {
+    type: COLOR,
+    value: {
+      ref: "iconButton.dynamic.hover.iconColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.dynamic.hover.iconColor"
+      }
+    }
+  },
+  "iconButton.dynamic.on.hover.iconColor": {
+    type: COLOR,
+    value: {
+      ref: "basics.colors.primary.autodeskBlue.700"
+    }
+  },
+  "iconButton.hover.on.iconColor": {
+    type: COLOR,
+    value: {
+      ref: "iconButton.dynamic.on.hover.iconColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.dynamic.on.hover.iconColor"
+      }
+    }
+  },
+  "iconButton.dynamic.default.iconColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.icon.default"
+    }
+  },
+  "iconButton.iconColor": {
+    type: COLOR,
+    value: {
+      ref: "iconButton.dynamic.default.iconColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.dynamic.default.iconColor"
+      }
+    }
+  },
+  "iconButton.dynamic.on.default.iconColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.icon.active"
+    }
+  },
+  "iconButton.on.iconColor": {
+    type: COLOR,
+    value: {
+      ref: "iconButton.dynamic.on.default.iconColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.dynamic.on.default.iconColor"
+      }
     }
   },
   "iconButton.pressed.backgroundColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.baseColor"
+      ref: "iconButton.dynamic.pressed.100To250BackgroundColor"
     },
-    transform: {
-      alpha: 0.2
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.dynamic.pressed.100To250BackgroundColor"
+      }
+    }
+  },
+  "iconButton.dynamic.pressed.iconColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.icon.pressed"
+    }
+  },
+  "iconButton.pressed.iconColor": {
+    type: COLOR,
+    value: {
+      ref: "iconButton.dynamic.pressed.iconColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.dynamic.pressed.iconColor"
+      }
+    }
+  },
+  "iconButton.dynamic.pressed.100To250BackgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.background.empty.level100To250.pressed"
     }
   },
   "iconButton.pressed.level100To250.backgroundColor": {
     type: COLOR,
     value: {
-      ref: "iconButton.pressed.backgroundColor"
+      ref: "iconButton.dynamic.pressed.100To250BackgroundColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.dynamic.pressed.100To250BackgroundColor"
+      }
+    }
+  },
+  "iconButton.dynamic.pressed.300To350BackgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.background.empty.level300To350.pressed"
     }
   },
   "iconButton.pressed.level300To350.backgroundColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.component.backgroundColor"
+      ref: "iconButton.dynamic.pressed.300To350BackgroundColor"
     },
-    transform: {
-      alpha: 0.5
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.dynamic.pressed.300To350BackgroundColor"
+      }
     }
   },
-
-  // Static
-  "iconButton.static.hover.level100To250.backgroundColor": {
+  "iconButton.dynamic.on.pressed.iconColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.baseColor"
+      ref: "basics.colors.primary.autodeskBlue.700"
+    }
+  },
+  "iconButton.pressed.on.iconColor": {
+    type: COLOR,
+    value: {
+      ref: "iconButton.dynamic.on.pressed.iconColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.dynamic.on.pressed.iconColor"
+      }
+    }
+  },
+  "iconButton.static.hover.100To250.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.reference.base"
     },
     transform: {
       alpha: 0.1
     }
   },
-  "iconButton.static.hover.level300To350.backgroundColor": {
+  "iconButton.static.hover.level100To250.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "iconButton.static.hover.100To250.backgroundColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.static.hover.100To250.backgroundColor"
+      }
+    }
+  },
+  "iconButton.static.hover.300To350.backgroundColor": {
     type: COLOR,
     value: {
       ref: "colorScheme.component.backgroundColor"
@@ -111,7 +251,18 @@ export default {
       alpha: 0.3
     }
   },
-  "iconButton.static.on.backgroundColor": {
+  "iconButton.static.hover.level300To350.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "iconButton.static.hover.300To350.backgroundColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.static.hover.300To350.backgroundColor"
+      }
+    }
+  },
+  "iconButton.static.on.default.backgroundColor": {
     type: COLOR,
     value: {
       ref: "colorScheme.accentColor"
@@ -120,7 +271,18 @@ export default {
       alpha: 0.15
     }
   },
-  "iconButton.static.on.borderColor": {
+  "iconButton.static.on.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "iconButton.static.on.default.backgroundColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.static.on.default.backgroundColor"
+      }
+    }
+  },
+  "iconButton.static.on.default.borderColor": {
     type: COLOR,
     value: {
       ref: "colorScheme.accentColor"
@@ -129,19 +291,21 @@ export default {
       alpha: 0.5
     }
   },
-  "iconButton.static.on.hover.level100To250.backgroundColor": {
+  "iconButton.static.on.borderColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.accentColor"
+      ref: "iconButton.static.on.default.borderColor"
     },
-    transform: {
-      alpha: 0.35
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.static.on.default.borderColor"
+      }
     }
   },
-  "iconButton.static.on.hover.level300To350.backgroundColor": {
+  "iconButton.static.on.focus.borderColor": {
     type: COLOR,
     value: {
-      ref: "iconButton.static.on.hover.level100To250.backgroundColor"
+      ref: "colorScheme.reference.accent"
     }
   },
   "iconButton.static.on.hover.borderColor": {
@@ -150,13 +314,143 @@ export default {
       ref: "iconButton.static.on.borderColor"
     }
   },
-  "iconButton.static.on.focus.borderColor": {
+  "iconButton.static.on.hover.100To250BackgroundColor": {
     type: COLOR,
     value: {
-      ref: "iconButton.static.on.borderColor"
+      ref: "colorScheme.reference.accent"
     },
     transform: {
-      alpha: 1.0
+      alpha: 0.35
+    }
+  },
+  "iconButton.static.on.hover.level100To250.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "iconButton.static.on.hover.100To250BackgroundColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.static.on.hover.100To250BackgroundColor"
+      }
+    }
+  },
+  "iconButton.static.on.hover.300To350BackgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "iconButton.static.on.hover.100To250BackgroundColor"
+    }
+  },
+  "iconButton.static.on.hover.level300To350.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "iconButton.static.on.hover.300To350BackgroundColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "iconButton.static.on.hover.300To350BackgroundColor"
+      }
+    }
+  },
+  "iconButton.borderWidth": {
+    type: BORDER_WIDTH,
+    value: {
+      ref: "basics.borderRadii.small"
+    }
+  },
+  "iconButton.static.on.focus.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.background.on.focus"
+    }
+  },
+  "iconButton.static.default.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.opacity.transparent"
+    }
+  },
+  "iconButton.static.default.borderColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.opacity.transparent"
+    }
+  },
+  "iconButton.static.pressed.100To250BackgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.background.empty.level100To250.pressed"
+    }
+  },
+  "iconButton.static.pressed.300To350BackgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.background.empty.level300To350.pressed"
+    }
+  },
+  "iconButton.dynamic.default.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.opacity.transparent"
+    }
+  },
+  "iconButton.dynamic.focus.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.opacity.transparent"
+    }
+  },
+  "iconButton.dynamic.hover.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.opacity.transparent"
+    }
+  },
+  "iconButton.static.focus.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.opacity.transparent"
+    }
+  },
+  "iconButton.static.focus.borderColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.opacity.transparent"
+    }
+  },
+  "iconButton.static.hover.borderColor*": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.opacity.transparent"
+    }
+  },
+  "iconButton.static.on.hover.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.background.on.hover"
+    }
+  },
+  "iconButton.static.on.pressed.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.background.on.pressed"
+    }
+  },
+  "iconButton.static.on.pressed.borderColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.border.on"
+    }
+  },
+  "iconButton.static.pressed.borderColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.opacity.transparent"
+    }
+  },
+  "iconButton.dynamic.focus.iconColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.icon.focus"
     }
   }
 };
--- packages/theme-data/src/colorSchemes/darkBlue/components/iconButton.js
+++ packages/theme-data/src/colorSchemes/darkBlue/components/iconButton.js
@@ -1,71 +1,12 @@
 export default {
-  "iconButton.iconColor": {
-    value: "#a2a6b0"
-  },
-  "iconButton.hover.iconColor": {
-    value: "#d0d2d7"
-  },
-  "iconButton.hover.on.iconColor": {
+  "iconButton.dynamic.on.hover.iconColor": {
     value: {
-      ref: "basics.colors.autodeskBlue300"
+      ref: "basics.colors.primary.autodeskBlue.300"
     }
   },
-  "iconButton.focus.halo.color": {
-    transform: {
-      alpha: 0.35
-    }
-  },
-  "iconButton.pressed.backgroundColor": {
+  "iconButton.dynamic.on.pressed.iconColor": {
     value: {
-      ref: "colorScheme.surfaceLevel300Color"
-    },
-    transform: {
-      alpha: 0.7
-    }
-  },
-  "iconButton.pressed.level300To350.backgroundColor": {
-    value: {
-      ref: "basics.colors.darkBlue200"
-    },
-    transform: {
-      alpha: 0.2
-    }
-  },
-  // Static
-  "iconButton.static.hover.level100To250.backgroundColor": {
-    value: {
-      ref: "iconButton.pressed.backgroundColor"
-    },
-    transform: {
-      alpha: 0.3
-    }
-  },
-  "iconButton.static.hover.level300To350.backgroundColor": {
-    value: {
-      ref: "iconButton.pressed.level300To350.backgroundColor"
-    },
-    transform: {
-      alpha: 0.1
-    }
-  },
-  "iconButton.static.on.backgroundColor": {
-    transform: {
-      alpha: 0.25
-    }
-  },
-  "iconButton.static.on.borderColor": {
-    transform: {
-      alpha: 0.6
-    }
-  },
-  "iconButton.static.on.hover.level100To250.backgroundColor": {
-    transform: {
-      alpha: 0.45
-    }
-  },
-  "iconButton.static.on.focus.borderColor": {
-    value: {
-      ref: "basics.colors.autodeskBlue500"
+      ref: "basics.colors.primary.autodeskBlue.300"
     }
   }
 };
