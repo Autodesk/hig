d4e15452dbbd6c7402321f2322534c48147dc1a9
Wilbert Mui <wmui51@yahoo.com>
2020-03-24 16:22:28 -0700

    feat: add/rename/deprecate input roles
    Renamed
    input.active.halo.color => input.focus.haloColor
    input.borderBottomColor => input.line.default.borderBottomColor
    input.box.backgroundColor => input.box.default.backgroundColor
    input.box.borderBottomColor => input.box.default.borderBottomColor
    input.boxType.horizontalPadding => input.box.paddingHorizontal
    input.focus.borderBottomColor => input.line.focus.borderBottomColor
    input.fontColor => input.value.fontColor
    input.fontFamily => input.value.fontFamily
    input.fontSize => input.value.fontSize
    input.fontWeight => input.value.fontWeight
    input.halo.color => input.hover.haloColor
    input.halo.width => input.haloWidth
    input.height => input.minHeight
    input.highlightColor => input.value.highlightColor
    input.horizontalPadding => input.line.paddingHorizontal
    input.hover.borderBottomColor => input.line.hover.borderBottomColor
    input.lineHeight => input.value.lineHeight
    input.placeholder.fontColor => input.value.placeholderFontColor
    input.verticalPadding => input.paddingVertical
    
    Deprecate
    input.active.halo.width
    input.focus.halo.color
    input.focus.halo.width
    input.hover.halo.width
    
    Added
    input.pressed.haloColor
    input.box.default.borderColor
    input.box.focus.backgroundColor
    input.box.focus.borderColor
    input.box.hover.backgroundColor
    input.box.hover.borderColor
    input.box.pressed.backgroundColor
    input.box.pressed.borderColor
    input.box.focus.borderBottomColor
    input.box.hover.borderBottomColor
    input.box.pressed.borderBottomColor
    input.line.pressed.borderBottomColor
    input.indicator.default
    input.indicator.focus
    input.indicator.hover
    input.indicator.pressed

--- packages/theme-data/src/colorSchemes/darkGray/components/input.js
+++ packages/theme-data/src/colorSchemes/darkGray/components/input.js
@@ -1,10 +1,13 @@
 export default {
-  "input.halo.color": {
-    value: {
-      ref: "colorScheme.baseColor"
-    },
+  "input.box.default.backgroundColor": {
+    value: "#212121",
     transform: {
-      alpha: 0.15
+      opacity: 0.6
+    }
+  },
+  "input.box.default.borderBottomColor": {
+    value: {
+      ref: "basics.colors.primary.white"
     }
   }
 };
