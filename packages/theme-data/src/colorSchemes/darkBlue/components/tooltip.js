4179914b20e6f5f21f92f8284f9c905146ad01c9
Wilbert Mui <wmui51@yahoo.com>
2020-03-25 11:29:01 -0700

    feat: add/rename/deprecate tooltip roles
    Renamed:
    tooltip.textColor => tooltip.title.fontColor
    
    Deprecate:
    tooltip.borderColor
    
    Added:
    tooltip.description.fontColor
    tooltip.description.fontFamily
    tooltip.description.fontSize
    tooltip.description.fontWeight
    tooltip.description.lineHeight
    tooltip.divider.borderBottomColor
    tooltip.title.fontFamily
    tooltip.title.fontSize
    tooltip.title.fontWeight
    tooltip.title.lineHeight

--- packages/theme-data/src/colorSchemes/darkBlue/components/tooltip.js
+++ packages/theme-data/src/colorSchemes/darkBlue/components/tooltip.js
@@ -1,12 +1,23 @@
 export default {
   "tooltip.backgroundColor": {
     value: {
-      ref: "basics.colors.darkBlue200"
+      ref: "basics.colors.secondary.darkBlue.200"
     }
   },
-  "tooltip.textColor": {
+  "tooltip.title.fontColor": {
     value: {
-      ref: "basics.colors.textAgainstLight"
+      ref: "basics.colors.text.againstLight"
+    }
+  },
+  "tooltip.description.fontColor": {
+    value: "#3C3C3C"
+  },
+  "tooltip.divider.borderBottomColor": {
+    value: {
+      ref: "basics.colors.primary.black"
+    },
+    transform: {
+      alpha: 0.25
     }
   }
 };
