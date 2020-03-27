f8a1cb4d5f0867ba8c1b3a7d53f4c8e2e9efbb57
Wilbert Mui <wmui51@yahoo.com>
2020-03-26 17:02:25 -0700

    feat: add/rename/deprecate checkbox roles
    Renamed
    checkbox.height => checkbox.minHeight
    checkbox.iconColor => checkbox.indicatorColor
    checkbox.width => checkbox.minWidth
    checkbox.backgroundColor => checkbox.unchecked.default.backgroundColor
    checkbox.borderColor => checkbox.unchecked.default.borderColor
    checkbox.hover.borderColor => checkbox.unchecked.hover.borderColor
    checkbox.focus.borderColor => checkbox.unchecked.focus.borderColor
    checkbox.pressed.borderColor => checkbox.unchecked.pressed.borderColor
    checkbox.checked.borderColor => checkbox.checked.default.borderColor
    checkbox.checked.backgroundColor => checkbox.checked.default.backgroundColor
    checkbox.focus.checked.backgroundColor => checkbox.checked.focus.backgroundColor
    checkbox.focus.checked.borderColor => checkbox.checked.focus.borderColor
    
    Deprecate
    checkbox.halo.color [DEPRECATED] => checkbox.default.haloColor
    checkbox.halo.width [DEPRECATED] => checkbox.default.haloWidth
    checkbox.hover.halo.width [DEPRECATED] => checkbox.hover.haloWidth
    checkbox.hover.halo.color [DEPRECATED] => checkbox.hover.haloColor
    checkbox.focus.halo.color [DEPRECATED] => checkbox.focus.haloColor
    checkbox.focus.halo.width [DEPRECATED] => checkbox.focus.haloWidth
    checkbox.pressed.halo.color [DEPRECATED] => checkbox.pressed.haloColor
    checkbox.pressed.halo.width [DEPRECATED] => checkbox.pressed.haloWidth
    checkbox.checked.iconColor [DEPRECATED] => checkbox.indicatorColor
    checkbox.indeterminate.borderColor [DEPRECATED] => checkbox.checked.default.borderColor
    checkbox.indeterminate.iconColor [DEPRECATED] => checkbox.indicatorColor
    checkbox.indeterminate.backgroundColor [DEPRECATED] => checkbox.checked.default.backgroundColor
    checkbox.focus.indeterminate.borderColor [DEPRECATED] => checkbox.checked.focus.borderColor
    
    Added
    checkbox.focus.haloColor
    checkbox.hover.haloColor
    checkbox.pressed.haloColor
    checkbox.focus.haloWidth
    checkbox.hover.haloWidth
    checkbox.pressed.haloWidth
    checkbox.unchecked.hover.backgroundColor
    checkbox.unchecked.focus.backgroundColor
    checkbox.unchecked.pressed.backgroundColor
    checkbox.checked.hover.backgroundColor
    checkbox.checked.hover.borderColor
    checkbox.checked.pressed.backgroundColor
    checkbox.checked.pressed.borderColor

--- packages/theme-data/src/baseTheme/components/checkbox.js
+++ packages/theme-data/src/baseTheme/components/checkbox.js
@@ -1,190 +1,437 @@
 import { COLOR, BORDER_WIDTH, BORDER_RADIUS, LENGTH } from "../../consts/types";
 
 export default {
+  "checkbox.borderWidth": {
+    type: BORDER_WIDTH,
+    value: {
+      ref: "basics.borderWidths.small"
+    }
+  },
+  "checkbox.borderRadius": {
+    type: BORDER_RADIUS,
+    value: {
+      ref: "basics.borderRadii.none"
+    }
+  },
+  "checkbox.halo.color": {
+    type: COLOR,
+    value: {
+      ref: "checkbox.hover.haloColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.hover.haloColor"
+      }
+    }
+  },
+  "checkbox.halo.width": {
+    type: LENGTH,
+    value: {
+      ref: "checkbox.hover.haloWidth"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.hover.haloWidth"
+      }
+    }
+  },
+  "checkbox.minHeight": {
+    type: LENGTH,
+    value: "16px"
+  },
+  "checkbox.height": {
+    type: LENGTH,
+    value: {
+      ref: "checkbox.minHeight"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.minHeight"
+      }
+    }
+  },
+  "checkbox.indicatorColor": {
+    type: COLOR,
+    value: {
+      ref: "basics.colors.primary.white"
+    }
+  },
+  "checkbox.iconColor": {
+    type: COLOR,
+    value: {
+      ref: "checkbox.indicatorColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.indicatorColor"
+      }
+    }
+  },
+  "checkbox.minWidth": {
+    type: LENGTH,
+    value: "16px"
+  },
+  "checkbox.width": {
+    type: LENGTH,
+    value: {
+      ref: "checkbox.minWidth"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.minWidth"
+      }
+    }
+  },
+  "checkbox.unchecked.default.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.opacity.transparent"
+    }
+  },
   "checkbox.backgroundColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.component.backgroundColor"
+      ref: "checkbox.unchecked.default.backgroundColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.unchecked.default.backgroundColor"
+      }
+    }
+  },
+  "checkbox.unchecked.default.borderColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.border.base"
     }
   },
   "checkbox.borderColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.baseColor"
+      ref: "checkbox.unchecked.default.borderColor"
     },
-    transform: {
-      alpha: 0.5
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.unchecked.default.borderColor"
+      }
     }
   },
-  "checkbox.borderWidth": {
-    type: BORDER_WIDTH,
-    value: "1px"
-  },
-  "checkbox.borderRadius": {
-    type: BORDER_RADIUS,
-    value: 0
-  },
-  "checkbox.halo.color": {
+  "checkbox.unchecked.hover.borderColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.baseColor"
-    },
-    transform: {
-      alpha: 0.15
+      ref: "colorScheme.border.base"
     }
   },
-  "checkbox.halo.width": {
-    type: LENGTH,
-    value: "2px"
-  },
-  "checkbox.height": {
-    type: LENGTH,
-    value: "16px"
-  },
-  "checkbox.iconColor": {
-    type: COLOR,
-    value: {
-      ref: "basics.colors.white"
-    }
-  },
-  "checkbox.width": {
-    type: LENGTH,
-    value: "16px"
-  },
-
-  // Hover
   "checkbox.hover.borderColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.baseColor"
+      ref: "checkbox.unchecked.hover.borderColor"
     },
-    transform: {
-      alpha: 0.5
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.unchecked.hover.borderColor"
+      }
     }
   },
   "checkbox.hover.halo.width": {
     type: LENGTH,
     value: {
-      ref: "checkbox.halo.width"
+      ref: "checkbox.hover.haloWidth"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.hover.haloWidth"
+      }
     }
   },
   "checkbox.hover.halo.color": {
     type: COLOR,
     value: {
-      ref: "checkbox.halo.color"
+      ref: "checkbox.hover.haloColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.hover.haloColor"
+      }
     }
   },
-
-  // Focus
+  "checkbox.unchecked.focus.borderColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.border.accent"
+    }
+  },
   "checkbox.focus.borderColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.accentColor"
+      ref: "checkbox.unchecked.focus.borderColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.unchecked.focus.borderColor"
+      }
     }
   },
   "checkbox.focus.halo.color": {
     type: COLOR,
     value: {
-      ref: "colorScheme.accentColor"
+      ref: "checkbox.focus.haloColor"
     },
-    transform: {
-      alpha: 0.35
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.focus.haloColor"
+      }
     }
   },
   "checkbox.focus.halo.width": {
     type: LENGTH,
     value: {
-      ref: "checkbox.halo.width"
+      ref: "checkbox.focus.haloWidth"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.focus.haloWidth"
+      }
     }
   },
-
-  // Pressed
+  "checkbox.unchecked.pressed.borderColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.border.base"
+    }
+  },
   "checkbox.pressed.borderColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.baseColor"
+      ref: "checkbox.unchecked.pressed.borderColor"
     },
-    transform: {
-      alpha: 0.5
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.unchecked.pressed.borderColor"
+      }
     }
   },
   "checkbox.pressed.halo.color": {
     type: COLOR,
     value: {
-      ref: "colorScheme.baseColor"
+      ref: "checkbox.pressed.haloColor"
     },
-    transform: {
-      alpha: 0.25
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.pressed.haloColor"
+      }
     }
   },
   "checkbox.pressed.halo.width": {
     type: LENGTH,
-    value: "4px"
+    value: {
+      ref: "checkbox.pressed.haloWidth"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.pressed.haloWidth"
+      }
+    }
   },
-
-  // Checked
+  "checkbox.checked.default.borderColor": {
+    type: COLOR,
+    value: {
+      ref: "basics.colors.primary.charcoal.600"
+    }
+  },
   "checkbox.checked.borderColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.baseColor"
+      ref: "checkbox.checked.default.borderColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.checked.default.borderColor"
+      }
     }
   },
   "checkbox.checked.iconColor": {
     type: COLOR,
     value: {
-      ref: "basics.colors.white"
+      ref: "checkbox.indicatorColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.indicatorColor"
+      }
+    }
+  },
+  "checkbox.checked.default.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "basics.colors.primary.charcoal.600"
     }
   },
   "checkbox.checked.backgroundColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.baseColor"
+      ref: "checkbox.checked.default.backgroundColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.checked.default.backgroundColor"
+      }
     }
   },
-
-  // Indeterminate
   "checkbox.indeterminate.borderColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.baseColor"
+      ref: "checkbox.checked.default.borderColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.checked.default.borderColor"
+      }
     }
   },
   "checkbox.indeterminate.iconColor": {
     type: COLOR,
     value: {
-      ref: "basics.colors.white"
+      ref: "checkbox.indicatorColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.indicatorColor"
+      }
     }
   },
   "checkbox.indeterminate.backgroundColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.baseColor"
+      ref: "checkbox.checked.default.backgroundColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.checked.default.backgroundColor"
+      }
     }
   },
-
-  // Legacy Web Light Specific
+  "checkbox.checked.focus.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "checkbox.checked.default.backgroundColor"
+    }
+  },
   "checkbox.focus.checked.backgroundColor": {
     type: COLOR,
     value: {
-      ref: "checkbox.backgroundColor"
+      ref: "checkbox.checked.focus.backgroundColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.checked.focus.backgroundColor"
+      }
+    }
+  },
+  "checkbox.checked.focus.borderColor": {
+    type: COLOR,
+    value: {
+      ref: "checkbox.checked.default.borderColor"
     }
   },
   "checkbox.focus.checked.borderColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.baseColor"
+      ref: "checkbox.checked.focus.borderColor"
     },
-    transform: {
-      alpha: 0.5
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.checked.focus.borderColor"
+      }
     }
   },
   "checkbox.focus.indeterminate.borderColor": {
     type: COLOR,
     value: {
-      ref: "colorScheme.baseColor"
+      ref: "checkbox.checked.focus.borderColor"
     },
-    transform: {
-      alpha: 0.5
+    metadata: {
+      deprecated: {
+        equivalent: "checkbox.checked.focus.borderColor"
+      }
+    }
+  },
+  "checkbox.focus.haloColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.halo.focus"
+    }
+  },
+  "checkbox.hover.haloColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.halo.hover"
+    }
+  },
+  "checkbox.pressed.haloColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.halo.pressed"
+    }
+  },
+  "checkbox.focus.haloWidth": {
+    type: BORDER_WIDTH,
+    value: {
+      ref: "basics.borderWidths.medium"
+    }
+  },
+  "checkbox.hover.haloWidth": {
+    type: BORDER_WIDTH,
+    value: {
+      ref: "basics.borderWidths.medium"
+    }
+  },
+  "checkbox.pressed.haloWidth": {
+    type: BORDER_WIDTH,
+    value: {
+      ref: "basics.borderWidths.large"
+    }
+  },
+  "checkbox.unchecked.hover.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.opacity.transparent"
+    }
+  },
+  "checkbox.unchecked.focus.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.opacity.transparent"
+    }
+  },
+  "checkbox.unchecked.pressed.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.opacity.transparent"
+    }
+  },
+  "checkbox.checked.hover.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.reference.base"
+    }
+  },
+  "checkbox.checked.hover.borderColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.reference.base"
+    }
+  },
+  "checkbox.checked.pressed.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.reference.base"
+    }
+  },
+  "checkbox.checked.pressed.borderColor": {
+    type: COLOR,
+    value: {
+      ref: "colorScheme.reference.base"
     }
   }
 };
