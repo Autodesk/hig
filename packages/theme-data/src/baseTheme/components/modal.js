27734e571873a0f9f0d4e1f61d4da97d07819587
Wilbert Mui <wmui51@yahoo.com>
2020-03-27 16:07:16 -0700

    feat: rename modal roles
    Renamed
    modal.overlay.color => modal.overlayColor
    modal.textColor => modal.fontColor
    modal.body.height => modal.body.minHeight
    modal.horizontalPadding => modal.paddingHorizontal
    modal.header.borderColor => modal.header.borderBottomColor
    modal.header.height => modal.header.minHeight
    modal.window.backgroundColor => modal.shell.backgroundColor
    modal.window.borderRadius => modal.shell.borderRadius
    modal.window.height => modal.shell.minHeight
    modal.window.width => modal.shell.minWidth
    modal.window.shadowColor => modal.shell.shadowColor

--- packages/theme-data/src/baseTheme/components/modal.js
+++ packages/theme-data/src/baseTheme/components/modal.js
@@ -1,44 +1,108 @@
 import { BORDER_RADIUS, COLOR, LENGTH, SPACING } from "../../consts/types";
 
 export default {
+  "modal.overlayColor": {
+    type: COLOR,
+    value: { ref: "basics.colors.primary.black" },
+    transform: { alpha: "0.5" }
+  },
   "modal.overlay.color": {
     type: COLOR,
-    value: { ref: "basics.colors.black" },
-    transform: { alpha: "0.5" }
+    value: { ref: "modal.overlayColor" },
+    metadata: { deprecated: { equivalent: "modal.overlayColor" } }
+  },
+  "modal.fontColor": {
+    type: COLOR,
+    value: { ref: "colorScheme.text.default" }
   },
   "modal.textColor": {
     type: COLOR,
-    value: { ref: "colorScheme.textColor" }
+    value: { ref: "modal.fontColor" },
+    metadata: { deprecated: { equivalent: "modal.fontColor" } }
   },
-  "modal.body.height": { type: LENGTH },
-  "modal.horizontalPadding": {
+  "modal.body.minHeight": {
+    type: LENGTH,
+    value: "152px"
+  },
+  "modal.body.height": {
+    type: LENGTH,
+    value: { ref: "modal.body.minHeight" },
+    metadata: { deprecated: { equivalent: "modal.body.minHeight" } }
+  },
+  "modal.paddingHorizontal": {
     type: SPACING,
     value: { ref: "density.spacings.large" }
   },
+  "modal.horizontalPadding": {
+    type: SPACING,
+    value: { ref: "modal.paddingHorizontal" },
+    metadata: { deprecated: { equivalent: "modal.paddingHorizontal" } }
+  },
   "modal.header.backgroundColor": {
     type: COLOR,
-    value: { ref: "colorScheme.surfaceLevel100Color" }
+    value: { ref: "colorScheme.surface.level100" }
+  },
+  "modal.header.borderBottomColor": {
+    type: COLOR,
+    value: { ref: "colorScheme.divider.heavyweight" }
   },
   "modal.header.borderColor": {
     type: COLOR,
-    value: { ref: "colorScheme.textColor" },
-    transform: { alpha: "0.25" }
+    value: { ref: "modal.header.borderBottomColor" },
+    metadata: { deprecated: { equivalent: "modal.header.borderBottomColor" } }
+  },
+  "modal.header.minHeight": {
+    type: LENGTH,
+    value: "60px"
   },
   "modal.header.height": {
-    type: LENGTH
+    type: LENGTH,
+    value: { ref: "modal.header.minHeight" },
+    metadata: { deprecated: { equivalent: "modal.header.minHeight" } }
+  },
+  "modal.shell.backgroundColor": {
+    type: COLOR,
+    value: { ref: "colorScheme.surface.level100" }
   },
   "modal.window.backgroundColor": {
     type: COLOR,
-    value: { ref: "colorScheme.surfaceLevel100Color" }
+    value: { ref: "modal.shell.backgroundColor" },
+    metadata: { deprecated: { equivalent: "modal.shell.backgroundColor" } }
   },
-  "modal.window.borderRadius": {
+  "modal.shell.borderRadius": {
     type: BORDER_RADIUS,
     value: { ref: "basics.borderRadii.large" }
   },
-  "modal.window.height": { type: LENGTH },
-  "modal.window.width": { type: LENGTH, value: "400px" },
+  "modal.window.borderRadius": {
+    type: BORDER_RADIUS,
+    value: { ref: "modal.shell.borderRadius" },
+    metadata: { deprecated: { equivalent: "modal.shell.borderRadius" } }
+  },
+  "modal.shell.minHeight": {
+    type: LENGTH,
+    value: "300px"
+  },
+  "modal.window.height": {
+    type: LENGTH,
+    value: { ref: "modal.shell.minHeight" },
+    metadata: { deprecated: { equivalent: "modal.shell.minHeight" } }
+  },
+  "modal.window.minWidth": {
+    type: LENGTH,
+    value: "400px"
+  },
+  "modal.window.width": {
+    type: LENGTH,
+    value: { ref: "modal.window.minWidth" },
+    metadata: { deprecated: { equivalent: "modal.window.minWidth" } }
+  },
+  "modal.shell.shadowColor": {
+    type: COLOR,
+    value: { ref: "colorScheme.shadow.high" }
+  },
   "modal.window.shadowColor": {
     type: COLOR,
-    value: { ref: "colorScheme.highShadowColor" }
+    value: { ref: "modal.shell.shadowColor" },
+    metadata: { deprecated: { equivalent: "modal.shell.shadowColor" } }
   }
 };
--- packages/theme-data/src/densities/mediumDensity/components/modal.js
+++
@@ -1,9 +0,0 @@
-export default {
-  "modal.body.height": {
-    value: "152px"
-  },
-  "modal.header.height": {
-    value: "60px"
-  },
-  "modal.window.height": { value: "300px" }
-};
