572119b4cda3ef94a91d315f01dc06163f243c7a
Wilbert Mui <wmui51@yahoo.com>
2020-03-30 10:43:35 -0700

    feat: rename progress-bar roles
    progressBar.backgroundColor => progress.bar.backgroundColor
    progressBar.highlightColor => progress.bar.highlightColor
    progressBar.borderRadius => progress.bar.borderRadius
    progressBar.height => progress.bar.minHeight

--- packages/theme-data/src/baseTheme/components/progressBar.js
+++ packages/theme-data/src/baseTheme/components/progressBar.js
@@ -1,29 +1,73 @@
 import { COLOR, BORDER_RADIUS, LENGTH } from "../../consts/types";
 
 export default {
-  "progressBar.backgroundColor": {
+  "progress.bar.backgroundColor": {
     type: COLOR,
     value: {
-      ref: "basics.colors.black"
+      ref: "basics.colors.primary.black"
     },
     transform: {
       alpha: "0.05"
     }
   },
-  "progressBar.highlightColor": {
+  "progressBar.backgroundColor": {
+    type: COLOR,
+    value: {
+      ref: "progress.bar.backgroundColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "progress.bar.backgroundColor"
+      }
+    }
+  },
+  "progress.bar.highlightColor": {
     type: COLOR,
     value: {
       ref: "basics.colors.autodeskBlue400"
     }
   },
-  "progressBar.borderRadius": {
+  "progressBar.highlightColor": {
+    type: COLOR,
+    value: {
+      ref: "progress.bar.highlightColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "progress.bar.highlightColor"
+      }
+    }
+  },
+  "progress.bar.borderRadius": {
     type: BORDER_RADIUS,
     value: {
       ref: "basics.borderRadii.medium"
     }
   },
-  "progressBar.height": {
+  "progressBar.borderRadius": {
+    type: BORDER_RADIUS,
+    value: {
+      ref: "progress.bar.borderRadius"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "progress.bar.borderRadius"
+      }
+    }
+  },
+  "progress.bar.minHeight": {
     type: LENGTH,
     value: "4px"
+  },
+  "progressBar.height": {
+    type: LENGTH,
+    value: {
+      ref: "progress.bar.minHeight"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "progress.bar.minHeight"
+      }
+    }
   }
 };
