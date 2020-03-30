d3535e32ab1be9d3491b656fc4c1800026153d78
Wilbert Mui <wmui51@yahoo.com>
2020-03-30 11:36:53 -0700

    feat: rename skeletonItem roles
    skeletonItem.defaultHeight => skeletonItem.minHeight

--- packages/theme-data/src/baseTheme/components/skeletonItem.js
+++ packages/theme-data/src/baseTheme/components/skeletonItem.js
@@ -3,16 +3,43 @@ import { BORDER_RADIUS, COLOR, LENGTH } from "../../consts/types";
 export default {
   "skeletonItem.borderRadius": {
     type: BORDER_RADIUS,
-    value: { ref: "basics.borderRadii.medium" }
+    value: {
+      ref: "basics.borderRadii.medium"
+    }
+  },
+  "skeletonItem.minHeight": {
+    type: LENGTH,
+    value: {
+      ref: "density.spacings.medium"
+    }
   },
   "skeletonItem.defaultHeight": {
     type: LENGTH,
-    value: { ref: "density.spacings.medium" }
+    value: {
+      ref: "skeletonItem.minHeight"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "skeletonItem.minHeight"
+      }
+    }
   },
   "skeletonItem.backgroundColor": {
-    type: COLOR
+    type: COLOR,
+    value: {
+      ref: "basics.colors.primary.black"
+    },
+    transform: {
+      alpha: 0.06
+    }
   },
   "skeletonItem.highlightColor": {
-    type: COLOR
+    type: COLOR,
+    value: {
+      ref: "basics.colors.primary.white"
+    },
+    transform: {
+      alpha: 0.3
+    }
   }
 };
