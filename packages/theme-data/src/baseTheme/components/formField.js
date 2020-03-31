d8b734dc24b5e94a70295002158ff5164a29863d
Wilbert Mui <wmui51@yahoo.com>
2020-03-31 13:01:38 -0700

    feat: deprecate formField roles
    Deprecate
    formField.instructionalText.fontSize
    formField.instructionalText.fontWeight
    formField.instructionalText.fontFamily
    formField.instructionalText.fontColor
    formField.instructionalText.lineHeight
    formField.icon.width
    formField.icon.paddingRight
    formField.icon.color

--- packages/theme-data/src/baseTheme/components/formField.js
+++ packages/theme-data/src/baseTheme/components/formField.js
@@ -10,33 +10,88 @@ import {
 export default {
   "formField.instructionalText.fontSize": {
     type: FONT_SIZE,
-    value: { ref: "density.fontSizes.small" }
+    value: {
+      ref: "input.value.fontSize"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "input.value.fontSize"
+      }
+    }
   },
   "formField.instructionalText.fontWeight": {
     type: FONT_WEIGHT,
-    value: { ref: "basics.fontWeights.regular" }
+    value: {
+      ref: "input.value.fontWeight"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "input.value.fontWeight"
+      }
+    }
   },
   "formField.instructionalText.fontFamily": {
     type: FONT_FAMILY,
-    value: { ref: "basics.fontFamilies.main" }
+    value: {
+      ref: "input.value.fontFamily"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "input.value.fontFamily"
+      }
+    }
   },
   "formField.instructionalText.fontColor": {
-    type: COLOR
+    type: COLOR,
+    value: {
+      ref: "input.value.placeholderFontColor"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "input.value.placeholderFontColor"
+      }
+    }
   },
   "formField.instructionalText.lineHeight": {
     type: LINE_HEIGHT,
-    value: { ref: "basics.lineHeights.medium" }
+    value: {
+      ref: "basics.lineHeights.medium"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "basics.lineHeights.medium"
+      }
+    }
   },
   "formField.icon.width": {
     type: LENGTH,
-    value: "24px"
+    value: "24px",
+    metadata: {
+      deprecated: {
+        equivalent: "basics.spacings"
+      }
+    }
   },
   "formField.icon.paddingRight": {
     type: LENGTH,
-    value: { ref: "density.spacings.small" }
+    value: {
+      ref: "density.spacings.small"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "density.spacings.small"
+      }
+    }
   },
   "formField.icon.color": {
     type: COLOR,
-    value: { ref: "colorScheme.textColor" }
+    value: {
+      ref: "input.indicator.default"
+    },
+    metadata: {
+      deprecated: {
+        equivalent: "input.indicator.default"
+      }
+    }
   }
 };
--- packages/theme-data/src/colorSchemes/lightGray/unresolvedRoles.js
+++ packages/theme-data/src/colorSchemes/lightGray/unresolvedRoles.js
@@ -3,15 +3,13 @@ import mapKeys from "../../utils/mapKeys";
 import baseTheme from "../../baseTheme";
 import mediumDensityTheme from "../../densities/mediumDensity";
 import system from "./system";
-import formField from "./components/formField";
 
 const lightGrayThemeConfig = extendTheme(
   baseTheme.unresolvedRoles,
   Object.assign(
     {},
     mediumDensityTheme.unresolvedRoles,
-    mapKeys(system.colorScheme, key => `colorScheme.${key}`),
-    formField
+    mapKeys(system.colorScheme, key => `colorScheme.${key}`)
   )
 );
 
