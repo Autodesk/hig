8c704651303aa9f782adfe15ccf6c22dfaf0089d
Wilbert Mui <wmui51@yahoo.com>
2020-04-02 15:49:27 -0700

    feat: deprecate component role
    Deprecate
    component.disabled.opacity

--- packages/theme-data/src/baseTheme/components/component.js
+++ packages/theme-data/src/baseTheme/components/component.js
@@ -1,8 +1,14 @@
 import { OPACITY } from "../../consts/types";
 
 export default {
+  // Deprecated
   "component.disabled.opacity": {
     type: OPACITY,
-    value: "0.4"
+    value: "0.4",
+    metadata: {
+      deprecated: {
+        equivalent: "colorScheme.opacity.disabled"
+      }
+    }
   }
 };
