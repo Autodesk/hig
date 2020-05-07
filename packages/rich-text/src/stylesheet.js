4358da8fed20f30ca618c8fe77e9822786ea1898
Wilbert Mui <wmui51@yahoo.com>
2020-05-07 14:59:13 -0700

    fix: stop using deprecated theme-data roles

--- packages/rich-text/src/stylesheet.js
+++ packages/rich-text/src/stylesheet.js
@@ -42,19 +42,19 @@ export default function stylesheet(themeData) {
     a: {
       textDecoration: "none",
       cursor: "pointer",
-      color: themeData["textLink.textColor"],
+      color: themeData["textLink.primary.default.fontColor"],
       outline: "none",
 
       "&:hover": {
-        color: themeData["textLink.hover.textColor"],
+        color: themeData["textLink.primary.hover.fontColor"],
         textDecoration: "underline",
-        textDecorationColor: themeData["textLink.hover.underline.color"]
+        textDecorationColor: themeData["textLink.primary.hover.underlineColor"]
       },
 
       "&:focus": {
-        color: themeData["textLink.focus.textColor"],
-        outline: `solid ${themeData["textLink.focus.halo.width"]} ${
-          themeData["textLink.focus.halo.color"]
+        color: themeData["textLink.primary.focus.fontColor"],
+        outline: `solid ${themeData["textLink.focus.haloWidth"]} ${
+          themeData["textLink.focus.haloColor"]
         }`
       }
     }
