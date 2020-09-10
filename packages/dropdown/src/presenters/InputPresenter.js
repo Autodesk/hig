34471fa5ed39f3592e22e647c7546aed3d171193
Wilbert Mui <wmui51@yahoo.com>
2020-09-10 14:12:43 -0700

    fix: long options should not run into the caret

--- packages/dropdown/src/presenters/InputPresenter.js
+++ packages/dropdown/src/presenters/InputPresenter.js
@@ -6,12 +6,18 @@ import ThemeContext from "@hig/theme-context";
 
 import stylesheet from "./InputPresenter.stylesheet";
 
-function customStyles(inputStylesheet) {
+function customStyles(inputStylesheet, props, themeData) {
+  const { variant } = props;
   return {
     ...inputStylesheet,
     input: {
       ...inputStylesheet.input,
-      cursor: "pointer"
+      cursor: `pointer`,
+      whiteSpace: `nowrap`,
+      textOverflow: `ellipsis`,
+      paddingRight: variant === `line`
+        ? `calc(${themeData["density.spacings.large"]} + ${themeData["input.line.paddingHorizontal"]}px)`
+        : `calc(${themeData["density.spacings.large"]} + ${themeData["input.box.paddingHorizontal"]})`
     }
   };
 }
@@ -23,10 +29,10 @@ export default function InputPresenter(props) {
     <ThemeContext.Consumer>
       {({ resolvedRoles, metadata }) => {
         const CaretIcon =
-          metadata.densityId === "medium-density" ? CaretDownMUI : CaretDownSUI;
+          metadata.densityId === `medium-density` ? CaretDownMUI : CaretDownSUI;
         const iconColor = isOpen
-          ? resolvedRoles["colorScheme.reference.accent"]
-          : resolvedRoles["input.indicator.default"];
+          ? resolvedRoles[`colorScheme.indicator.on`]
+          : resolvedRoles[`input.indicator.default`];
 
         return (
           <div className={css(stylesheet(props, resolvedRoles).wrapper)}>
