dde9506212b3f6b95abb62e95108f8c28cc5a690
Wilbert Mui <wmui51@yahoo.com>
2020-09-28 16:19:17 -0700

    test: update jest snapshots

--- packages/dropdown/src/__stories__/stories.js
+++ packages/dropdown/src/__stories__/stories.js
@@ -3,7 +3,9 @@ export default [
     description: "default",
     getProps: () => ({
       placeholder: "Select a theme",
-      options: ["HIG Light Theme", "HIG Dark Blue Theme", "Matrix Theme"]
+      options: ["HIG Light Theme", "HIG Dark Blue Theme", "Matrix Theme"],
+      typable: true,
+      onInputChange: event => { console.log(event.target.value) }
     })
   }
 ];
