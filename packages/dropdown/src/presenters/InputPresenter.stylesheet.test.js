155d7e7b0b494e48fc73af49252d6ee2210de45a
Wilbert Mui <wmui51@yahoo.com>
2020-09-29 13:41:19 -0700

    test: add tests for stylesheet functions

---
+++ packages/dropdown/src/presenters/InputPresenter.stylesheet.test.js
@@ -0,0 +1,24 @@
+import stylesheet from "./InputPresenter.stylesheet";
+
+describe("dropdown/InputPresenter.styelsheet", () => {
+  const styles = stylesheet({}, {}, {}, {});
+
+  it("returns an object", () => {
+    expect(styles).toEqual(expect.any(Object));
+  });
+
+  it("returned object contains property of inputWrapper", () => {
+    expect(styles).toHaveProperty("inputWrapper", expect.any(Object));
+  });
+
+  it("returned object contains property of caret", () => {
+    expect(styles).toHaveProperty("caret", expect.any(Object));
+  });
+
+  it("returns the custom stylesheet", () => {
+    const props = {
+      stylesheet: () => ({ padding: 0 })
+    };
+    expect(stylesheet(props, {})).toEqual({ padding: 0 });
+  });
+});
