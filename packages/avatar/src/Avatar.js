91a0251efe79f73dc935bf06f7de770d59d99dd8
Guobi Wu <guobi.wu@gmail.com>
2020-09-29 17:03:41 +0800

    Update Avatar.js

--- packages/avatar/src/Avatar.js
+++ packages/avatar/src/Avatar.js
@@ -45,8 +45,8 @@ function initialsFromName(name) {
   // /\p{sc=Han}/gu
   const regexCheckHan = /(?:[\u2E80-\u2E99\u2E9B-\u2EF3\u2F00-\u2FD5\u3005\u3007\u3021-\u3029\u3038-\u303B\u3400-\u4DBF\u4E00-\u9FFC\uF900-\uFA6D\uFA70-\uFAD9]|\uD81B[\uDFF0\uDFF1]|[\uD840-\uD868\uD86A-\uD86C\uD86F-\uD872\uD874-\uD879\uD880-\uD883][\uDC00-\uDFFF]|\uD869[\uDC00-\uDEDD\uDF00-\uDFFF]|\uD86D[\uDC00-\uDF34\uDF40-\uDFFF]|\uD86E[\uDC00-\uDC1D\uDC20-\uDFFF]|\uD873[\uDC00-\uDEA1\uDEB0-\uDFFF]|\uD87A[\uDC00-\uDFE0]|\uD87E[\uDC00-\uDE1D]|\uD884[\uDC00-\uDF4A])/g;
   const results = name.matchAll(regexGetInit);
-  let initials = [];
-  for (let result of results) {
+  const initials = [];
+  for (const result of results) {
     // Return initial if it is a Chinese or Japanese character
     if (result[2].match(regexCheckHan)) {
       return result[2];
