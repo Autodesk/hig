cdaaad3393eff781278cfa1ed2f8fadae9a540dd
Wilbert Mui <wmui51@yahoo.com>
2021-02-05 20:37:15 -0800

    feat: add stylesheet prop, pass down className

--- packages/spacer/src/Spacer.stylesheet.js
+++ packages/spacer/src/Spacer.stylesheet.js
@@ -1,17 +1,16 @@
 import { sizes } from "./availableSizes";
 
-function getSpacing(spacing, themeData) {
-  return themeData[`density.spacings.${sizes[spacing]}`];
-}
-
 export default function stylesheet(props, themeData) {
-  return {
+  const { size, spacing, stylesheet: customStylsheet } = props;
+  const styles = {
     spacer: {
-      width: props.size ? props.size : getSpacing(props.spacing, themeData),
-      height: props.size ? props.size : getSpacing(props.spacing, themeData),
-      flex: props.size
-        ? `0 0 ${props.size}`
-        : `0 0 ${getSpacing(props.spacing, themeData)}`
+      width: size || themeData[`density.spacings.${sizes[spacing]}`],
+      height: size || themeData[`density.spacings.${sizes[spacing]}`],
+      flex: size
+        ? `0 0 ${size}`
+        : `0 0 ${themeData[`density.spacings.${sizes[spacing]}`]}`
     }
   };
+
+  return customStylsheet ? customStylsheet(styles, props, themeData) : styles;
 }
