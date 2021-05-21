300cd53eed683112e55eb7b7c198eb720e205141
Wilbert Mui <wmui51@yahoo.com>
2021-05-21 00:35:49 -0700

    update test story

--- packages/tree-view/src/__stories__/stories.js
+++ packages/tree-view/src/__stories__/stories.js
@@ -6,13 +6,12 @@ export default [
     description: "WM",
     getProps: () => ({
       test: "WM",
-      checkmark: false,
       children: [
         <TreeItem id="option-1" key="option-1" test="WM" label="Tree Item Nested Parent">
-          <TreeItem label="Tree Item Level 2">
-            <TreeItem>
+          <TreeItem label="Tree Item Level 2" test="WM">
+            <TreeItem test="WM">
               Tree Item Level 3
-              <TreeItem label="Level 4" />
+              <TreeItem label="Level 4" test="WM" />
             </TreeItem>
           </TreeItem>
         </TreeItem>,
@@ -35,47 +34,25 @@ export default [
     description: "RR",
     getProps: () => ({
       test: "RR",
-      checkmark: false,
       children: [
-        <TreeItem id="option-1" key="option-1" test="WM" label="Tree Item Nested Parent">
-          <TreeItem label="Tree Item Level 2">
-            <TreeItem>
-            children: [
-        <TreeItem id="option-1" key="option-1" test="WM" label="Tree Item Nested Parent">
-          <TreeItem label="Tree Item Level 2">
-            <TreeItem>
+        <TreeItem id="option-1" key="option-1" test="RR" label="Tree Item Nested Parent">
+          <TreeItem test="RR" label="Tree Item Level 2">
+            <TreeItem test="RR">
               Tree Item Level 3
-              <TreeItem label="Level 4" />
+              <TreeItem label="Level 4" test="RR" />
             </TreeItem>
           </TreeItem>
         </TreeItem>,
-        <TreeItem id="option-2" key="option-2" test="WM" label="test label">
+        <TreeItem id="option-2" key="option-2" label="test label" test="RR">
           TreeItem 2
         </TreeItem>,
-        <TreeItem id="option-3" key="option-3" test="WM">
+        <TreeItem id="option-3" key="option-3" test="RR">
           TreeItem 3
         </TreeItem>,
-        <TreeItem id="option-4" key="option-4" test="WM">
+        <TreeItem id="option-4" key="option-4" test="RR">
           TreeItem 4
         </TreeItem>,
-        <TreeItem id="option-5" key="option-5" test="WM">
-          TreeItem 5
-        </TreeItem>
-      ]
-              <TreeItem label="Level 4" />
-            </TreeItem>
-          </TreeItem>
-        </TreeItem>,
-        <TreeItem id="option-2" key="option-2" test="WM" label="test label">
-          TreeItem 2
-        </TreeItem>,
-        <TreeItem id="option-3" key="option-3" test="WM">
-          TreeItem 3
-        </TreeItem>,
-        <TreeItem id="option-4" key="option-4" test="WM">
-          TreeItem 4
-        </TreeItem>,
-        <TreeItem id="option-5" key="option-5" test="WM">
+        <TreeItem id="option-5" key="option-5" test="RR">
           TreeItem 5
         </TreeItem>
       ]
