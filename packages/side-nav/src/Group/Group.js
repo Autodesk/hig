5eebc67c78b892c08d9e946a4125c5782c622479
Steve Arias <steveariasarias@hotmail.com>
2021-11-12 14:28:07 -0500

    Feat : Add GlobNav components

--- packages/side-nav/src/Group/Group.js
+++ packages/side-nav/src/Group/Group.js
@@ -1,52 +1,54 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { css, cx } from "emotion";
 import ThemeContext from "@hig/theme-context";
 import { createCustomClassNames } from "@hig/utils";
 import stylesheet from "./stylesheet";
 
-export default class Group extends Component {
-  static propTypes = {
-    /** A slot for arbitrary content to render above the Modules */
-    intro: PropTypes.node,
-    /** One or more SideNav Modules */
-    children: PropTypes.node,
-    /** Function to modify the component's styles */
-    stylesheet: PropTypes.func
-  };
+const Group = props => {
+  const {
+    intro,
+    children,
+    stylesheet: customStylesheet,
+    ...otherProps
+  } = props;
+  const { className } = otherProps;
+  const introClassName = createCustomClassNames(className, "intro");
 
-  render() {
-    const {
-      intro,
-      children,
-      stylesheet: customStylesheet,
-      ...otherProps
-    } = this.props;
-    const { className } = otherProps;
-    const introClassName = createCustomClassNames(className, "intro");
+  return (
+    <ThemeContext.Consumer>
+      {({ resolvedRoles }) => {
+        const styles = stylesheet(
+          {
+            stylesheet: customStylesheet,
+            ...otherProps
+          },
+          resolvedRoles
+        );
+        return (
+          <section className={cx([css(styles.group), className])}>
+            {intro && (
+              <div className={cx([css(styles.intro), introClassName])}>
+                {intro}
+              </div>
+            )}
+            {children}
+          </section>
+        );
+      }}
+    </ThemeContext.Consumer>
+  );
+};
 
-    return (
-      <ThemeContext.Consumer>
-        {({ resolvedRoles }) => {
-          const styles = stylesheet(
-            {
-              stylesheet: customStylesheet,
-              ...otherProps
-            },
-            resolvedRoles
-          );
-          return (
-            <section className={cx([css(styles.group), className])}>
-              {intro && (
-                <div className={cx([css(styles.intro), introClassName])}>
-                  {intro}
-                </div>
-              )}
-              {children}
-            </section>
-          );
-        }}
-      </ThemeContext.Consumer>
-    );
-  }
-}
+Group.displayName = "Group";
+
+Group.propTypes = {
+  /** A slot for arbitrary content to render above the Modules */
+  intro: PropTypes.node,
+  /** One or more SideNav Modules */
+  children: PropTypes.node,
+  /** Function to modify the component's styles */
+  stylesheet: PropTypes.func
+};
+
+export default Group;
--- packages/side-nav/src/presenters/SideNav.js
+++ packages/side-nav/src/presenters/SideNav.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { css, cx } from "emotion";
 import { ThemeContext } from "@hig/theme-context";
@@ -10,44 +10,8 @@ import { createCustomClassNames } from "@hig/utils";
 
 import stylesheet from "./stylesheet";
 
-export default class SideNav extends Component {
-  static propTypes = {
-    /** Additional content to include below navigation items */
-    children: PropTypes.node,
-    /** Copyright text to include  */
-    copyright: PropTypes.node,
-    /** 0 or more SideNav Groups */
-    groups: PropTypes.node,
-    /** Subtitle at the top of the SideNav */
-    headerLabel: PropTypes.string,
-    /** An href for the SideNav Subtitle */
-    headerLink: PropTypes.string,
-    /** Called when headerLink is clicked */
-    onClickHeader: PropTypes.func,
-    /** 0 or more SideNav Links */
-    links: PropTypes.node,
-    /** Called when minimize button is clicked */
-    onMinimize: PropTypes.func,
-    /** A SideNav Search element */
-    search: PropTypes.node,
-    /** Renders a button to minimize the SideNav */
-    showMinimizeButton: PropTypes.bool,
-    /** Function to modify the component's styles */
-    stylesheet: PropTypes.func,
-    /** Title at the top of the SideNav */
-    superHeaderLabel: PropTypes.string,
-    /** An href for the SideNav Title */
-    superHeaderLink: PropTypes.string,
-    /** Called when superHeaderLink is clicked */
-    onClickSuperHeader: PropTypes.func
-  };
-
-  static defaultProps = {
-    onMinimize: () => {},
-    showMinimizeButton: false
-  };
-
-  _renderHeader = (link, label, styles, onClick, className) => {
+const SideNav = props => {
+  const _renderHeader = (link, label, styles, onClick, className) => {
     if (!label) {
       return null;
     }
@@ -72,7 +36,7 @@ export default class SideNav extends Component {
     );
   };
 
-  _renderHeaders = resolvedRoles => {
+  const _renderHeaders = resolvedRoles => {
     const {
       headerLabel,
       headerLink,
@@ -82,7 +46,7 @@ export default class SideNav extends Component {
       superHeaderLink,
       onClickSuperHeader,
       ...otherProps
-    } = this.props;
+    } = props;
     const { className } = otherProps;
 
     if (!(superHeaderLabel || headerLabel)) {
@@ -91,25 +55,25 @@ export default class SideNav extends Component {
 
     return (
       <div>
-        {this._renderHeader(
+        {_renderHeader(
           superHeaderLink,
           superHeaderLabel,
           stylesheet(
             {
               isLink: superHeaderLink,
               stylesheet: customStylesheet,
-              ...this.props
+              ...props
             },
             resolvedRoles
           ).headers.super,
           onClickSuperHeader,
           createCustomClassNames(className, "headers__super")
         )}
-        {this._renderHeader(
+        {_renderHeader(
           headerLink,
           headerLabel,
           stylesheet(
-            { isLink: headerLink, stylesheet: customStylesheet, ...this.props },
+            { isLink: headerLink, stylesheet: customStylesheet, ...props },
             resolvedRoles
           ).headers.normal,
           onClickHeader,
@@ -119,82 +83,120 @@ export default class SideNav extends Component {
     );
   };
 
-  render() {
-    const {
-      children,
-      copyright,
-      groups,
-      links,
-      onMinimize,
-      search,
-      showMinimizeButton,
-      stylesheet: customStylesheet,
-      ...otherProps
-    } = this.props;
-    const { className } = otherProps;
-    const overflowClassName = createCustomClassNames(className, "overflow");
-    const slotClassName = createCustomClassNames(className, "slot");
-    const linksClassName = createCustomClassNames(className, "links");
-    const copyrightClassName = createCustomClassNames(className, "copyright");
+  const {
+    children,
+    copyright,
+    groups,
+    links,
+    onMinimize,
+    search,
+    showMinimizeButton,
+    stylesheet: customStylesheet,
+    ...otherProps
+  } = props;
+  const { className } = otherProps;
+  const overflowClassName = createCustomClassNames(className, "overflow");
+  const slotClassName = createCustomClassNames(className, "slot");
+  const linksClassName = createCustomClassNames(className, "links");
+  const copyrightClassName = createCustomClassNames(className, "copyright");
 
-    return (
-      <ThemeContext.Consumer>
-        {({ resolvedRoles, metadata }) => {
-          const styles = stylesheet(
-            { stylesheet: customStylesheet, ...otherProps },
-            resolvedRoles
-          );
+  return (
+    <ThemeContext.Consumer>
+      {({ resolvedRoles, metadata }) => {
+        const styles = stylesheet(
+          { stylesheet: customStylesheet, ...otherProps },
+          resolvedRoles
+        );
 
-          return (
-            <nav className={cx([css(styles.sideNav), className])}>
-              <div className={cx([css(styles.overflow), overflowClassName])}>
-                {this._renderHeaders(resolvedRoles)}
+        return (
+          <nav className={cx([css(styles.sideNav), className])}>
+            <div className={cx([css(styles.overflow), overflowClassName])}>
+              {_renderHeaders(resolvedRoles)}
 
-                {groups && <div>{groups}</div>}
+              {groups && <div>{groups}</div>}
 
-                {children && (
-                  <div className={cx([css(styles.slot), slotClassName])}>
-                    {children}
-                  </div>
-                )}
-
-                {links && (
-                  <div className={cx([css(styles.links), linksClassName])}>
-                    {links}
-                  </div>
-                )}
-
-                {copyright && (
-                  <div
-                    className={cx([css(styles.copyright), copyrightClassName])}
-                  >
-                    {copyright}
-                  </div>
-                )}
-              </div>
-
-              {search}
-
-              {showMinimizeButton && (
-                <div className={css(styles.minimize)}>
-                  <IconButton
-                    icon={
-                      metadata.densityId === "medium-density" ? (
-                        <Back24 />
-                      ) : (
-                        <Back16 />
-                      )
-                    }
-                    title="Minimize"
-                    aria-label="Minimize"
-                    onClick={onMinimize}
-                  />
+              {children && (
+                <div className={cx([css(styles.slot), slotClassName])}>
+                  {children}
                 </div>
               )}
-            </nav>
-          );
-        }}
-      </ThemeContext.Consumer>
-    );
-  }
-}
+
+              {links && (
+                <div className={cx([css(styles.links), linksClassName])}>
+                  {links}
+                </div>
+              )}
+
+              {copyright && (
+                <div
+                  className={cx([css(styles.copyright), copyrightClassName])}
+                >
+                  {copyright}
+                </div>
+              )}
+            </div>
+
+            {search}
+
+            {showMinimizeButton && (
+              <div className={css(styles.minimize)}>
+                <IconButton
+                  icon={
+                    metadata.densityId === "medium-density" ? (
+                      <Back24 />
+                    ) : (
+                      <Back16 />
+                    )
+                  }
+                  title="Minimize"
+                  aria-label="Minimize"
+                  onClick={onMinimize}
+                />
+              </div>
+            )}
+          </nav>
+        );
+      }}
+    </ThemeContext.Consumer>
+  );
+};
+
+SideNav.displayName = "SideNav";
+
+SideNav.propTypes = {
+  /** Additional content to include below navigation items */
+  children: PropTypes.node,
+  /** Copyright text to include  */
+  copyright: PropTypes.node,
+  /** 0 or more SideNav Groups */
+  groups: PropTypes.node,
+  /** Subtitle at the top of the SideNav */
+  headerLabel: PropTypes.string,
+  /** An href for the SideNav Subtitle */
+  headerLink: PropTypes.string,
+  /** Called when headerLink is clicked */
+  onClickHeader: PropTypes.func,
+  /** 0 or more SideNav Links */
+  links: PropTypes.node,
+  /** Called when minimize button is clicked */
+  onMinimize: PropTypes.func,
+  /** A SideNav Search element */
+  search: PropTypes.node,
+  /** Renders a button to minimize the SideNav */
+  showMinimizeButton: PropTypes.bool,
+  /** Function to modify the component's styles */
+  stylesheet: PropTypes.func,
+  /** Title at the top of the SideNav */
+  superHeaderLabel: PropTypes.string,
+  /** An href for the SideNav Title */
+  superHeaderLink: PropTypes.string,
+  /** Called when superHeaderLink is clicked */
+  onClickSuperHeader: PropTypes.func
+};
+
+SideNav.defaultProps = {
+  onMinimize: () => {},
+  showMinimizeButton: false
+};
+
+export default SideNav;
