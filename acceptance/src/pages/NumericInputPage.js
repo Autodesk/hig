4cdff0a4d3abf40220de34259cd6cabffd54bc1c
robbie roxas <robbieroxas@robbies-MacBook-Pro.local>
2022-01-08 18:19:34 -0800

    refactor: revert NumericInputPage acceptance

--- acceptance/src/pages/NumericInputPage.js
+++ acceptance/src/pages/NumericInputPage.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 import NumericInput from "@hig/numeric-input";
 import Label from "@hig/label";
 import Spacer from "@hig/spacer";
@@ -14,67 +14,39 @@ const states = [
 
 const variants = ["line", "box"];
 
-
-export default class NumericInputPage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      value: 0
-    }
-  }
-
-  render() {
-    return (
-      <div style={{backgroundColor: '#fff'}}>
-        <NumericInput
-          variant="box"
-          value={this.state.value}
-          step={0.3}
-          onChange={value => {
-            console.log(value);
-            // console.log(typeof value);
-            this.setState({value})
-          }}
-        />
-      </div>
-    );
-  }
+function NumericInputPage() {
+  return (
+    <ThemeRepeater>
+      {({ id }) => {
+        return (
+          <div>
+            {variants.map(variant => {
+              return (
+                <div key={variant}>
+                  <Text>{variant}</Text>
+                  {states.map(({ label, props }) => {
+                    const key = id + label + variant;
+                    return (
+                      <div key={key}>
+                        <div style={{}}>
+                          <Label htmlFor={key} disabled={props.disabled}>
+                            {label}
+                          </Label>
+                          <NumericInput id={key} {...props} variant={variant} />
+                          <Spacer spacing="xs" />
+                        </div>
+                        <Spacer />
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        );
+      }}
+    </ThemeRepeater>
+  );
 }
 
-// function NumericInputPage() {
-//   return (
-//     <ThemeRepeater>
-//       {({ id }) => {
-//         return (
-//           <div>
-//             {variants.map(variant => {
-//               return (
-//                 <div key={variant}>
-//                   <Text>{variant}</Text>
-//                   {states.map(({ label, props }) => {
-//                     const key = id + label + variant;
-//                     return (
-//                       <div key={key}>
-//                         <div style={{}}>
-//                           <Label htmlFor={key} disabled={props.disabled}>
-//                             {label}
-//                           </Label>
-//                           <NumericInput id={key} {...props} variant={variant} />
-//                           <Spacer spacing="xs" />
-//                         </div>
-//                         <Spacer />
-//                       </div>
-//                     );
-//                   })}
-//                 </div>
-//               );
-//             })}
-//           </div>
-//         );
-//       }}
-//     </ThemeRepeater>
-//   );
-// }
-
-// export default NumericInputPage;
+export default NumericInputPage;
