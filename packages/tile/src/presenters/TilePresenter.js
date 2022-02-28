56d679befee12f960f1511b435b2602ac92e1769
robbie roxas <robbieroxas@robbies-MacBook-Pro.local>
2022-02-28 11:05:34 -0800

    refactor: add additional content

--- packages/tile/src/presenters/TilePresenter.js
+++ packages/tile/src/presenters/TilePresenter.js
@@ -9,6 +9,14 @@ const TilePresenter = (props) => {
     headerContainer: HeaderContainer,
     title,
     subtitle,
+    version,
+    identifier,
+    statusIcons,
+    actionIcons,
+    notification,
+    overflowMenu,
+    cta,
+    ctaType,
   } = props;
   return (
     <ThemeContext.Consumer>
@@ -16,11 +24,36 @@ const TilePresenter = (props) => {
         const styles = stylesheet(props, resolvedRoles);
         return (
           <div className={css(styles.higTileContainer)}>
-            <div className={css(styles.higTileHeader)}><HeaderContainer /></div>
-            <div className={css(styles.higTileContent)}>
-              <div className={css(styles.higTileTitle)}>{title}</div>
-              <div className={css(styles.higTileSubTitle)}>{subtitle}</div>
+            <div className={css(styles.higTileNotifications)}>
+              <div className={css(styles.higTileNotificationBadge)}>{notification.type}</div>
             </div>
+            <div className={css(styles.higTileHeader)}><HeaderContainer /></div>
+            
+            <div className={css(styles.higTileContent)}>
+              <div className={css(styles.higTileIdentifierContainer)}>
+                <div className={css(styles.higTileIdentifierIcon)}>{identifier}</div>
+              </div>
+              <div className={css(styles.higTileTitleContainer)}>
+                <div className={css(styles.higTileTitle)}>{title}</div>
+                <div className={css(styles.higTileOverflowMenu)}>{overflowMenu}</div>
+              </div>
+              <div className={css(styles.higTileSubTitle)}>{subtitle}</div>
+
+              <div className={css(styles.higTileAdditionalContent)}>
+                <div className={css(styles.higVersionHolder)}>{version}</div>
+                <div className={css(styles.higGroupIcons)}>
+                  {statusIcons[0]}
+                  {actionIcons[0].icon}
+                </div>
+              </div>
+              <div className={css(styles.higTileCTAHolder)}>
+                <div className={css(styles.higTileCTA)}>
+                  {cta}
+                </div>
+              </div>
+            </div>
+            
+            
           </div>
         )
       }}
