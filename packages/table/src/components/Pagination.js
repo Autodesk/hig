2cfa35aae5c537b514e7b679c6d999199a5288e9
robbie roxas <robbieroxas@robbies-MacBook-Pro.local>
2022-03-22 20:01:19 -0700

    refactor: pagination, sortColumns linting

--- packages/table/src/components/Pagination.js
+++ packages/table/src/components/Pagination.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { css } from "emotion";
 
 const styles = {
@@ -9,5 +10,17 @@ const styles = {
 const Pagination = ({ pageDetails, children }) => (
   <div className={css(styles)}>{children(pageDetails)}</div>
 );
+
+Pagination.propTypes = {
+  children: PropTypes.func,
+  pageDetails: PropTypes.shape({
+    pageIndex: PropTypes.number,
+    pageOptions: PropTypes.number,
+    previousPage: PropTypes.number,
+    nextPage: PropTypes.number,
+    canPreviousPage: PropTypes.bool,
+    canNextPage: PropTypes.bool
+  })
+};
 
 export default Pagination;
--- packages/table/src/components/SortColumns.js
+++ packages/table/src/components/SortColumns.js
@@ -1,7 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const SortColumns = ({ children, isSorted, isSortedDesc }) => (
   <>{children({ isSorted, isSortedDesc })}</>
 );
+
+SortColumns.propTypes = {
+  children: PropTypes.func,
+  isSorted: PropTypes.bool,
+  isSortedDesc: PropTypes.bool
+};
 
 export default SortColumns;
