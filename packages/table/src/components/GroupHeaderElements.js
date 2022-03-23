13f1e1549e799c21126a9bbe333c5c9620239f90
robbie roxas <robbieroxas@robbies-MacBook-Pro.local>
2022-03-22 19:47:11 -0700

    refactor: fix groupHeaderElements linting

--- packages/table/src/components/GroupHeaderElements.js
+++ packages/table/src/components/GroupHeaderElements.js
@@ -1,11 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import { CaretRight16, CaretDown16 } from "@hig/icons";
 import { css } from "emotion";
 
 const GroupHeaderElements = ({ isGrouped, groupHeaderElementStyles }) => (
-  <GroupHeader className={css(groupHeaderElementStyles)}>
+  <div className={css(groupHeaderElementStyles)}>
     {isGrouped ? <CaretDown16 /> : <CaretRight16 />}
-  </GroupHeader>
+  </div>
 );
+
+GroupHeaderElements.propTypes = {
+  isGrouped: PropTypes.bool,
+  groupHeaderElementStyles: PropTypes.shape({
+    styles: PropTypes.string
+  })
+};
 
 export default GroupHeaderElements;
