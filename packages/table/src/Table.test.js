9e583f5e9dab3a53d7e9645a8c241f132cd54fe7
Wilbert Mui <wmui51@yahoo.com>
2022-03-23 20:00:34 -0700

    test: add more tests

--- packages/table/src/Table.test.js
+++ packages/table/src/Table.test.js
@@ -1,62 +1,18 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import Table from "./Table";
-import { SAMPLE_TABLE_OBJECT } from "./__fixtures__/sampleTableObject";
+import SAMPLE_TABLE_OBJECT from "./__fixtures__/sampleTableObject";
 
 describe("table/Table", () => {
   it("renders default Table", () => {
-    const wrapper = (
-      <Table tableObject={SAMPLE_TABLE_OBJECT} />
-    );
+    const wrapper = <Table tableObject={SAMPLE_TABLE_OBJECT} />;
     const tree = renderer.create(wrapper).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-//   it("renders checkmark indicator Menu and Options", () => {
-//     const wrapper = (
-//       <Menu checkmark>
-//         <Option id="option-1">Option 1</Option>
-//         <Option id="option-2">Option 2</Option>
-//         <Option id="option-3">Option 3</Option>
-//       </Menu>
-//     );
-//     const tree = renderer.create(wrapper).toJSON();
-//     expect(tree).toMatchSnapshot();
-//   });
-
-//   it("renders Menu and Options with defaultSelected", () => {
-//     const wrapper = (
-//       <Menu defaultSelected={["option-2"]}>
-//         <Option id="option-1">Option 1</Option>
-//         <Option id="option-2">Option 2</Option>
-//         <Option id="option-3">Option 3</Option>
-//       </Menu>
-//     );
-//     const tree = renderer.create(wrapper).toJSON();
-//     expect(tree).toMatchSnapshot();
-//   });
-
-//   it("renders Menu and Options with defaultSelected and checkmark variant", () => {
-//     const wrapper = (
-//       <Menu defaultSelected={["option-2"]} checkmark>
-//         <Option id="option-1">Option 1</Option>
-//         <Option id="option-2">Option 2</Option>
-//         <Option id="option-3">Option 3</Option>
-//       </Menu>
-//     );
-//     const tree = renderer.create(wrapper).toJSON();
-//     expect(tree).toMatchSnapshot();
-//   });
-
-//   it("renders Menu and Options with checkmark and unselect", () => {
-//     const wrapper = (
-//       <Menu defaultSelected={["option-2"]} unselect={false}>
-//         <Option id="option-1">Option 1</Option>
-//         <Option id="option-2">Option 2</Option>
-//         <Option id="option-3">Option 3</Option>
-//       </Menu>
-//     );
-//     const tree = renderer.create(wrapper).toJSON();
-//     expect(tree).toMatchSnapshot();
-//   });
+  it("renders Table with alternating background", () => {
+    const wrapper = <Table tableObject={SAMPLE_TABLE_OBJECT} alternateBg />;
+    const tree = renderer.create(wrapper).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
