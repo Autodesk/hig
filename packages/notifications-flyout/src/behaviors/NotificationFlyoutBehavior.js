0e5a9a27c4d3b0306e6ab7936c656b601bce3a56
Wilbert Mui <wmui51@yahoo.com>
2022-03-25 07:36:06 -0700

    style: fix linter error

--- packages/notifications-flyout/src/behaviors/NotificationFlyoutBehavior.js
+++ packages/notifications-flyout/src/behaviors/NotificationFlyoutBehavior.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-unused-prop-types */
 
 import { Component } from "react";
-import PropTypes from "prop-types"; 
+import PropTypes from "prop-types";
 
 import parseNotifications from "./parseNotifications";
 
