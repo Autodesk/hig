e05a8163334df029547227bb8cc34f7e0b2984d4
Wilbert Mui <wmui51@yahoo.com>
2022-05-01 10:58:54 -0700

    docs: add new storybook build

---
+++ packages/dropdown/src/__stories__/Dropdown.new-stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ArgsTable } from '@storybook/addon-docs';
+
+import Dropdown from '../index';
+import Readme from "../../README.md";
+
+export default {
+  title: 'Components/Dropdown',
+  component: Dropdown,
+  argTypes: {},
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Readme />
+          <ArgsTable />
+        </>
+      )
+    }
+  }
+};
+
+const Template = args => (
+  <Dropdown {...args} />
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+  options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+};
---
+++ packages/table/src/__stories__/Table.new-stories.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ArgsTable } from '@storybook/addon-docs';
+
+import Table from '../index';
+import SAMPLE_TABLE_OBJECT from '../__fixtures__/sampleTableObject';
+import Readme from "../../README.md";
+
+export default {
+  title: 'Components/Table',
+  component: Table,
+  argTypes: {},
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Readme />
+          <ArgsTable />
+        </>
+      )
+    }
+  }
+};
+
+const Template = args => (
+  <Table {...args} />
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+  tableObject: SAMPLE_TABLE_OBJECT
+};
