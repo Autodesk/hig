f35e811175d14d695f6222660b305f8406776f76
Bryan Young <bryan.young@autodesk.com>
2022-05-06 12:29:06 -0700

    feat: deprecated old segmented button theme data

--- packages/theme-data/src/baseTheme/components/segmentedButton.js
+++ packages/theme-data/src/baseTheme/components/segmentedButton.js
@@ -9,172 +9,317 @@ import {
 } from "../../consts/types";
 
 export default {
+  /**
+   *  ## Deprecated
+   *
+   *
+   */
   "segmentedButton.borderRadius": {
+    type: BORDER_RADIUS,
     value: {
       ref: "button.borderRadius",
     },
-    type: BORDER_RADIUS,
+    metadata: {
+      deprecated: {
+        equivalent: "button.borderRadius"
+      }
+    }
   },
   "segmentedButton.borderWidth": {
+    type: BORDER_WIDTH,
     value: {
       ref: "button.borderWidth",
     },
-    type: BORDER_WIDTH,
+    metadata: {
+      deprecated: {
+        equivalent: "button.borderWidth"
+      }
+    }
   },
   "segmentedButton.gutterWidth": {
+    type: LENGTH,
     value: {
       ref: "button.gutterWidth",
     },
-    type: LENGTH,
+    metadata: {
+      deprecated: {
+        equivalent: "button.gutterWidth"
+      }
+    }
   },
   "segmentedButton.minWidth": {
+    type: LENGTH,
     value: {
       ref: "button.minWidth",
     },
-    type: LENGTH,
+    metadata: {
+      deprecated: {
+        equivalent: "button.minWidth"
+      }
+    }
   },
   "segmentedButton.paddingHorizontal": {
+    type: SPACING,
     value: {
       ref: "button.paddingHorizontal",
     },
-    type: SPACING,
+    metadata: {
+      deprecated: {
+        equivalent: "button.paddingHorizontal"
+      }
+    }
   },
   "segmentedButton.paddingVertical": {
+    type: SPACING,
     value: {
       ref: "button.paddingVertical",
     },
-    type: SPACING,
+    metadata: {
+      deprecated: {
+        equivalent: "button.paddingVertical"
+      }
+    }
   },
   "segmentedButton.default.backgroundColor": {
+    type: COLOR,
     value: {
       ref: "button.outline.default.backgroundColor",
     },
-    type: COLOR,
+    metadata: {
+      deprecated: {
+        equivalent: "button.segmented.default.backgroundColor"
+      }
+    }
   },
   "segmentedButton.hover.backgroundColor": {
+    type: COLOR,
     value: {
       ref: "button.outline.hover.backgroundColor",
     },
-    type: COLOR,
+    metadata: {
+      deprecated: {
+        equivalent: "button.segmented.hover.backgroundColor"
+      }
+    }
   },
   "segmentedButton.focus.backgroundColor": {
+    type: COLOR,
     value: {
       ref: "button.outline.focus.backgroundColor",
     },
-    type: COLOR,
+    metadata: {
+      deprecated: {
+        equivalent: "button.segmented.focus.backgroundColor"
+      }
+    }
   },
   "segmentedButton.pressed.backgroundColor": {
+    type: COLOR,
     value: {
       ref: "button.outline.pressed.backgroundColor",
     },
-    type: COLOR,
+    metadata: {
+      deprecated: {
+        equivalent: "button.segmented.active.backgroundColor"
+      }
+    }
   },
   "segmentedButton.default.borderColor": {
+    type: COLOR,
     value: {
       ref: "button.outline.default.borderColor",
     },
-    type: COLOR,
+    metadata: {
+      deprecated: {
+        equivalent: "button.segmented.default.borderColor"
+      }
+    }
   },
   "segmentedButton.hover.borderColor": {
+    type: COLOR,
     value: {
       ref: "button.outline.hover.borderColor",
     },
-    type: COLOR,
+    metadata: {
+      deprecated: {
+        equivalent: "button.segmented.hover.borderColor"
+      }
+    }
   },
   "segmentedButton.focus.borderColor": {
+    type: COLOR,
     value: {
       ref: "button.outline.focus.borderColor",
     },
-    type: COLOR,
+    metadata: {
+      deprecated: {
+        equivalent: "button.segmented.focus.borderColor"
+      }
+    }
   },
   "segmentedButton.pressed.borderColor": {
+    type: COLOR,
     value: {
       ref: "button.outline.pressed.borderColor",
     },
-    type: COLOR,
+    metadata: {
+      deprecated: {
+        equivalent: "button.segmented.active.borderColor"
+      }
+    }
   },
   "segmentedButton.hover.haloWidth": {
+    type: BORDER_WIDTH,
     value: {
       ref: "button.hover.haloWidth",
     },
-    type: BORDER_WIDTH,
+    metadata: {
+      deprecated: {
+        equivalent: "button.hover.haloWidth"
+      }
+    }
   },
   "segmentedButton.focus.haloWidth": {
+    type: BORDER_WIDTH,
     value: {
       ref: "button.haloWidth",
     },
-    type: BORDER_WIDTH,
+    metadata: {
+      deprecated: {
+        equivalent: "button.haloWidth"
+      }
+    }
   },
   "segmentedButton.pressed.haloWidth": {
+    type: BORDER_WIDTH,
     value: {
       ref: "button.pressed.haloWidth",
     },
-    type: BORDER_WIDTH,
+    metadata: {
+      deprecated: {
+        equivalent: "button.pressed.haloWidth"
+      }
+    }
   },
   "segmentedButton.hover.haloColor": {
+    type: COLOR,
     value: {
       ref: "button.hover.haloColor",
     },
-    type: COLOR,
+    metadata: {
+      deprecated: {
+        equivalent: "button.hover.haloColor"
+      }
+    }
   },
   "segmentedButton.focus.haloColor": {
+    type: COLOR,
     value: {
       ref: "button.focus.haloColor",
     },
-    type: COLOR,
+    metadata: {
+      deprecated: {
+        equivalent: "button.focus.haloColor"
+      }
+    }
   },
   "segmentedButton.pressed.haloColor": {
+    type: COLOR,
     value: {
       ref: "button.pressed.haloColor",
     },
-    type: COLOR,
+    metadata: {
+      deprecated: {
+        equivalent: "button.pressed.haloColor"
+      }
+    }
   },
   "segmentedButton.default.iconColor": {
+    type: COLOR,
     value: {
       ref: "button.outline.iconColor",
     },
-    type: COLOR,
+    metadata: {
+      deprecated: {
+        equivalent: "button.outline.iconColor"
+      }
+    }
   },
   "segmentedButton.active.iconColor": {
+    type: COLOR,
     value: {
       ref: "colorScheme.surface.level250",
     },
-    type: COLOR,
+    metadata: {
+      deprecated: {
+        equivalent: "button.outline.iconColor" 
+      }
+    }
   },
   "segmentedButton.active.backgroundColor": {
+    type: COLOR,
     value: {
       ref: "colorScheme.reference.base",
     },
-    type: COLOR,
+    metadata: {
+      deprecated: {
+        equivalent: "button.segmented.active.backgroundColor"
+      }
+    }
   },
   "segmentedButton.label.fontSize": {
+    type: FONT_SIZE,
     value: {
       ref: "button.label.fontSize",
     },
-    type: FONT_SIZE,
+    metadata: {
+      deprecated: {
+        equivalent: "button.label.fontSize"
+      }
+    }
   },
   "segmentedButton.label.default.fontColor": {
+    type: COLOR,
     value: {
       ref: "button.outline.label.fontColor",
     },
-    type: COLOR,
+    metadata: {
+      deprecated: {
+        equivalent: "button.segmented.label.fontColor"
+      }
+    }
   },
   "segmentedButton.label.default.fontWeight": {
+    type: FONT_WEIGHT,
     value: {
       ref: "button.label.fontWeight",
     },
-    type: FONT_WEIGHT,
+    metadata: {
+      deprecated: {
+        equivalent: "button.label.fontWeight"
+      }
+    }
   },
   "segmentedButton.label.active.fontColor": {
+    type: COLOR,
     value: {
       ref: "basics.colors.text.againstDark",
     },
-    type: COLOR,
+    metadata: {
+      deprecated: {
+        equivalent: "button.segmented.label.fontColor"
+      }
+    }
   },
   "segmentedButton.label.active.fontWeight": {
+    type: FONT_WEIGHT,
     value: {
       ref: "basics.fontWeights.bold",
     },
-    type: FONT_WEIGHT,
-  },
+    metadata: {
+      deprecated: {
+        equivalent: "basics.fontWeights.bold" 
+      }
+    }
+  }
 };
