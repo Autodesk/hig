99fc094dfe57bb28d5d75085a0d2e9397139f293
Wilbert Mui <wmui51@yahoo.com>
2022-05-12 10:39:16 -0700

    docs: include description in props table

--- packages/storybook-new/.storybook/preview.js
+++ packages/storybook-new/.storybook/preview.js
@@ -62,6 +62,7 @@ addDecorator(withThemes(
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
+    expanded: true,
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
