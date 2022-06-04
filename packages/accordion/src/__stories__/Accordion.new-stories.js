0ee79c43a8792a56d41d5fde577075c7272cfc53
Wilbert Mui <wmui51@yahoo.com>
2022-06-04 00:24:15 -0700

    docs: add accordion story

---
+++ packages/accordion/src/__stories__/Accordion.new-stories.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { ArgsTable, Primary } from "@storybook/addon-docs";
+import Typography from "@hig/typography";
+// import { Error12, Error16 } from "@hig/icons";
+
+import Accordion from "../index";
+import Readme from "../../README.md";
+import { indicators, indicatorPositions } from "../constants";
+
+export default {
+  title: "Components/Accordion",
+  component: Accordion,
+  argTypes: {
+    indicator: {
+      options: [indicators.CARET, indicators.OPERATOR],
+      control: { type: "select" },
+    },
+    indicatorPosition: {
+      options: [indicatorPositions.LEFT, indicatorPositions.RIGHT],
+      control: { type: "select" },
+    },
+  },
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Primary />
+          <Readme />
+          <ArgsTable />
+        </>
+      ),
+    },
+  },
+};
+
+const Template = (args) => <Accordion {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  children: <Typography>Accordion content</Typography>,
+  label: "Accordion Label",
+};
