6d18b809370ddb692a18417147a4ce6bf0dbff2e
Bryan Young <bryan.young@autodesk.com>
2022-08-09 16:27:37 -0700

    refactor: updated refs for dark blue tile

--- packages/theme-data/src/colorSchemes/darkBlue/components/tile.js
+++ packages/theme-data/src/colorSchemes/darkBlue/components/tile.js
@@ -1,33 +1,33 @@
 export default {
   "tile.checkbox.unchecked.backgroundColor": {
-    ref: {
-      value: "basics.colors.surface.darkBlue.100",
+    value: {
+      ref: "basics.colors.tertiary.pink.500",
     },
   },
   "tile.checkbox.indicatorColor": {
-    ref: {
-      value: "basics.colors.surface.darkBlue.200",
+    value: {
+      ref: "basics.colors.surface.darkBlue.200",
     },
   },
   "tile.checkbox.unchecked.default.borderColor": {
-    ref: {
-      value: "basics.colors.secondary.darkBlue.200",
+    value: {
+      ref: "basics.colors.secondary.darkBlue.200",
     },
     transform: {
       alpha: 0.5,
     },
   },
   "tile.checkbox.hover.haloColor": {
-    ref: {
-      value: "basics.colors.secondary.darkBlue.200",
+    value: {
+      ref: "basics.colors.secondary.darkBlue.200",
     },
     transform: {
       alpha: 0.15,
     },
   },
   "tile.checkbox.pressed.haloColor": {
-    ref: {
-      value: "basics.colors.secondary.darkBlue.200",
+    value: {
+      ref: "basics.colors.secondary.darkBlue.200",
     },
     transform: {
       alpha: 0.25,
