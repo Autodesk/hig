bb472dc9d0c9bdcb4bb85902ba740c8e632ac641
Bryan Young <bryan.young@autodesk.com>
2022-08-10 11:01:20 -0700

    feat: added tile to dark gray components

---
+++ packages/theme-data/src/colorSchemes/darkGray/components/tile.js
@@ -0,0 +1,36 @@
+export default {
+  "tile.checkbox.unchecked.backgroundColor": {
+    value: {
+      ref: "colorScheme.background.transparent",
+    },
+  },
+  "tile.checkbox.indicatorColor": {
+    value: {
+      ref: "basics.colors.primary.charcoal.900",
+    },
+  },
+  "tile.checkbox.unchecked.default.borderColor": {
+    value: {
+      ref: "basics.colors.primary.white",
+    },
+    transform: {
+      alpha: 0.5,
+    },
+  },
+  "tile.checkbox.hover.haloColor": {
+    value: {
+      ref: "basics.colors.primary.white",
+    },
+    transform: {
+      alpha: 0.15,
+    },
+  },
+  "tile.checkbox.pressed.haloColor": {
+    value: {
+      ref: "basics.colors.primary.white",
+    },
+    transform: {
+      alpha: 0.25,
+    },
+  },
+};
