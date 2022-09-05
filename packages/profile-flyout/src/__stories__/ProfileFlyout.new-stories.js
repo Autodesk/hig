81ddb17e2d0efd405df01de5bced096c27bd3580
Wilbert Mui <wmui51@yahoo.com>
2022-09-04 20:20:51 -0700

    feat: update theme-data dependency
    - this is really a docs: commit but need to get the updated dependencies onto npm

---
+++ packages/profile-flyout/src/__stories__/ProfileFlyout.new-stories.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { ArgsTable, Primary } from "@storybook/addon-docs";
+import avatarImagePath from "@hig/storybook/storybook-support/fixtures/avatar/chris-reynolds.png";
+
+import ProfileFlyout, { ProfileContent } from "../index";
+import Readme from "../../README.md";
+
+export default {
+  title: "Legacy Components/ProfileFlyout",
+  component: ProfileFlyout,
+  subcomponents: { ProfileContent },
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Primary />
+          <Readme />
+          <ArgsTable />
+        </>
+      ),
+    },
+  },
+};
+
+const renderProfileContent = () => (
+  <ProfileContent
+    profileName="David Gonzalez"
+    profileEmail="gonzalezd@autodesk.com"
+  >
+    <p>Child content can go here.</p>
+  </ProfileContent>
+);
+
+const Template = (args, context) => (
+  <ProfileFlyout
+    {...args}
+    key={`${context.globals.colorScheme}-${context.globals.density}`}
+  />
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+  children: renderProfileContent(),
+  avatarImage: avatarImagePath,
+  avatarName: "David Gonzalez",
+};
