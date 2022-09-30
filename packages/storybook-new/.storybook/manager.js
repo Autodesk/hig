2297cadd7bcd3e5da23900c0b7ad38a0651867be
Wilbert Mui <wmui51@yahoo.com>
2022-09-30 14:21:36 -0700

    docs: storybook tweaks

--- packages/storybook-new/.storybook/manager.js
+++ packages/storybook-new/.storybook/manager.js
@@ -1,6 +1,27 @@
+import React from "react";
 import { addons } from "@storybook/addons";
+import Typography from "@hig/typography";
 
 import weaveTheme from "./weaveTheme";
+
+const ROOT_IDS = [
+  "basics",
+  "dev-lab",
+  "components",
+  "legacy-components",
+  "theme-data",
+];
+
+const customRootStyles = (styles, props) => ({
+  ...styles,
+  typography: {
+    ...styles.typography,
+    fontWeight: 700,
+    textTransform: "initial",
+    letterSpacing: "normal",
+    // transform: "translateY(-5px)",
+  }
+})
 
 addons.setConfig({
   isFullscreen: false,
@@ -12,6 +33,19 @@ addons.setConfig({
   theme: undefined,
   selectedPanel: undefined,
   initialActive: "sidebar",
+  sidebar: {
+    showRoots: true,
+    renderLabel: (item) => {
+      console.log(item);
+      console.log('hi')
+      if (ROOT_IDS.indexOf(item.id) > -1) {
+        console.log('here');
+        return <Typography stylesheet={customRootStyles}>{item.name}</Typography>;
+      }
+      return <Typography>{item.name}</Typography>;
+    },
+    collapsedRoots: ROOT_IDS,
+  },
   toolbar: {
     title: { hidden: false },
     zoom: { hidden: true },
