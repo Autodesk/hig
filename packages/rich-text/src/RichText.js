6498b2e0a10927ff2d09dc319899da8c184fd47d
Wilbert Mui <wmui51@yahoo.com>
2023-02-21 22:19:43 -0800

    chore: update to weave-design namespace

--- packages/rich-text/src/RichText.js
+++ packages/rich-text/src/RichText.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { css, cx } from "emotion";
-import ThemeContext from "@hig/theme-context";
+import ThemeContext from "@weave-design/theme-context";
 
 import stylesheet from "./stylesheet";
 
