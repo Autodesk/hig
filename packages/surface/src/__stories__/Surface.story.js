bfa3560c3cfe8b2987b1d9c2c9a21fb42dcbeedf
Wilbert Mui <wmui51@yahoo.com>
2023-02-23 21:09:56 -0800

    chore: use weave-design namespace

--- packages/surface/src/__stories__/Surface.story.js
+++ packages/surface/src/__stories__/Surface.story.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ArgsTable, Primary } from "@storybook/addon-docs";
-import Typography from "@hig/typography";
+import Typography from "@weave-design/typography";
 
 import Surface from "../index";
 import Readme from "../../README.md";
