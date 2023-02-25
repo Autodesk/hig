512ad2a3098891bba0dffb373b5ff278359f232d
Wilbert Mui <wmui51@yahoo.com>
2023-02-24 23:56:16 -0800

    test: update test

--- packages/checkbox/src/presenters/CheckboxPresenter.test.js
+++ packages/checkbox/src/presenters/CheckboxPresenter.test.js
@@ -1,13 +1,8 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { generateId } from "@weave-design/utils";
 import CheckboxPresenter from "./CheckboxPresenter";
 
 describe("checkbox/presenters/CheckboxPresenter", () => {
-  afterEach(() => {
-    generateId.mockReset();
-  });
-
   [
     {
       description: "renders without props",
