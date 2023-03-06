279580dafc09ce538e877213f0193b056f2aeeff
Wilbert Mui <wmui51@yahoo.com>
2023-03-06 15:46:04 -0800

    test: update tests

--- packages/notifications-flyout/__mocks__/@hig/progress-ring.js
+++
@@ -1,8 +0,0 @@
-/** @todo Remove when `@hig/progress-ring` is migrated from vanilla */
-import React from "react";
-
-function ProgressRingMock(props) {
-  return <div data-props={JSON.stringify(props, null, 2)} />;
-}
-
-module.exports = jest.fn(ProgressRingMock);
