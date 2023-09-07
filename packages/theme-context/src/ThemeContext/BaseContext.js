06d7a57c047af452bb5a9c617127e4306ca649cf
Wilbert Mui <wmui51@yahoo.com>
2023-09-07 10:05:56 -0700

    refactor: linter fix

--- packages/theme-context/src/ThemeContext/BaseContext.js
+++ packages/theme-context/src/ThemeContext/BaseContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext } from "react";
 import defaultTheme from "@weave-design/theme-data/build/json/lightGrayMediumDensityTheme/theme.json";
 
 const { Provider, Consumer } = createContext(defaultTheme);
